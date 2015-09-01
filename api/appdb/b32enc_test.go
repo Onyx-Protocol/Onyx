bdc09df5cf3b80b87b433c7854b93de187edbb96
Jeff Lee <jeffomatic@gmail.com>
2015-09-01 12:34:21 -0700

    api/appdb: add function b32enc_crockford to schema
    Closes chain/chainprv#25.
    Reviewers: @kr

---
+++ api/appdb/b32enc_test.go
@@ -0,0 +1,44 @@
+package appdb
+
+import "testing"
+
+// Adapted from encoding/base32.
+// See $GOROOT/src/encoding/base32/base32_test.go.
+func TestB32encCrockford(t *testing.T) {
+	cases := []struct {
+		decoded, encoded string
+	}{
+		{"", ""},
+		{"f", "CR"},
+		{"fo", "CSQG"},
+		{"foo", "CSQPY"},
+		{"foob", "CSQPYRG"},
+		{"fooba", "CSQPYRK1"},
+		{"foobar", "CSQPYRK1E8"},
+		{"leasure.", "DHJP2WVNE9JJW"},
+		{"easure.", "CNGQ6XBJCMQ0"},
+		{"asure.", "C5SQAWK55R"},
+		{"sure.", "EDTQ4S9E"},
+		{"sure", "EDTQ4S8"},
+		{"sur", "EDTQ4"},
+		{"su", "EDTG"},
+		{
+			"Twas brillig, and the slithy toves",
+			"AHVP2WS0C9S6JV3CD5KJR831DSJ20X38CMG76V39EHM7J83MDXV6AWR",
+		},
+		{"\xDE\xAD\xBE\xEF", "VTPVXVR"},
+		{"\x00\x11\x22\x33\x44\x55\x66\x77", "008J4CT4ANK7E"},
+	}
+
+	for _, test := range cases {
+		var got string
+		err := db.QueryRow(`SELECT b32enc_crockford($1)`, test.decoded).Scan(&got)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+			continue
+		}
+		if got != test.encoded {
+			t.Errorf("b32enc_crockford(%q) = %q want %q", test.decoded, got, test.encoded)
+		}
+	}
+}
--- api/appdb/schema.sql
+++ api/appdb/schema.sql
@@ -40,6 +40,81 @@ COMMENT ON EXTENSION plv8 IS 'PL/JavaScript (v8) trusted procedural language';
 SET search_path = public, pg_catalog;
 
 --
+-- Name: b32enc_crockford(bytea); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION b32enc_crockford(src bytea) RETURNS text
+    LANGUAGE plpgsql IMMUTABLE
+    AS $$
+DECLARE
+	-- alphabet is the base32 alphabet defined
+	-- by Douglas Crockford. It preserves lexical
+	-- order and avoids visually-similar symbols.
+	-- See http://www.crockford.com/wrmg/base32.html.
+	alphabet text := '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+	dst text := '';
+	n integer;
+	b0 integer;
+	b1 integer;
+	b2 integer;
+	b3 integer;
+	b4 integer;
+	b5 integer;
+	b6 integer;
+	b7 integer;
+BEGIN
+	FOR r IN 0..(length(src)-1) BY 5
+	LOOP
+		b0:=0; b1:=0; b2:=0; b3:=0; b4:=0; b5:=0; b6:=0; b7:=0;
+
+		-- Unpack 8x 5-bit source blocks into an 8 byte
+		-- destination quantum
+		n := length(src) - r;
+		IF n >= 5 THEN
+			b7 := get_byte(src, r+4) & 31;
+			b6 := get_byte(src, r+4) >> 5;
+		END IF;
+		IF n >= 4 THEN
+			b6 := b6 | (get_byte(src, r+3) << 3) & 31;
+			b5 := (get_byte(src, r+3) >> 2) & 31;
+			b4 := get_byte(src, r+3) >> 7;
+		END IF;
+		IF n >= 3 THEN
+			b4 := b4 | (get_byte(src, r+2) << 1) & 31;
+			b3 := (get_byte(src, r+2) >> 4) & 31;
+		END IF;
+		IF n >= 2 THEN
+			b3 := b3 | (get_byte(src, r+1) << 4) & 31;
+			b2 := (get_byte(src, r+1) >> 1) & 31;
+			b1 := (get_byte(src, r+1) >> 6) & 31;
+		END IF;
+		b1 := b1 | (get_byte(src, r) << 2) & 31;
+		b0 := get_byte(src, r) >> 3;
+
+		-- Encode 5-bit blocks using the base32 alphabet
+		dst := dst || substr(alphabet, b0+1, 1);
+		dst := dst || substr(alphabet, b1+1, 1);
+		IF n >= 2 THEN
+			dst := dst || substr(alphabet, b2+1, 1);
+			dst := dst || substr(alphabet, b3+1, 1);
+		END IF;
+		IF n >= 3 THEN
+			dst := dst || substr(alphabet, b4+1, 1);
+		END IF;
+		IF n >= 4 THEN
+			dst := dst || substr(alphabet, b5+1, 1);
+			dst := dst || substr(alphabet, b6+1, 1);
+		END IF;
+		IF n >= 5 THEN
+			dst := dst || substr(alphabet, b7+1, 1);
+		END IF;
+	END LOOP;
+	RETURN dst;
+END;
+$$;
+
+
+--
 -- Name: next_chain_id(text); Type: FUNCTION; Schema: public; Owner: -
 --
 
