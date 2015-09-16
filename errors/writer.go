725384fbb1487d7cd9a1377648ea98fcf4d08acd
Keith Rarick <kr@xph.us>
2015-09-15 17:12:29 -0700

    errors: add Writer for sticky errors
    Closes chain/chainprv#97.
    Reviewers: @jeffomatic

---
+++ errors/writer.go
@@ -0,0 +1,38 @@
+package errors
+
+import "io"
+
+// NewWriter returns a new Writer that writes to w
+// until an error is returned.
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{w: w}
+}
+
+// Writer is in an implementation of the
+// "sticky error writer" pattern as described
+// in https://blog.golang.org/errors-are-values.
+//
+// A Writer makes one call
+// on the underlying writer for each call to Write,
+// until an error is returned. From that point on,
+// it makes no calls on the underlying writer,
+// and returns the same error value every time.
+type Writer struct {
+	w   io.Writer
+	err error
+}
+
+// Write makes one call on the underlying writer
+// if no error has previously occurred.
+func (w *Writer) Write(buf []byte) (n int, err error) {
+	if w.err != nil {
+		return 0, w.err
+	}
+	n, w.err = w.w.Write(buf)
+	return n, w.err
+}
+
+// Err returns the first error encountered by Write, if any.
+func (w *Writer) Err() error {
+	return w.err
+}
---
+++ errors/writer_test.go
@@ -0,0 +1,45 @@
+package errors
+
+import (
+	"io"
+	"testing"
+)
+
+func TestWriter(t *testing.T) {
+	errX := New("x")
+	tw := testWriter{nil, errX, nil}
+	w := NewWriter(&tw)
+	_, err := w.Write([]byte{1})
+	if err != nil {
+		t.Error("unexpected error", err)
+	}
+	if len(tw) != 2 {
+		t.Errorf("len(tw) = %d want 2", len(tw))
+	}
+	for i := 0; i < 10; i++ {
+		_, err = w.Write([]byte{1})
+		if err != errX {
+			t.Errorf("err = %v want %v", err, errX)
+		}
+		if len(tw) != 1 {
+			t.Errorf("len(tw) = %d want 1", len(tw))
+		}
+	}
+	if got := w.Err(); got != errX {
+		t.Errorf("w.Err() = %v want %v", got, errX)
+	}
+}
+
+// testWriter returns its errors in order.
+// elements of a testWriter may be nil.
+// if its len is 0, it returns io.EOF.
+type testWriter []error
+
+func (tw *testWriter) Write(p []byte) (int, error) {
+	if len(*tw) == 0 {
+		return len(p), io.EOF
+	}
+	err := (*tw)[0]
+	*tw = (*tw)[1:]
+	return len(p), err
+}
