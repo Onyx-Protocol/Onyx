3b118b4ab3877bb5f265dfb005513379d29ddb10
Jeff Lee <jeffomatic@gmail.com>
2015-10-09 15:25:58 -0700

    api: move getAssetActivity to issuer_nodes.go
    assets.go is a little too specific, so let's reduce our file count.
    
    Closes chain/chainprv#193.
    Reviewers: @erykwalder

--- api/issuer_nodes.go
+++ api/issuer_nodes.go
@@ -106,3 +106,22 @@ func getAssetGroupActivity(ctx context.Context, groupID string) (interface{}, er
 	}
 	return ret, nil
 }
+
+// GET /v3/assets/:assetID/activity
+func getAssetActivity(ctx context.Context, assetID string) (interface{}, error) {
+	prev, limit, err := getPageData(ctx, defActivityPageSize)
+	if err != nil {
+		return nil, err
+	}
+
+	activity, last, err := appdb.AssetActivity(ctx, assetID, prev, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := map[string]interface{}{
+		"last":       last,
+		"activities": httpjson.Array(activity),
+	}
+	return ret, nil
+}
