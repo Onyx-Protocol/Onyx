c0ea3a96d4c5d734be68f861ebc9febad870f2eb
Keith Rarick <kr@xph.us>
2015-10-20 14:23:18 -0700

    log/rotation: new package
    Closes chain/chainprv#232.
    Reviewers: @ryandotsmith

---
+++ log/rotation/file.go
@@ -0,0 +1,105 @@
+// Package rotation writes and rotates log files.
+package rotation
+
+import (
+	"bytes"
+	"os"
+	"strconv"
+)
+
+// A File is a log file with associated rotation files.
+// The rotation files are named after the base file
+// with a numeric suffix: base.1, base.2, and so on.
+// Calls to Write write data to the base file.
+// When the base file reaches the given size,
+// it is renamed to base.1
+// (and base.1 is renamed to base.2, and so on)
+// and a new base file is opened for subsequent writes.
+//
+// Any errors encountered while rotating files are ignored.
+// Only errors opening and writing the base file are reported.
+type File struct {
+	base string   // file name
+	size int64    // max size of f (limit on w)
+	n    int      // number of rotated files
+	buf  []byte   // partial line from last write
+	f    *os.File // current base file
+	w    int64    // bytes written to f
+}
+
+// Create creates a log writing to the named file
+// with mode 0666 (before umask),
+// truncating it if it already exists.
+// It will rotate to files name.1, name.2,
+// up to name.n.
+// The minimum value for n is 1;
+// lesser values will be taken as 1.
+func Create(name string, size, n int) *File {
+	return &File{
+		base: name,
+		size: int64(size),
+		n:    n,
+	}
+}
+
+var dropmsg = []byte("\nlog write error; some data dropped\n")
+
+// Write writes p to the log file f.
+// It writes only complete lines to the underlying file.
+// Incomplete lines are buffered in memory
+// and written once a NL is encountered.
+func (f *File) Write(p []byte) (n int, err error) {
+	f.buf = append(f.buf, p...)
+	n = len(p)
+	if i := bytes.LastIndexByte(f.buf, '\n'); i >= 0 {
+		_, err = f.write(f.buf[:i+1])
+		// Even if the write failed, discard the entire
+		// requested write payload. If we kept it around,
+		// then a failure to open the log file would
+		// cause us to accumulate data in memory
+		// without bound.
+		f.buf = f.buf[i+1:]
+		if err != nil {
+			// If we recover and resume logging,
+			// leave a message to indicate we dropped
+			// some lines.
+			f.buf = append(dropmsg, f.buf...)
+		}
+	}
+	return
+}
+
+// write writes the given data to f,
+// rotating files if necessary.
+func (f *File) write(p []byte) (int, error) {
+	// If p would increase the file over the
+	// max size, it is time to rotate.
+	if f.w+int64(len(p)) > f.size {
+		// best-effort; ignore errors
+		f.rotate()
+		f.f.Close()
+		f.f = nil
+		f.w = 0
+	}
+	if f.f == nil {
+		var err error
+		f.f, err = os.Create(f.base)
+		if err != nil {
+			return 0, err
+		}
+	}
+	n, err := f.f.Write(p)
+	f.w += int64(n)
+	return n, err
+}
+
+func (f *File) rotate() {
+	for i := f.n - 1; i > 0; i-- {
+		os.Rename(f.name(i), f.name(i+1))
+	}
+	os.Rename(f.base, f.name(1))
+}
+
+func (f *File) name(i int) string {
+	return f.base + "." + strconv.Itoa(i)
+}
---
+++ log/rotation/file_test.go
@@ -0,0 +1,228 @@
+package rotation
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestRotate(t *testing.T) {
+	defer os.Remove("x")
+	defer os.Remove("x.1")
+	defer os.Remove("x.2")
+	defer os.Remove("x.3") // just in case
+	os.Remove("x")
+	os.Remove("x.1")
+	os.Remove("x.2")
+	os.Remove("x.3") // just in case
+
+	f := Create("x", 1e6, 2)
+
+	touch("x")
+	f.rotate()
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if isRegular("x.2") {
+		t.Fatal("want no rotated file x.2")
+	}
+	if isRegular("x.3") {
+		t.Fatal("want no rotated file x.3")
+	}
+
+	touch("x")
+	f.rotate()
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if !isRegular("x.2") {
+		t.Fatal("want rotated file x.2")
+	}
+	if isRegular("x.3") {
+		t.Fatal("want no rotated file x.3")
+	}
+
+	touch("x")
+	f.rotate()
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if !isRegular("x.2") {
+		t.Fatal("want rotated file x.2")
+	}
+	if isRegular("x.3") {
+		t.Fatal("want no rotated file x.3")
+	}
+}
+
+func TestRotate0(t *testing.T) {
+	defer os.Remove("x")
+	defer os.Remove("x.1")
+	defer os.Remove("x.2") // just in case
+	os.Remove("x")
+	os.Remove("x.1")
+	os.Remove("x.2") // just in case
+
+	f := Create("x", 1e6, 0)
+
+	touch("x")
+	f.rotate()
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if isRegular("x.2") {
+		t.Fatal("want no rotated file x.2")
+	}
+
+	touch("x")
+	f.rotate()
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if isRegular("x.2") {
+		t.Fatal("want no rotated file x.2")
+	}
+}
+
+func TestInternalWriteNoRotate(t *testing.T) {
+	defer os.Remove("x")
+	defer os.Remove("x.1") // just in case
+	os.Remove("x")
+	os.Remove("x.1") // just in case
+
+	f := &File{
+		base: "x",
+		n:    1,
+		size: 10,
+		w:    0,
+	}
+	b := []byte("abc\n")
+	n, err := f.write(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(b) {
+		t.Fatalf("write(%q) = %d want %d", b, n, len(b))
+	}
+	if isRegular("x.1") {
+		t.Fatal("want no rotated file x.1")
+	}
+	if f.w != int64(len(b)) {
+		t.Fatalf("f.w = %d want %d", f.w, len(b))
+	}
+}
+
+func TestInternalWriteRotate(t *testing.T) {
+	defer os.Remove("x")
+	defer os.Remove("x.1")
+	os.Remove("x")
+	os.Remove("x.1")
+
+	f0, err := os.Create("x")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f := &File{
+		base: "x",
+		n:    1,
+		size: 10,
+		w:    9,
+		f:    f0,
+	}
+	b := []byte("abc\n")
+	n, err := f.write(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(b) {
+		t.Fatalf("write(%q) = %d want %d", b, n, 4)
+	}
+	if !isRegular("x.1") {
+		t.Fatal("want rotated file x.1")
+	}
+	if f.f == f0 {
+		t.Fatal("want new file object")
+	}
+	if f.w != int64(len(b)) {
+		t.Fatalf("f.w = %d want %d", f.w, len(b))
+	}
+}
+
+func TestWrite(t *testing.T) {
+	defer os.Remove("x")
+	defer os.Remove("x.1")
+	os.Remove("x")
+	os.Remove("x.1")
+	f := Create("x", 1e6, 1)
+
+	b0 := []byte("ab")
+	n, err := f.Write(b0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(b0) {
+		t.Fatalf("n = %d want %d", n, len(b0))
+	}
+	if !bytes.Equal(f.buf, b0) {
+		t.Fatalf("buf = %q want %q", f.buf, b0)
+	}
+	if f.w != 0 {
+		t.Fatalf("w = %d want 0", f.w)
+	}
+
+	b1 := []byte("c\n")
+	n, err = f.Write(b1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(b1) {
+		t.Fatalf("n = %d want %d", n, len(b1))
+	}
+	if !bytes.Equal(f.buf, nil) {
+		t.Fatalf("buf = %q want %q", f.buf, "")
+	}
+	if f.w != int64(len(b0)+len(b1)) {
+		t.Fatalf("w = %d want %d", f.w, len(b0)+len(b1))
+	}
+}
+
+func TestOpenFail(t *testing.T) {
+	f := Create("/tmp/improbable-test-path/second-level/x", 1e6, 1)
+	b := []byte("abc\nd")
+	want := append(dropmsg, 'd')
+	n, err := f.Write(b)
+	if err == nil {
+		t.Error("want error")
+	}
+	if n != len(b) {
+		t.Errorf("n = %d want %d", n, len(b))
+	}
+	if !bytes.Equal(f.buf, want) {
+		t.Fatalf("buf = %q want %q", f.buf, want)
+	}
+
+	n, err = f.Write(b)
+	if err == nil {
+		t.Error("want error")
+	}
+	if n != len(b) {
+		t.Errorf("n = %d want %d", n, len(b))
+	}
+	// don't accumulate multiple dropped-log messages
+	if !bytes.Equal(f.buf, want) {
+		t.Fatalf("buf = %q want %q", f.buf, want)
+	}
+}
+
+func isRegular(name string) bool {
+	fi, err := os.Stat(name)
+	return err == nil && fi.Mode().IsRegular()
+}
+
+func touch(name string) {
+	f, _ := os.Create(name)
+	if f != nil {
+		f.Close()
+	}
+}
