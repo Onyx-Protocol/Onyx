40f6e9483be4c72fb4b3fe79f76b3f1b5af727dd
Jeff Lee <jeffomatic@gmail.com>
2016-01-20 16:42:01 -0800

    api: remove list of papi errors in comments
    The list of old papi errors was useful for ensuring compatibility with
    existing clients, but enough time has passed that it doesn't seem
    relevant anymore. It seems like some error codes in the new API were
    being chosen to ignore collisions with the list of papi errors, which
    seems unnecessary.
    
    Closes chain/chainprv#470.
    Reviewers: @kr

--- api/errors.go
+++ api/errors.go
@@ -50,62 +50,6 @@ var (
 		appdb.ErrBadRole:            errorInfo{400, "CH800", "Member role must be \"developer\" or \"admin\"."},
 		appdb.ErrAlreadyMember:      errorInfo{400, "CH801", "User is already a member of the project."},
 		errNotAdmin:                 errorInfo{403, "CH781", "Admin privileges are required perform this action"},
-
-		// Error codes imported from papi for convenient reference.
-		// Please delete lines from this block when you add them
-		// to the live code above or when you know they won't be used.
-		//
-		// ErrAPITryAgain     = errorInfo{500, "CH009", "Chain API Error, Try Again"}
-		// ErrRateLimit       = errorInfo{429, "CH011", "Exceeded rate limit. Email enterprise@chain.com to learn about our production services."}
-		//
-		// ErrTxStatusUnknown = errorInfo{500, "CH203", "Transaction relayed, status unknown"}
-		//
-		// ErrBadNotifURL   = errorInfo{400, "CH400", "Invalid notification URL"}
-		// ErrDupNotif      = errorInfo{400, "CH401", "New notification conflicts with an existing notification"}
-		// ErrBadBlockChain = errorInfo{400, "CH403", "Invalid block chain"}
-		// ErrBadNotifType  = errorInfo{400, "CH408", "Invalid notification type"}
-		//
-		// ErrMissingWallet       = errorInfo{404, "CH703", "Requested wallet could not be found"}
-		// ErrMissingWallet400    = errorInfo{400, "CH703", "Requested wallet could not be found"}
-		// ErrBadAppID            = errorInfo{400, "CH705", "Invalid application ID"}
-		// ErrBadKeyRotate        = errorInfo{400, "CH706", "Old XPub is not in current wallet key rotation"}
-		// ErrWalletOwnership     = errorInfo{400, "CH707", "Chain must provide fewer keys than required signatures"}
-		// ErrZeroSigReq          = errorInfo{400, "CH708", "Signatures required must be at least one"}
-		// ErrRequestedKeys       = errorInfo{400, "CH709", "Chain can only provide one key"}
-		// ErrMissingBucket400    = errorInfo{400, "CH710", "Requested bucket could not be found"}
-		// ErrMissingApp          = errorInfo{404, "CH711", "Requested application could not be found"}
-		// ErrMissingReceiver     = errorInfo{404, "CH721", "Requested receiver could not be found"}
-		// ErrMaxInputs           = errorInfo{400, "CH730", "Maximum number of inputs passed"}
-		// ErrNoInputs            = errorInfo{400, "CH731", "No inputs provided"}
-		// ErrNoOutputs           = errorInfo{400, "CH732", "No outputs provided"}
-		//
-		// ErrZeroInput           = errorInfo{400, "CH734", "Input amount must be greater than 0"}
-		// ErrZeroOutput          = errorInfo{400, "CH735", "Output amount must be greater than 0"}
-		// ErrSoloWallet          = errorInfo{400, "CH736", "Wallet input must be the only input"}
-		// ErrBadOut              = errorInfo{400, "CH737", "Invalid output"}
-		//
-		// ErrFeePayer            = errorInfo{400, "CH739", "There must be exactly one fee payer"}
-		// ErrMetadataHex         = errorInfo{400, "CH740", "Metadata must be hex encoded"}
-		// ErrMetadataLen         = errorInfo{400, "CH741", "Metadata cannot be longer than 40 bytes"}
-		// ErrTxTooBig            = errorInfo{400, "CH742", "Transaction byte size is too big"}
-		//
-		// ErrMetaAndOA           = errorInfo{400, "CH745", "Metadata cannot be added to open asset transactions"}
-		//
-		// ErrInvalidSig          = errorInfo{400, "CH751", "Signature was not valid for transaction"}
-		// ErrNTplInput           = errorInfo{400, "CH752", "Transaction template has wrong number of inputs"}
-		// ErrExtraSig            = errorInfo{400, "CH754", "Too many signatures"}
-		//
-		// ErrBadRedeem           = errorInfo{400, "CH756", "Redeem script is not a valid multisig script"}
-		// ErrMissingAsset        = errorInfo{404, "CH760", "Requested asset could not be found"}
-		//
-		// ErrAssetOpRetTooBig    = errorInfo{400, "CH762", "Too many outputs and/or asset defintion too large"}
-		// ErrAssetDefIDForbidden = errorInfo{400, "CH763", "Asset definition should not have asset IDs"}
-		// ErrInvalidPolicy       = errorInfo{400, "CH772", "Invalid policy"}
-		// ErrMissingApproval     = errorInfo{404, "CH773", "Requested approval could not be found"}
-		// ErrBadApprovals        = errorInfo{400, "CH774", "Approval request cannot be satisfied"}
-		// ErrMissingPolicy       = errorInfo{404, "CH775", "Requested policy could not be found"}
-		//
-		// ErrNoTxActivity        = errorInfo{404, "CH790", "Transaction details could not be found."}
 	}
 )
 
