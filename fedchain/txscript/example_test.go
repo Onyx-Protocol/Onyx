a08728a3459a1648f1db52fea680670e435de034
Eric Rykwalder <e.rykwalder@gmail.com>
2016-01-25 17:52:33 -0800

    fedchain/txscript: remove unused signing features
    Closes chain/chainprv#485.
    Reviewers: @kr

--- fedchain/txscript/example_test.go
+++ fedchain/txscript/example_test.go
@@ -8,11 +8,9 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
 
-	"chain/fedchain/bc"
 	"chain/fedchain/txscript"
 )
 
@@ -77,103 +75,4 @@ func ExampleExtractPkScriptAddrs() {
 	// Script Class: pubkeyhash
 	// Addresses: [12gpXQVcCL2qhTNQgyLVdCFG2Qs2px98nV]
 	// Required Signatures: 1
-}
-
-// This example demonstrates manually creating and signing a redeem transaction.
-func ExampleSignTxOutput() {
-	// Ordinarily the private key would come from whatever storage mechanism
-	// is being used, but for this example just hard code it.
-	privKeyBytes, err := hex.DecodeString("22a47fa09a223f2aa079edf85a7c2" +
-		"d4f8720ee63e502ee2869afab7de234b80c")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), privKeyBytes)
-	pubKeyHash := btcutil.Hash160(pubKey.SerializeCompressed())
-	addr, err := btcutil.NewAddressPubKeyHash(pubKeyHash,
-		&chaincfg.MainNetParams)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// For this example, create a fake transaction that represents what
-	// would ordinarily be the real transaction that is being spent.  It
-	// contains a single output that pays to address in the amount of 1 BTC.
-	originTx := &bc.TxData{
-		Version: bc.CurrentTransactionVersion,
-		Inputs: []*bc.TxInput{{
-			SignatureScript: []byte{txscript.OP_0, txscript.OP_0},
-		}},
-	}
-	pkScript, err := txscript.PayToAddrScript(addr)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	originTx.Outputs = append(originTx.Outputs, &bc.TxOutput{Value: 100000000, Script: pkScript})
-
-	// Create the transaction to redeem the fake transaction.
-	redeemTx := &bc.TxData{
-		Version: bc.CurrentTransactionVersion,
-		Inputs: []*bc.TxInput{{
-			Previous:        bc.Outpoint{Hash: originTx.Hash(), Index: 0},
-			SignatureScript: nil,
-		}},
-		Outputs: []*bc.TxOutput{{
-			Value:  0,
-			Script: nil,
-		}},
-	}
-
-	// Sign the redeeming transaction.
-	lookupKey := func(a btcutil.Address) (*btcec.PrivateKey, bool, error) {
-		// Ordinarily this function would involve looking up the private
-		// key for the provided address, but since the only thing being
-		// signed in this example uses the address associated with the
-		// private key from above, simply return it with the compressed
-		// flag set since the address is using the associated compressed
-		// public key.
-		//
-		// NOTE: If you want to prove the code is actually signing the
-		// transaction properly, uncomment the following line which
-		// intentionally returns an invalid key to sign with, which in
-		// turn will result in a failure during the script execution
-		// when verifying the signature.
-		//
-		// privKey.D.SetInt64(12345)
-		//
-		return privKey, true, nil
-	}
-	// Notice that the script database parameter is nil here since it isn't
-	// used.  It must be specified when pay-to-script-hash transactions are
-	// being signed.
-	sigScript, err := txscript.SignTxOutput(&chaincfg.MainNetParams,
-		redeemTx, 0, originTx.Outputs[0].Script, txscript.SigHashAll,
-		txscript.KeyClosure(lookupKey), nil, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	redeemTx.Inputs[0].SignatureScript = sigScript
-
-	// Prove that the transaction has been validly signed by executing the
-	// script pair.
-	flags := txscript.ScriptBip16 | txscript.ScriptVerifyDERSignatures |
-		txscript.ScriptStrictMultiSig |
-		txscript.ScriptDiscourageUpgradableNops
-	vm, err := txscript.NewEngine(nil, nil, originTx.Outputs[0].Script, redeemTx, 0, flags)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	if err := vm.Execute(); err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Transaction successfully signed")
-
-	// Output:
-	// Transaction successfully signed
 }
