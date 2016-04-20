0edc8587d260cb322115630fe9cd76e2d0ec5a5d
Keith Rarick <kr@xph.us>
2016-04-20 14:03:07 -0700

    cmd/cored: rename from api
    Closes chain/chainprv#803.
    Reviewers: @jbowens

--- cmd/cored/race.go
+++ cmd/cored/race.go
@@ -10,7 +10,7 @@ See builds/api/build for the details.
 To use this facility, log in to a machine, stop the
 normal binary via upstart, and start the race-enabled
 binary directly by loading the config vars and running
-command 'api_race'. Race reports are printed to stderr.
+command 'cored_race'. Race reports are printed to stderr.
 See https://golang.org/doc/articles/race_detector.html
 for more info.
 
