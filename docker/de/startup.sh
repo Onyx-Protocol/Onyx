3d7d12101f1f7eec3ba34b8ec1c9588caa2d34f6
Boyma Fahnbulleh <boymanjor@icloud.com>
2016-06-10 10:14:04 -0700

    docker/de: add chain dashboard
    This commit introduces chain dashboard to chain-core developer
    edition. It also adds idempotent, database initialization.
    
    Closes chain/chainprv#1037.
    Reviewers: @ryandotsmith

--- docker/de/startup.sh
+++ docker/de/startup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 chown -R postgres /var/lib/postgresql/data
-#TODO(ryandotsmith): ensure we deal with persistence and idempotency
+
 su postgres -c 'initdb -D /var/lib/postgresql/data'
 su postgres -c 'pg_ctl start -w -D /var/lib/postgresql/data -l /var/lib/postgresql/data/postgres.log'
 su postgres -c 'createdb core' 
-su postgres -c 'psql core -f /var/lib/chain/schema.sql'
+if [[ $? -eq 0 ]]; then
+  su postgres -c 'psql core -f /var/lib/chain/schema.sql'
+fi
 
-/usr/bin/chain/cored
+/usr/bin/chain/corectl boot hello@chain.com password
+/usr/bin/chain/cored &
+/srv/chain/dashboard/bin/rails s --binding=0.0.0.0 -p 8081
