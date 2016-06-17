c39c1138a94e24874c34dae0e01f9c5351793f47
Jeff Lee <jeffomatic@gmail.com>
2016-06-17 12:17:00 -0700

    core: createAsset no longer returns assetIssuerID
    This is a partial reversal of 841e1b2. We've decided to deprecate
    the issuerNodeID field the createAsset response, rather than
    replace it with assetIssuerID. It is somewhat redundant, since the
    client provided the asset issuer ID to begin with, and is
    inconsistent with appdb.AssetResponse (returned by getAsset),
    which never included issuerNodeID in the first place.
    
    Closes chain/chainprv#1056.
    Reviewers: @erykwalder

--- core/issuer_nodes.go
+++ core/issuer_nodes.go
@@ -172,10 +172,9 @@ func createAsset(ctx context.Context, inodeID string, in struct {
 	}
 
 	ret := map[string]interface{}{
-		"id":              ast.Hash.String(),
-		"asset_issuer_id": ast.IssuerNodeID,
-		"issuer_node_id":  ast.IssuerNodeID, // deprecated
-		"label":           ast.Label,
+		"id":             ast.Hash.String(),
+		"issuer_node_id": ast.IssuerNodeID, // deprecated
+		"label":          ast.Label,
 	}
 	return ret, nil
 }
