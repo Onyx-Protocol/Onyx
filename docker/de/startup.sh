04037caace3c41c5b392a28e5d12b61a58d52ae9
Boyma Fahnbulleh <boymanjor@icloud.com>
2016-09-09 10:51:41 -0700

    docker/de: do not run schema.sql on startup
    `cored` inits the schema on dev builds.
    
    Closes chain/chainprv#1651
    Reviewers: @erykwalder

--- docker/de/startup.sh
+++ docker/de/startup.sh
@@ -8,11 +8,9 @@ chown -R postgres /var/lib/postgresql/data
 su postgres -c 'initdb -D /var/lib/postgresql/data'
 su postgres -c 'pg_ctl start -w -D /var/lib/postgresql/data -l /var/lib/postgresql/data/postgres.log'
 su postgres -c 'createdb core'
-if [[ $? -eq 0 ]]; then
-	su postgres -c 'psql core -f /var/lib/chain/schema.sql'
-	# TODO(kr): generate credentials, print to stdout,
-	# and save to /var/log/chain/credentials.json
-fi
+# TODO(kr): if createdb call is successful,
+# generate credentials, print to stdout, and
+# save to /var/log/chain/credentials.json
 
 (
 	/usr/bin/chain/cored
