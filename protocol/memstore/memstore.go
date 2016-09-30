2609fd13a0880204d6c5be556d564a4e414ac09a
Jackson Owens <jackson_owens@alumni.brown.edu>
2016-09-30 10:51:41 -0700

    protocol/memstore: use mutex
    Running go test -race in the core/generator package produced a few race
    conditions, all related to accessing the memstore. Add a mutex to
    memstore to prevent data races.
    
    Closes chain/chainprv#2032

--- protocol/memstore/memstore.go
+++ protocol/memstore/memstore.go
@@ -2,6 +2,7 @@ package memstore
 
 import (
 	"context"
+	"sync"
 
 	"chain/protocol/bc"
 	"chain/protocol/state"
@@ -12,6 +13,7 @@ import (
 // All its fields are exported
 // so tests can directly inspect their values.
 type MemStore struct {
+	mu          sync.Mutex
 	Blocks      []*bc.Block
 	State       *state.Snapshot
 	StateHeight uint64
@@ -23,21 +25,33 @@ func New() *MemStore {
 }
 
 func (m *MemStore) Height(context.Context) (uint64, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	return uint64(len(m.Blocks)), nil
 }
 
 func (m *MemStore) SaveBlock(ctx context.Context, b *bc.Block) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.Blocks = append(m.Blocks, b)
 	return nil
 }
 
 func (m *MemStore) SaveSnapshot(ctx context.Context, height uint64, snapshot *state.Snapshot) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.State = state.Copy(snapshot)
 	m.StateHeight = height
 	return nil
 }
 
 func (m *MemStore) GetBlock(ctx context.Context, height uint64) (*bc.Block, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	index := height - 1
 	if index < 0 || index >= uint64(len(m.Blocks)) {
 		return nil, nil
@@ -46,6 +60,9 @@ func (m *MemStore) GetBlock(ctx context.Context, height uint64) (*bc.Block, erro
 }
 
 func (m *MemStore) LatestSnapshot(context.Context) (*state.Snapshot, uint64, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	if m.State == nil {
 		m.State = state.Empty()
 	}
