5c640a5bddec0229265e68d2219c5433da55c362
Ryan Smith <ryandotsmith@users.noreply.github.com>
2016-10-05 16:42:30 -0700

    cmd/testnet-info: new command
    We've had a heckuva time getting Cloudflare + S3 to properly support CORS +
    TLS.
    This may end up being less code than the XML CORS configuration that S3
    requires...
    
    Closes chain/chainprv#2095

---
+++ cmd/testnet-info/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"chain/env"
+)
+
+var (
+	addr = env.String("LISTEN", ":8080")
+	info = struct {
+		GeneratorURL      *string `json:"generator_url"`
+		BlockchainID      *string `json:"blockchain_id"`
+		NetworkRPCVersion *int    `json:"network_rpc_version"`
+		NextReset         *string `json:"next_reset"`
+	}{
+		env.String("GENERATOR_URL", ""),
+		env.String("BLOCKCHAIN_ID", ""),
+		env.Int("NETWORK_RPC_VERSION", 1),
+		env.String("NEXT_RESET", ""),
+	}
+)
+
+func main() {
+	env.Parse()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		json.NewEncoder(w).Encode(info)
+	})
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
