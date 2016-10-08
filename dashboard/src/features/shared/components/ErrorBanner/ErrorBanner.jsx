2ff7f40d0a80367d7e06004afd100971ed7f9c44
Dominic Dagradi <dominic@chain.com>
2016-10-07 18:29:17 -0700

    dashboard: prevent error messages from escaping container
    Closes chain/chainprv#2215

--- dashboard/src/features/shared/components/ErrorBanner/ErrorBanner.jsx
+++ dashboard/src/features/shared/components/ErrorBanner/ErrorBanner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './ErrorBanner.scss'
 
 class ErrorBanner extends React.Component {
   render() {
     return (
-      <div className='alert alert-danger'>
+      <div className={styles.main}>
         {this.props.title && <strong>{this.props.title}<br/></strong>}
         {this.props.message}
       </div>
