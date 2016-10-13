7028a71f764bb48653eab716f679df8548e4a094
Dan Robinson <danrobinson010@gmail.com>
2016-10-12 17:04:47 -0700

    NOTICE: centralize and organize copyright notices
    Creates a NOTICE file to refer to licensed code that is not isolated in its
    own vendored package. Updates files throughout for consistency. Also adds
    license-related files to vendored packages as appropriate.
    
    Closes chain/chainprv#2294

--- cmd/vet/vet_test.go
+++ cmd/vet/vet_test.go
@@ -1,6 +1,7 @@
 // Copyright 2013 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file at
+// https://github.com/golang/go/blob/master/LICENSE.
 
 package main_test
 
--- crypto/ed25519/ed25519.go
+++ crypto/ed25519/ed25519.go
@@ -1,6 +1,7 @@
 // Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file at
+// https://github.com/golang/crypto/blob/master/LICENSE.
 
 // Package ed25519 implements the Ed25519 signature algorithm. See
 // http://ed25519.cr.yp.to/.
