c65769ea1bf62d89ebc24c990344a5bda12d0b3a
Keith Rarick <kr@xph.us>
2016-10-18 23:08:05 -0700

    core: adjust latency range for long or slow RPCs
    Closes [unavailable]

--- core/metrics.go
+++ core/metrics.go
@@ -14,6 +14,13 @@ var (
 	latencies = map[string]*metrics.RotatingLatency{}
 
 	latencyExpvar = expvar.NewMap("latency")
+	latencyRange  = map[string]time.Duration{
+		networkRPCPrefix + "get-block":         5 * time.Second,
+		networkRPCPrefix + "get-blocks":        5 * time.Second,
+		networkRPCPrefix + "signer/sign-block": 5 * time.Second,
+		networkRPCPrefix + "get-snapshot":      30 * time.Second,
+		// the rest have a default range
+	}
 )
 
 // latency returns a rotating latency histogram for the given request.
@@ -25,7 +32,11 @@ func latency(tab *http.ServeMux, req *http.Request) *metrics.RotatingLatency {
 	}
 	// Create a histogram only if the path is legit.
 	if _, pat := tab.Handler(req); pat == req.URL.Path {
-		l := metrics.NewRotatingLatency(5, 100*time.Millisecond)
+		d, ok := latencyRange[req.URL.Path]
+		if !ok {
+			d = 100 * time.Millisecond
+		}
+		l := metrics.NewRotatingLatency(5, d)
 		latencies[req.URL.Path] = l
 		latencyExpvar.Set(req.URL.Path, l)
 		return l
