94a687153b6f0f7673ac5a5a4a5301aa48589f6b
Jeff Lee <jeffomatic@gmail.com>
2016-10-22 14:03:37 -0700

    dashboard: don't allow new-tx submits when replication lag is high
    Closes [unavailable]

--- dashboard/src/features/transactions/components/New/New.jsx
+++ dashboard/src/features/transactions/components/New/New.jsx
@@ -41,6 +41,13 @@ class Form extends React.Component {
   }
 
   submitWithValidation(data) {
+    const lagThreshold = 5
+    if (this.props.replicationLag === null || this.props.replicationLag >= lagThreshold) {
+      return Promise.reject({
+        _error: `Replication lag must be less than ${lagThreshold} to submit transactions via the dashboard. Please wait for the local core to catch up to the generator.`
+      })
+    }
+
     return new Promise((resolve, reject) => {
       this.props.submitForm(data)
         .catch((err) => reject({_error: err.message}))
@@ -176,7 +183,10 @@ const validate = values => {
 }
 
 export default BaseNew.connect(
-  BaseNew.mapStateToProps('transaction'),
+  state => ({
+    ...BaseNew.mapStateToProps('transaction')(state),
+    replicationLag: state.core.replicationLag,
+  }),
   BaseNew.mapDispatchToProps('transaction'),
   reduxForm({
     form: 'NewTransactionForm',
