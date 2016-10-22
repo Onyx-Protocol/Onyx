5e38eae70a233ce90f9044d3ab32c26881de56e1
Jeff Lee <jeffomatic@gmail.com>
2016-10-22 00:33:53 -0700

    dashboard: only commit token to localStorage
    This commit reduces the risk of bad core state infecting localStorage.
    Reloading the app is now more likely to fix issues due to bugs in the
    core reducer.
    
    Closes [unavailable]

--- dashboard/src/utility/localStorage.js
+++ dashboard/src/utility/localStorage.js
@@ -1,7 +1,14 @@
 export const exportState = (store) => () => {
   const state = store.getState()
   const exportable = {
-    core: state.core,
+    core: {
+      clientToken: (state.core || {}).clientToken,
+
+      // TODO: If the dashboard has a way of probing the core for a token
+      // requirement, we won't need to store these anymore.
+      requireClientToken: (state.core || {}).requireClientToken,
+      validToken: (state.core || {}).validToken,
+    },
     transaction: {
       generated: (state.transaction || {}).generated,
     },
