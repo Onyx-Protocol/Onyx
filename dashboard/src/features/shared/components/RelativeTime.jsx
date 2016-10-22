132846c6b315a888e374c31a2820e1c221e7735f
Dominic Dagradi <dominic@chain.com>
2016-10-22 00:18:04 -0700

    dashboard: add fallback for relative timestamps with clock skew
    Closes [unavailable]

--- dashboard/src/features/shared/components/RelativeTime.jsx
+++ dashboard/src/features/shared/components/RelativeTime.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import moment from 'moment'
+import { humanizeDuration } from 'utility/time'
 
 class RelativeTime extends React.Component {
   render() {
-    const timestamp = moment(this.props.timestamp).fromNow()
+    let timestamp = moment(this.props.timestamp).fromNow()
+
+    const diff = moment(this.props.timestamp).diff(moment())
+    if (diff > 0) {
+      timestamp = humanizeDuration(diff/1000) + ' ahead of local time'
+    }
+
     return(
       <span title={this.props.timestamp}>{timestamp}</span>
     )
