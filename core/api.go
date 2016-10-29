52a25477d00cb2f3b1a0764db2a7fcc5471d6351
Jackson Owens <jackson@chain.com>
2016-10-28 19:00:47 -0700

    core: up max request size to 10mb

--- core/api.go
+++ core/api.go
@@ -79,7 +79,7 @@ type RequestLimit struct {
 }
 
 func maxBytes(h http.Handler) http.Handler {
-	const maxReqSize = 1e5 // 100kB
+	const maxReqSize = 1e7 // 10mb
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// A block can easily be bigger than maxReqSize, but everything
 		// else should be pretty small.
