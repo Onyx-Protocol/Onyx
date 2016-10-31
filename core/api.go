bdfe841c2f1c7d1ac4a39a59a9106c7f38616938
Jackson Owens <jackson@chain.com>
2016-10-31 13:40:51 -0700

    1mb

--- core/api.go
+++ core/api.go
@@ -79,7 +79,7 @@ type RequestLimit struct {
 }
 
 func maxBytes(h http.Handler) http.Handler {
-	const maxReqSize = 1e7 // 10mb
+	const maxReqSize = 1e6 // 1mb
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// A block can easily be bigger than maxReqSize, but everything
 		// else should be pretty small.
