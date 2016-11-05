7dd7d0e016a06e11ab9ae5be85d8128a9be13c87
Jeff Lee <jeffomatic@gmail.com>
2016-11-04 17:30:29 -0700

    fix unspent outputs example

--- docs/core/examples/ruby/unspent_outputs.rb
+++ docs/core/examples/ruby/unspent_outputs.rb
@@ -6,89 +6,51 @@ signer = Chain::HSMSigner.new
 key = chain.mock_hsm.keys.create
 signer.add_key(key, chain.mock_hsm.signer_conn)
 
-chain.assets.create(
-  alias: 'gold',
-  root_xpubs: [key.xpub],
-  quorum: 1,
-)
+chain.assets.create(alias: 'gold', root_xpubs: [key.xpub], quorum: 1)
+chain.accounts.create(alias: 'alice', root_xpubs: [key.xpub], quorum: 1)
+chain.accounts.create(alias: 'bob', root_xpubs: [key.xpub], quorum: 1)
 
-chain.accounts.create(
-  alias: 'alice',
-  root_xpubs: [key.xpub],
-  quorum: 1,
-)
-
-chain.accounts.create(
-  alias: 'bob',
-  root_xpubs: [key.xpub],
-  quorum: 1,
-)
-
-issuance_tx = chain.transactions.submit(
-  client,
-  signer.sign(
-    chain.transactions.build do |b|
-      b.issue asset_alias: 'gold', amount: 200
-      b.control_with_account account_alias: 'alice', asset_alias: 'gold', amount: 100
-      b.control_with_account account_alias: 'alice', asset_alias: 'gold', amount: 100
-    end
-  )
-)
+issuance_tx = chain.transactions.submit(signer.sign(chain.transactions.build { |b|
+  b.issue asset_alias: 'gold', amount: 200
+  b.control_with_account account_alias: 'alice', asset_alias: 'gold', amount: 100
+  b.control_with_account account_alias: 'alice', asset_alias: 'gold', amount: 100
+}))
 
 # snippet alice-unspent-outputs
-aliceUnspentOutputs = chain.unspent_outputs.query()
+alice_unspent_outputs = chain.unspent_outputs.query(
   filter: 'account_alias=$1',
   filter_params: ['alice'],
-  .execute(client)
-
-while (aliceUnspentOutputs.hasNext()) {
-  UnspentOutput utxo = aliceUnspentOutputs.next()
-  puts('Unspent output in alice account: ' + utxo.transaction_id + ':' + utxo.position)
-}
+).each do |utxo|
+  puts "Unspent output in alice account: #{utxo.transaction_id}:#{utxo.position}"
+end
 # endsnippet
 
 # snippet gold-unspent-outputs
-goldUnspentOutputs = chain.unspent_outputs.query()
+goldUnspentOutputs = chain.unspent_outputs.query(
   filter: 'asset_alias=$1',
   filter_params: ['gold'],
-  .execute(client)
-
-while (goldUnspentOutputs.hasNext()) {
-  UnspentOutput utxo = goldUnspentOutputs.next()
-  puts('Unspent output containing gold: ' + utxo.transaction_id + ':' + utxo.position)
-}
+).each do |utxo|
+  puts "Unspent output containing gold: #{utxo.transaction_id}:#{utxo.position}"
+end
 # endsnippet
 
-String prevTransactionId = issuance_tx.id
+prev_transaction_id = issuance_tx.id
 
 # snippet build-transaction-all
-spendOutput = chain.transactions.build do |b|
-  .addAction(new Transaction.Action.SpendAccountUnspentOutput()
-    .setTransactionId(prevTransactionId)
-    .setPosition(0)
-  b.control_with_account
-    account_alias: 'bob',
-    asset_alias: 'gold',
-    amount: 100,
-  ).build(client)
+spend_output = chain.transactions.build do |b|
+  b.spend_account_unspent_output transaction_id: prev_transaction_id, position: 0
+  b.control_with_account account_alias: 'bob', asset_alias: 'gold', amount: 100
+end
 # endsnippet
 
-chain.transactions.submit(signer.sign(spendOutput))
+chain.transactions.submit(signer.sign(spend_output))
 
 # snippet build-transaction-partial
-spendOutputWithChange = chain.transactions.build do |b|
-  .addAction(new Transaction.Action.SpendAccountUnspentOutput()
-    .setTransactionId(prevTransactionId)
-    .setPosition(1)
-  b.control_with_account
-    account_alias: 'bob',
-    asset_alias: 'gold',
-    amount: 40,
-  b.control_with_account
-    account_alias: 'alice',
-    asset_alias: 'gold',
-    amount: 60,
-  ).build(client)
+spend_output_with_change = chain.transactions.build do |b|
+  b.spend_account_unspent_output transaction_id: prev_transaction_id, position: 1
+  b.control_with_account account_alias: 'bob', asset_alias: 'gold', amount: 40
+  b.control_with_account account_alias: 'alice', asset_alias: 'gold', amount: 60
+end
 # endsnippet
 
-chain.transactions.submit(signer.sign(spendOutputWithChange))
+chain.transactions.submit(signer.sign(spend_output_with_change))
