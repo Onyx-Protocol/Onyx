e45487fbbbbcd009852cefe60bee622ff1821b29
Chris Garvin <chris.garvin@me.com>
2016-11-14 15:58:53 -0800

    updated utxo header style

--- dashboard/src/features/unspents/components/ListItem.jsx
+++ dashboard/src/features/unspents/components/ListItem.jsx
@@ -7,7 +7,12 @@ class ListItem extends React.Component {
     const item = {...this.props.item}
     delete item.id
     return(<KeyValueTable
-            title={`${item.transaction_id} - ${item.position}`}
+            title={
+              <span>
+                Transaction <code>{item.transaction_id}</code> -
+                Position <code>{item.position}</code>
+              </span>
+             }
             actions={[
               <RawJsonButton key='raw-json' item={item} title={`utxo-${item.transaction_id}-${item.position}.json`}/>
             ]}
