4f4a3d6518a0c39e0b5c08d47976323eec87425a
Jackson Owens <jackson@chain.com>
2016-11-14 20:50:22 -0800

    protocol/mempool: topologically sort
    Sort transactions topologically when Dumping the contents of the
    pool, if they're not already in topological order. The old txdb pool
    used this, but I forgot to port when we moved to the mempool.
    
    Also, don't add a tx to the pool if it's already there.

--- protocol/mempool/mempool.go
+++ protocol/mempool/mempool.go
@@ -9,34 +9,49 @@ import (
 	"context"
 	"sync"
 
+	"chain/log"
 	"chain/protocol/bc"
 )
 
 // MemPool satisfies the protocol.Pool interface.
 type MemPool struct {
-	mu   sync.Mutex
-	pool []*bc.Tx // in topological order
+	mu     sync.Mutex
+	pool   []*bc.Tx // in topological order
+	hashes map[bc.Hash]bool
 }
 
 // New returns a new MemPool.
 func New() *MemPool {
-	return &MemPool{}
+	return &MemPool{hashes: make(map[bc.Hash]bool)}
 }
 
 // Insert adds a new pending tx to the pending tx pool.
 func (m *MemPool) Insert(ctx context.Context, tx *bc.Tx) error {
 	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.hashes[tx.Hash] {
+		return nil
+	}
+
+	m.hashes[tx.Hash] = true
 	m.pool = append(m.pool, tx)
-	m.mu.Unlock()
 	return nil
 }
 
 // Dump returns all pending transactions in the pool and
 // empties the pool.
-func (m *MemPool) Dump(context.Context) ([]*bc.Tx, error) {
+func (m *MemPool) Dump(ctx context.Context) ([]*bc.Tx, error) {
 	m.mu.Lock()
 	txs := m.pool
 	m.pool = nil
+	m.hashes = make(map[bc.Hash]bool)
 	m.mu.Unlock()
+
+	if !isTopSorted(txs) {
+		log.Messagef(ctx, "set of %d txs not in topo order; sorting", len(txs))
+		txs = topSort(txs)
+	}
+
 	return txs, nil
 }
