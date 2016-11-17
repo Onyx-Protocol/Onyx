54b1bccac7f9b21f5b357918e4204d5ade773ca6
Zarya Faraj <zaryafaraj@gmail.com>
2016-11-16 16:30:10 -0800

    dashboard: provide help links when incorrect query is entered

--- dashboard/src/features/shared/components/BaseList/EmptyList.jsx
+++ dashboard/src/features/shared/components/BaseList/EmptyList.jsx
@@ -26,8 +26,15 @@ class EmptyList extends React.Component {
     } else if (!this.props.showFirstTimeFlow) {
       classNames.push(styles.noResults)
       emptyBlock = <div>
-        <span className={styles.emptyLabel}>No results for query:</span>
+        <span className={`${styles.emptyLabel} ${styles.noResultsLabel}`}>No results for query:</span>
         <code className={styles.code}>{this.props.currentFilter.filter}</code>
+        <div className={styles.queryHelp}>
+          <p>To learn how to query the API, pleare refer to the documentation:</p>
+          <ol className={styles.queryHelpList}>
+            <li className={styles.queryHelpListItem}><a href="/docs/core/build-applications/queries" target="_blank">Queries</a></li>
+            <li className={styles.queryHelpListItem}><a href="/docs/core/reference/api-objects" target="_blank">API Objects</a></li>
+          </ol>
+        </div>
       </div>
     }
 
