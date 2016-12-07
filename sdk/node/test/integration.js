5a9b83a7c9cf4460e64de3a83a8dfb474505b9c7
Chris Garvin <chris.garvin@me.com>
2016-12-07 11:26:57 -0800

    partial integration file

--- sdk/node/test/integration.js
+++ sdk/node/test/integration.js
@@ -2,6 +2,17 @@ const chain = require('../index.js')
 const uuid = require('uuid')
 const assert = require('assert')
 
+const balanceByAssetAlias = (balances) => {
+  let res = {}
+  return Promise.resolve(balances)
+  .then((balance) => {
+    balance.items.forEach((item) => {
+      res[item.sum_by['asset_alias']] = item.amount
+    })
+    return res
+  })
+}
+
 describe('Chain SDK integration test', function() {
   it('integration test', function() {
     const client = new chain.Client()
@@ -11,6 +22,8 @@ describe('Chain SDK integration test', function() {
     const bobAlias = `bob-${uuid.v4()}`
     const goldAlias = `gold-${uuid.v4()}`
     const silverAlias = `silver-${uuid.v4()}`
+    const bronzeAlias = `bronze-${uuid.v4()}`
+    const copperAlias = `copper-${uuid.v4()}`
 
     let aliceKey, bobKey, goldKey, silverKey, otherKey
 
@@ -27,6 +40,8 @@ describe('Chain SDK integration test', function() {
       client.mockHsm.keys.create({alias: bobAlias}),
       client.mockHsm.keys.create({alias: goldAlias}),
       client.mockHsm.keys.create({alias: silverAlias}),
+      client.mockHsm.keys.create({alias: bronzeAlias}),
+      client.mockHsm.keys.create({alias: copperAlias}),
       client.mockHsm.keys.create(),
     ])).then(keys => {
       aliceKey = keys[0]
@@ -35,10 +50,10 @@ describe('Chain SDK integration test', function() {
       silverKey = keys[3]
       otherKey = keys[4]
 
-      signer.addKey(aliceKey, client.mockHsm.signerUrl)
-      signer.addKey(bobKey, client.mockHsm.signerUrl)
-      signer.addKey(goldKey, client.mockHsm.signerUrl)
-      signer.addKey(silverKey, client.mockHsm.signerUrl)
+      signer.addKey(aliceKey.xpub, client.mockHsm.signerUrl)
+      signer.addKey(bobKey.xpub, client.mockHsm.signerUrl)
+      signer.addKey(goldKey.xpub, client.mockHsm.signerUrl)
+      signer.addKey(silverKey.xpub, client.mockHsm.signerUrl)
     })
 
     // Account creation
@@ -66,6 +81,147 @@ describe('Chain SDK integration test', function() {
       assert.equal(batchResponse.successes.length, 2)
       assert.equal(batchResponse.errors.length, 1)
     })
+
+    // Asset creation
+
+    .then(() => Promise.all([
+      client.assets.create({alias: goldAlias, root_xpubs: [goldKey.xpub], quorum: 1}),
+      client.assets.create({alias: silverAlias, root_xpubs: [silverKey.xpub], quorum: 1})
+    ]))
+
+    .then(() => client.assets.create({alias: 'unobtanium'}))
+    .catch(exception => {
+      // Request is missing key fields
+      assert.ok(exception instanceof Error)
+    })
+
+    // Batch asset creation
+
+    .then(() =>
+      client.assets.createBatch([
+        {alias: bronzeAlias, root_xpubs: [otherKey.xpub], quorum: 1}, // success
+        {alias: 'unobtanium'},
+        {alias: copperAlias, root_xpubs: [otherKey.xpub], quorum: 1}, // success
+      ])
+    ).then(batchResponse => {
+      assert.equal(batchResponse.successes.length, 2)
+      assert.equal(batchResponse.errors.length, 1)
+    })
+
+    // Basic issuance
+
+    .then(() =>
+      client.transactions.build( function(builder){
+        builder.issue({
+          asset_alias: goldAlias,
+          amount: 100
+        })
+        builder.issue({
+          asset_alias: silverAlias,
+          amount: 200
+        })
+        builder.controlWithAccount({
+          account_alias: aliceAlias,
+          asset_alias: goldAlias,
+          amount: 100
+        })
+        builder.controlWithAccount({
+          account_alias: bobAlias,
+          asset_alias: silverAlias,
+          amount: 200
+        })
+      })
+      .then((issuance) => signer.sign(issuance))
+      .then((signed) => client.transactions.submit(signed))
+    )
+
+    .then(() => Promise.all([
+      balanceByAssetAlias(client.balances.query({filter: `account_alias='${aliceAlias}'`})),
+      balanceByAssetAlias(client.balances.query({filter: `account_alias='${bobAlias}'`}))
+    ]))
+    .then(balances => {
+      assert.deepEqual(balances[0], {[goldAlias]: 100})
+      assert.deepEqual(balances[1], {[silverAlias]: 200})
+    })
+
+    // Bad singleton build call
+
+    .then(() => client.transactions.build( function(builder) {
+      builder.issue({
+        asset_alias: "unobtanium",
+        amount: 100
+      })
+    }))
+    .catch(exception => {
+      // Non-existent asset
+      assert.ok(exception instanceof Error)
+    })
+
+    // Bad singleton submit call
+
+    .then(() => client.transactions.build( function(builder) {
+      builder.issue({
+        asset_alias: goldAlias,
+        amount: 1
+      })
+      builder.controlWithAccount({
+        account_alias: aliceAlias,
+        asset_alias: goldAlias,
+        amount: 100
+      })
+    }))
+    .then((issuance) => signer.sign(issuance))
+    .then((signed) => client.transactions.submit(signed))
+    .catch(exception => {
+      // unbalanced transaction
+      assert.ok(exception instanceof Error)
+    })
+
+    // Atomic swap
+
+    .then(() => client.transactions.build( function(builder) {
+      builder.spendFromAccount({
+        account_alias: aliceAlias,
+        asset_alias: goldAlias,
+        amount: 10
+      })
+      builder.controlWithAccount({
+        account_alias: aliceAlias,
+        asset_alias: silverAlias,
+        amount: 20
+      })
+    })
+    .then((swapProposal) => {
+      swapProposal.allow_additional_actions = true
+      return signer.sign(swapProposal)
+    })
+    .then((swapProposal) =>
+      client.transactions.build( function(builder) {
+        builder.baseTransaction(swapProposal.raw_transaction)
+        builder.spendFromAccount({
+          account_alias: bobAlias,
+          asset_alias: silverAlias,
+          amount: 20
+        })
+        builder.controlWithAccount({
+          account_alias: bobAlias,
+          asset_alias: goldAlias,
+          amount: 10
+        })
+      }))
+      .then((swapTx) => signer.sign(swapTx))
+      .then((signed) => client.transactions.submit(signed))
+    )
+
+    .then(() => Promise.all([
+      balanceByAssetAlias(client.balances.query({filter: `account_alias='${aliceAlias}'`})),
+      balanceByAssetAlias(client.balances.query({filter: `account_alias='${bobAlias}'`}))
+    ]))
+    .then(balances => {
+      assert.deepEqual(balances[0], {[goldAlias]: 90, [silverAlias]: 20})
+      assert.deepEqual(balances[1], {[goldAlias]: 10, [silverAlias]: 180})
+    })
+
 
   })
 })
