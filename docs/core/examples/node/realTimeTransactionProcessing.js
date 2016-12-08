0dde99383fdbcf270dce3a435d5b3201f3950a2e
Dominic Dagradi <dominic@chain.com>
2016-12-08 11:37:31 -0800

    feed example file

--- docs/core/examples/node/realTimeTransactionProcessing.js
+++ docs/core/examples/node/realTimeTransactionProcessing.js
@@ -4,51 +4,45 @@ const client = new chain.Client()
 const signer = new chain.HsmSigner()
 let key, feed
 
+// snippet processor-method
 processTransaction = (tx) => {
   console.log(`New transaction at ${tx.timestamp}`)
-  console.log(`ID: ${tx.id}`)
+  console.log(`\tID: ${tx.id}`)
 
   tx.inputs.forEach((input, index) => {
-    console.log(`Input ${index}`)
-    console.log(`Type: ${input.type}`)
-    console.log(`Asset: ${input.asset_alias}`)
-    console.log(`Amount: ${input.amount}`)
-    console.log(`Account: ${input.account_alias}`)
+    console.log(`\tInput ${index}`)
+    console.log(`\t\tType: ${input.type}`)
+    console.log(`\t\tAsset: ${input.asset_alias}`)
+    console.log(`\t\tAmount: ${input.amount}`)
+    console.log(`\t\tAccount: ${input.account_alias}`)
   })
 
   tx.outputs.forEach((output, index) => {
-    console.log(`Output ${index}`)
-    console.log(`Type: ${output.type}`)
-    console.log(`Purpose: ${output.purpose}`)
-    console.log(`Asset: ${output.asset_alias}`)
-    console.log(`Amount: ${output.amount}`)
-    console.log(`Account: ${output.account_alias}`)
+    console.log(`\tOutput ${index}`)
+    console.log(`\t\tType: ${output.type}`)
+    console.log(`\t\tPurpose: ${output.purpose}`)
+    console.log(`\t\tAsset: ${output.asset_alias}`)
+    console.log(`\t\tAmount: ${output.amount}`)
+    console.log(`\t\tAccount: ${output.account_alias}`)
   })
 }
+// endsnippet
 
-client.transactionFeeds.get({alias: 'local-transactions'})
-  .then((data) => console.log(data))
-
-Promise.all([
-  client.mockHsm.keys.create(),
-]).then(keys => {
-  key  = keys[0].xpub
-
+client.mockHsm.keys.create()
+.then(_key => {
+  key = _key.xpub
   signer.addKey(key, client.mockHsm.signerUrl)
 }).then(() => Promise.all([
-
   client.assets.create({
     alias: 'gold',
     root_xpubs: [key],
     quorum: 1,
   }),
-
   client.accounts.create({
     alias: 'alice',
     root_xpubs: [key],
     quorum: 1
   }),
-
   client.accounts.create({
     alias: 'bob',
     root_xpubs: [key],
@@ -61,15 +55,33 @@ Promise.all([
     filter: "is_local='yes'"
   })
   // endsnippet
-])
-).then(() => Promise.all([
-  client.transactionFeeds.get({
+]))
+.then(() => {
+
+  // snippet get-feed
+  const feedPromise = client.transactionFeeds.get({
     alias: 'local-transactions'
   })
-]))
-  .then(feeds => {
-    feed = feeds[0]
-}).then(() =>
+  // endsnippet
+
+  return feedPromise
+})
+.then(feed => {
+
+  // snippet processing-loop
+  const processingLoop = (tx, next, done, fail) => {
+    processTransaction(tx)
+    next(true)
+  }
+  // endsnippet
+
+  // snippet processing-thread
+  // JavaScript is single-threaded, and uses a callback for processing
+  feed.consume(processingLoop)
+  // endsnippet
+})
+.then(() =>
+  // snippet issue
   client.transactions.build( function(builder){
     builder.issue({
       asset_alias: 'gold',
@@ -83,7 +95,9 @@ Promise.all([
   })
   .then((issuance) => signer.sign(issuance))
   .then((signed) => client.transactions.submit(signed))
+  // endsnippet
 ).then(() =>
+  // snippet transfer
   client.transactions.build( function(builder){
     builder.spendFromAccount({
       account_alias: 'alice',
@@ -95,6 +109,8 @@ Promise.all([
       asset_alias: 'gold',
       amount: 50
     })
-  }).then((transfer) => signer.sign(transfer))
-    .then((signed) => client.transactions.submit(signed))
+  })
+  .then((issuance) => signer.sign(issuance))
+  .then((signed) => client.transactions.submit(signed))
+  // endsnippet
 )
