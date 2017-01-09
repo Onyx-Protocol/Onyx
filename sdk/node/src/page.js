4a63e113974bd189acbc07f5ad792f7acc2f893f
Dominic Dagradi <dominic@chain.com>
2017-01-09 12:18:25 -0800

    document page

--- sdk/node/src/page.js
+++ sdk/node/src/page.js
@@ -8,12 +8,44 @@
  * @class
  */
 class Page {
+
+  /**
+   * Create a page object
+   *
+   * @param  {Object} data  API response for a single page of data
+   * @param  {Object} owner Chain API object implementing the `query` method
+   */
   constructor(data, owner) {
+    /**
+     * Array of Chain Core objects
+     * @type {Array}
+     */
+    this.items = []
+
+    /**
+     * Filter for requesting the immediate next page of results
+     * @type {Filter}
+     */
+    this.next = {}
+
+
+    /**
+     * Indicator that there are more results to load if true
+     * @type {Boolean}
+     */
+    this.last_page = false
+
     Object.assign(this, data)
 
     this.owner = owner
   }
 
+  /**
+   * Fetch the next page of data for the query specified in this object
+   *
+   * @return {Promise<Page>} A promise resolving to a Page object containing
+   *                         the requested results
+   */
   nextPage() {
     return this.owner.query(this.next)
   }
