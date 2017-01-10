bc1e64b31fda0d886d5cd5e1dfb9732b217c8346
Oleg Andreev <oleganza@gmail.com>
2017-01-10 10:45:27 -0800

    local consts to make things clearer

--- protocol/bc/spend.go
+++ protocol/bc/spend.go
@@ -13,19 +13,23 @@ type SpendInput struct {
 func (si *SpendInput) IsIssuance() bool { return false }
 
 func NewSpendInput(txhash Hash, index uint32, arguments [][]byte, assetID AssetID, amount uint64, controlProgram, referenceData []byte) *TxInput {
+	const (
+		vmver = 1
+		assetver = 1
+	)
 	oc := OutputCommitment{
 		AssetAmount: AssetAmount{
 			AssetID: assetID,
 			Amount:  amount,
 		},
-		VMVersion:      1,
+		VMVersion:      vmver,
 		ControlProgram: controlProgram,
 	}
 	return &TxInput{
-		AssetVersion:  assetVersion,
+		AssetVersion:  assetver,
 		ReferenceData: referenceData,
 		TypedInput: &SpendInput{
-			OutputID: ComputeOutputID(txhash, index, oc.Hash(assetVersion)),
+			OutputID: ComputeOutputID(txhash, index, oc.Hash(assetver)),
 			OutputCommitment: oc,
 			Arguments: arguments,
 		},
