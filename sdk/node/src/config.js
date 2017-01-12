5c30d519e451511bf184111478cd548eebb380bd
Dominic Dagradi <dominic@chain.com>
2017-01-12 15:22:45 -0800

    add callback to config

--- sdk/node/src/config.js
+++ sdk/node/src/config.js
@@ -1,3 +1,5 @@
+const shared = require('./shared')
+
 /**
  * @class
  * Chain Core can be configured as a new blockchain network, or as a node in an
@@ -19,8 +21,13 @@ class Config {
      * @param {Boolean} everything - If `true`, all objects including access tokens and
      *                               MockHSM keys will be deleted. If `false`, then access tokens
      *                               and MockHSM keys will be preserved.
+     * @param {objectCallback} [callback] - Optional callback. Use instead of Promise return value as desired.
+     * @returns {Promise<Object>} Status of reset request.
      */
-    this.reset = (everything = false) => client.request('/reset', {everything: everything})
+    this.reset = (everything = false, cb) => shared.tryCallback(
+      client.request('/reset', {everything: everything}),
+      cb
+    )
 
     /**
      * Configure specified Chain Core.
@@ -29,22 +36,31 @@ class Config {
      * @param {Boolean} opts.isGenerator - Whether the local core will be a block generator
      *                                      for the blockchain; i.e., you are starting a new blockchain on
      *                                      the local core. `false` if you are connecting to a
-     *                                      pre-existing blockchain..
+     *                                      pre-existing blockchain.
      * @param {string} opts.generatorUrl - A URL for the block generator. Required if
      *                                      `isGenerator` is false.
      * @param {string} opts.generatorAccessToken - A network access token provided by administrators
      *                                               of the block generator. Required if `isGenerator` is false.
      * @param {string} opts.blockchainId - The unique ID of the generator's blockchain.
      *                                      Required if `isGenerator` is false.
+     * @param {objectCallback} [callback] - Optional callback. Use instead of Promise return value as desired.
+     * @returns {Promise<Object>} Status of configuration request.
      */
-    this.configure = (opts = {}) => client.request('/configure', opts)
+    this.configure = (opts = {}, cb) => shared.tryCallback(
+      client.request('/configure', opts),
+      cb
+    )
 
     /**
      * Get info on specified Chain Core.
      *
-     * @returns {Promise} Requested info of specified Chain Core.
+     * @param {objectCallback} [callback] - Optional callback. Use instead of Promise return value as desired.
+     * @returns {Promise<Object>} Requested info of specified Chain Core.
      */
-    this.info = () => client.request('/info')
+    this.info = (cb) => shared.tryCallback(
+      client.request('/info'),
+      cb
+    )
   }
 }
 
