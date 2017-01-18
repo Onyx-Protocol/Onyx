hhmingw/chain

fixing up outputids and unspentids

core/migrate/data.go
@@ -75,12 +75,12 @@ var migrations = []migration{
 	{Name: "2017-01-05.0.core.rename_block_key.sql", SQL: `
 		ALTER TABLE config RENAME COLUMN block_xpub TO block_pub;
 	`},
-	{Name: "2017-01-10.0.core.add-outputid-to-outputs.sql", SQL: `
+	{Name: "2017-01-10.0.core.add-output-id-to-outputs.sql", SQL: `
 		ALTER TABLE annotated_outputs ADD COLUMN output_id text NOT NULL;
 		ALTER TABLE ONLY annotated_outputs ADD CONSTRAINT annotated_outputs_unique_output_id UNIQUE (output_id);
-	`},
-	{Name: "2017-01-10.1.core.add-outputid-to-utxos.sql", SQL: `
 		ALTER TABLE account_utxos ADD COLUMN output_id text NOT NULL;
 		ALTER TABLE ONLY account_utxos ADD CONSTRAINT account_utxos_unique_output_id UNIQUE (output_id);
+		ALTER TABLE account_utxos ADD COLUMN unspent_id text NOT NULL;
+		ALTER TABLE ONLY account_utxos ADD CONSTRAINT account_utxos_unique_unspent_id UNIQUE (unspent_id);
 	`},
 }

core/query/index.go
@@ -111,15 +111,13 @@ func (ind *Indexer) insertAnnotatedOutputs(ctx context.Context, b *bc.Block, ann
 		outputIndexes     pg.Uint32s
 		outputTxHashes    pq.StringArray
 		outputData        pq.StringArray
-		prevoutHashes     pq.StringArray
-		prevoutIndexes    pg.Uint32s
+		prevoutOIDs       pq.StringArray
 	)
 
 	for pos, tx := range b.Transactions {
 		for _, in := range tx.Inputs {
 			if !in.IsIssuance() {
-				prevoutHashes = append(prevoutHashes, in.Outpoint().Hash.String())
-				prevoutIndexes = append(prevoutIndexes, in.Outpoint().Index)
+				prevoutOIDs = append(prevoutOIDs, in.OutputID().String())
 			}
 		}
 
@@ -172,8 +170,8 @@ func (ind *Indexer) insertAnnotatedOutputs(ctx context.Context, b *bc.Block, ann
 
 	const updateQ = `
 		UPDATE annotated_outputs SET timespan = INT8RANGE(LOWER(timespan), $1)
-		WHERE (tx_hash, output_index) IN (SELECT unnest($2::text[]), unnest($3::integer[]))
+		WHERE (output_id) IN (SELECT unnest($2::text[]))
 	`
-	_, err = ind.db.Exec(ctx, updateQ, b.TimestampMS, prevoutHashes, prevoutIndexes)
+	_, err = ind.db.Exec(ctx, updateQ, b.TimestampMS, prevoutOIDs)
 	return errors.Wrap(err, "updating spent annotated outputs")
 }

Onyx-Protocol/Onyx

core/migrate/data.go
@@ -104,12 +104,12 @@ var migrations = []migration{
 			ALTER COLUMN definition SET NOT NULL;
 		ALTER TABLE assets ADD COLUMN vm_version bigint NOT NULL;
 	`},
-	{Name: "2017-01-10.0.core.add-outputid-to-outputs.sql", SQL: `
-		ALTER TABLE annotated_outputs ADD COLUMN output_id text NOT NULL;
+	{Name: "2017-01-10.0.core.add-output-id-to-outputs.sql", SQL: `
+		ALTER TABLE annotated_outputs ADD COLUMN output_id bytea NOT NULL;
 		ALTER TABLE ONLY annotated_outputs ADD CONSTRAINT annotated_outputs_unique_output_id UNIQUE (output_id);
-	`},
-	{Name: "2017-01-10.1.core.add-outputid-to-utxos.sql", SQL: `
-		ALTER TABLE account_utxos ADD COLUMN output_id text NOT NULL;
+		ALTER TABLE account_utxos ADD COLUMN output_id bytea NOT NULL;
 		ALTER TABLE ONLY account_utxos ADD CONSTRAINT account_utxos_unique_output_id UNIQUE (output_id);
+		ALTER TABLE account_utxos ADD COLUMN unspent_id bytea NOT NULL;
+		ALTER TABLE ONLY account_utxos ADD CONSTRAINT account_utxos_unique_unspent_id UNIQUE (unspent_id);
 	`},
 }

core/query/index.go
@@ -111,16 +111,14 @@ func (ind *Indexer) insertAnnotatedOutputs(ctx context.Context, b *bc.Block, ann
 		outputIndexes     pg.Uint32s
 		outputTxHashes    pq.ByteaArray
 		outputData        pq.StringArray
-		prevoutHashes     pq.ByteaArray
-		prevoutIndexes    pg.Uint32s
+		prevoutOIDs       pq.ByteaArray
 	)
 
 	for pos, tx := range b.Transactions {
 		for _, in := range tx.Inputs {
 			if !in.IsIssuance() {
-				outpoint := in.Outpoint()
-				prevoutHashes = append(prevoutHashes, outpoint.Hash[:])
-				prevoutIndexes = append(prevoutIndexes, outpoint.Index)
+				oid := in.OutputID()
+				prevoutOIDs = append(prevoutOIDs, oid[:])
 			}
 		}
 
@@ -173,8 +171,8 @@ func (ind *Indexer) insertAnnotatedOutputs(ctx context.Context, b *bc.Block, ann
 
 	const updateQ = `
 		UPDATE annotated_outputs SET timespan = INT8RANGE(LOWER(timespan), $1)
-		WHERE (tx_hash, output_index) IN (SELECT unnest($2::bytea[]), unnest($3::integer[]))
+		WHERE (output_id) IN (SELECT unnest($2::bytea[]))
 	`
-	_, err = ind.db.Exec(ctx, updateQ, b.TimestampMS, prevoutHashes, prevoutIndexes)
+	_, err = ind.db.Exec(ctx, updateQ, b.TimestampMS, prevoutOIDs)
 	return errors.Wrap(err, "updating spent annotated outputs")
 }