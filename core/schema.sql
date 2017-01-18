0044a93398fe65c5a651295e7d18e928319dead5
Oleg Andreev <oleganza@gmail.com>
2017-01-18 11:55:50 -0800

    removed PG9.6-only `idle_in_transaction_session_timeout` statement from schema

--- core/schema.sql
+++ core/schema.sql
@@ -7,7 +7,6 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
