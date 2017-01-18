a35c3f392584c00681b1ed97d82d1c97b456d728
Tess Rinearson <tess.rinearson@gmail.com>
2017-01-17 16:10:48 -0800

    protocol/bc: move OutputCommitment into its own file
    Part of #349. 
    
    Note that this simply splits out `OutputCommitment` without making some of
    the other changes to the output commitment that are included in 349.
    
    Closes #397

---
+++ protocol/bc/outputcommitment.go
@@ -0,0 +1,66 @@
+package bc
+
+import (
+	"fmt"
+	"io"
+
+	"chain/encoding/blockchain"
+	"chain/encoding/bufpool"
+	"chain/errors"
+)
+
+// OutputCommitment contains the commitment data for a transaction
+// output (which also appears in the spend input of that output).
+type OutputCommitment struct {
+	AssetAmount
+	VMVersion      uint64
+	ControlProgram []byte
+}
+
+func (oc *OutputCommitment) writeTo(w io.Writer, assetVersion uint64) (err error) {
+	b := bufpool.Get()
+	defer bufpool.Put(b)
+	if assetVersion == 1 {
+		err = oc.AssetAmount.writeTo(b)
+		if err != nil {
+			return errors.Wrap(err, "writing asset amount")
+		}
+
+		_, err = blockchain.WriteVarint63(b, oc.VMVersion)
+		if err != nil {
+			return errors.Wrap(err, "writing vm version")
+		}
+		_, err = blockchain.WriteVarstr31(b, oc.ControlProgram)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = blockchain.WriteVarstr31(w, b.Bytes())
+	return errors.Wrap(err, "writing control program")
+}
+
+func (oc *OutputCommitment) readFrom(r io.Reader, txVersion, assetVersion uint64) (n int, err error) {
+	if assetVersion != 1 {
+		return n, fmt.Errorf("unrecognized asset version %d", assetVersion)
+	}
+	all := txVersion == 1
+	return blockchain.ReadExtensibleString(r, all, func(r io.Reader) error {
+		_, err := oc.AssetAmount.readFrom(r)
+		if err != nil {
+			return errors.Wrap(err, "reading asset+amount")
+		}
+
+		oc.VMVersion, _, err = blockchain.ReadVarint63(r)
+		if err != nil {
+			return errors.Wrap(err, "reading VM version")
+		}
+
+		if oc.VMVersion != 1 {
+			return fmt.Errorf("unrecognized VM version %d for asset version 1", oc.VMVersion)
+		}
+
+		oc.ControlProgram, _, err = blockchain.ReadVarstr31(r)
+		return errors.Wrap(err, "reading control program")
+	})
+}
--- protocol/bc/txoutput.go
+++ protocol/bc/txoutput.go
@@ -1,30 +1,20 @@
 package bc
 
 import (
-	"fmt"
 	"io"
 
 	"chain/encoding/blockchain"
-	"chain/encoding/bufpool"
 	"chain/errors"
 )
 
 // TODO(bobg): Review serialization/deserialization logic for
 // assetVersions other than 1.
 
-type (
-	TxOutput struct {
-		AssetVersion uint64
-		OutputCommitment
-		ReferenceData []byte
-	}
-
-	OutputCommitment struct {
-		AssetAmount
-		VMVersion      uint64
-		ControlProgram []byte
-	}
-)
+type TxOutput struct {
+	AssetVersion uint64
+	OutputCommitment
+	ReferenceData []byte
+}
 
 func NewTxOutput(assetID AssetID, amount uint64, controlProgram, referenceData []byte) *TxOutput {
 	return &TxOutput{
@@ -64,31 +54,6 @@ func (to *TxOutput) readFrom(r io.Reader, txVersion uint64) (err error) {
 	return errors.Wrap(err, "reading output witness")
 }
 
-func (oc *OutputCommitment) readFrom(r io.Reader, txVersion, assetVersion uint64) (n int, err error) {
-	if assetVersion != 1 {
-		return n, fmt.Errorf("unrecognized asset version %d", assetVersion)
-	}
-	all := txVersion == 1
-	return blockchain.ReadExtensibleString(r, all, func(r io.Reader) error {
-		_, err := oc.AssetAmount.readFrom(r)
-		if err != nil {
-			return errors.Wrap(err, "reading asset+amount")
-		}
-
-		oc.VMVersion, _, err = blockchain.ReadVarint63(r)
-		if err != nil {
-			return errors.Wrap(err, "reading VM version")
-		}
-
-		if oc.VMVersion != 1 {
-			return fmt.Errorf("unrecognized VM version %d for asset version 1", oc.VMVersion)
-		}
-
-		oc.ControlProgram, _, err = blockchain.ReadVarstr31(r)
-		return errors.Wrap(err, "reading control program")
-	})
-}
-
 // assumes r has sticky errors
 func (to *TxOutput) writeTo(w io.Writer, serflags byte) {
 	blockchain.WriteVarint63(w, to.AssetVersion) // TODO(bobg): check and return error
@@ -103,25 +68,4 @@ func (to *TxOutput) witnessHash() Hash {
 
 func (to *TxOutput) WriteCommitment(w io.Writer) {
 	to.OutputCommitment.writeTo(w, to.AssetVersion)
-}
-
-func (oc *OutputCommitment) writeTo(w io.Writer, assetVersion uint64) (err error) {
-	b := bufpool.Get()
-	defer bufpool.Put(b)
-	if assetVersion == 1 {
-		err = oc.AssetAmount.writeTo(b)
-		if err != nil {
-			return err
-		}
-		_, err = blockchain.WriteVarint63(b, oc.VMVersion)
-		if err != nil {
-			return err
-		}
-		_, err = blockchain.WriteVarstr31(b, oc.ControlProgram)
-		if err != nil {
-			return err
-		}
-	}
-	_, err = blockchain.WriteVarstr31(w, b.Bytes())
-	return err
 }
