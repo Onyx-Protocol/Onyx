se77en/chain

Reorganize and refactor TxData, TxInput, TxOutput, and related types in anticipation of future changes. Factor out common streaming operations to (Read|Write)(VarstrList|ExtensibleString). Fix serialization errors and update test vectors.
Closes https://github.com/chain/chain/issues/330.
Closes https://github.com/chain/chain/issues/335.

protocol/bc/issuance.go
@@ -1,0 +1,47 @@
+package bc
+
+import (
+	"io"
+
+	"chain/encoding/blockchain"
+	"chain/errors"
+)
+
+type IssuanceInput struct {
+	// Commitment
+	Nonce  []byte
+	Amount uint64
+	// Note: as long as we require serflags=0x7, we don't need to
+	// explicitly store the asset ID here even though it's technically
+	// part of the input commitment. We can compute it instead from
+	// values in the witness (which, with serflags other than 0x7,
+	// might not be present).
+
+	// Witness
+	AssetWitness
+}
+
+func (ii *IssuanceInput) IsIssuance() bool { return true }
+
+func (ii *IssuanceInput) AssetID() AssetID {
+	return ComputeAssetID(ii.IssuanceProgram, ii.InitialBlock, ii.VMVersion)
+}
+
+func (ii1 *IssuanceInput) readCommitment(r io.Reader) (assetID AssetID, err error) {
+	ii1.Nonce, _, err = blockchain.ReadVarstr31(r)
+	if err != nil {
+		return assetID, errors.Wrap(err, "reading nonce")
+	}
+
+	_, err = io.ReadFull(r, assetID[:])
+	if err != nil {
+		return assetID, errors.Wrap(err, "reading asset ID")
+	}
+
+	ii1.Amount, _, err = blockchain.ReadVarint63(r)
+	return assetID, errors.Wrap(err, "reading amount")
+}
+
+func (ii1 *IssuanceInput) readWitness(r io.Reader, assetVersion uint64) error {
+	return ii1.AssetWitness.readFrom(r, assetVersion)
+}

Onyx-Protocol/Onyx

protocol/bc/issuance.go
@@ -30,28 +30,28 @@ func (ii *IssuanceInput) AssetDefinitionHash() (defhash Hash) {
 	return
 }
 
-func NewIssuanceInput(
-	nonce []byte,
-	amount uint64,
-	referenceData []byte,
-	initialBlock Hash,
-	issuanceProgram []byte,
-	arguments [][]byte,
-	assetDefinition []byte,
-) *TxInput {
-	return &TxInput{
-		AssetVersion:  1,
-		ReferenceData: referenceData,
-		TypedInput: &IssuanceInput{
-			Nonce:  nonce,
-			Amount: amount,
-			IssuanceWitness: IssuanceWitness{
-				InitialBlock:    initialBlock,
-				AssetDefinition: assetDefinition,
-				VMVersion:       1,
-				IssuanceProgram: issuanceProgram,
-				Arguments:       arguments,
-			},
-		},
+
+func (ii *IssuanceInput) IsIssuance() bool { return true }
+
+func (ii *IssuanceInput) AssetID() AssetID {
+	return ComputeAssetID(ii.IssuanceProgram, ii.InitialBlock, ii.VMVersion)
+}
+
+func (ii *IssuanceInput) readCommitment(r io.Reader) (assetID AssetID, err error) {
+	ii.Nonce, _, err = blockchain.ReadVarstr31(r)
+	if err != nil {
+		return assetID, errors.Wrap(err, "reading nonce")
 	}
+
+	_, err = io.ReadFull(r, assetID[:])
+	if err != nil {
+		return assetID, errors.Wrap(err, "reading asset ID")
+	}
+
+	ii.Amount, _, err = blockchain.ReadVarint63(r)
+	return assetID, errors.Wrap(err, "reading amount")
+}
+
+func (ii *IssuanceInput) readWitness(r io.Reader, assetVersion uint64) error {
+	return ii.IssuanceWitness.readFrom(r, assetVersion)
 }