e2a45f1bdc0789c77b5ae85e82dff6f25a9423e9
Eric Rykwalder <e.rykwalder@gmail.com>
2017-01-19 11:36:48 -0800

    core: alphabetize query.go
    Simple cut and paste change to make navigating the file a little easier.

--- core/query.go
+++ core/query.go
@@ -11,6 +11,133 @@ import (
 	"chain/net/http/httpjson"
 )
 
+// listAccounts is an http handler for listing accounts matching
+// an index or an ad-hoc filter.
+//
+// POST /list-accounts
+func (h *Handler) listAccounts(ctx context.Context, in requestQuery) (page, error) {
+	limit := in.PageSize
+	if limit == 0 {
+		limit = defGenericPageSize
+	}
+
+	// Build the filter predicate.
+	p, err := filter.Parse(in.Filter)
+	if err != nil {
+		return page{}, errors.Wrap(err, "parsing acc query")
+	}
+	after := in.After
+
+	// Use the filter engine for querying account tags.
+	accounts, after, err := h.Indexer.Accounts(ctx, p, in.FilterParams, after, limit)
+	if err != nil {
+		return page{}, errors.Wrap(err, "running acc query")
+	}
+
+	result := make([]*accountResponse, 0, len(accounts))
+	for _, a := range accounts {
+		var r accountResponse
+		err := json.Unmarshal(a, &r)
+		if err != nil {
+			return page{}, errors.Wrap(err, "unmarshaling stored account")
+		}
+		result = append(result, &r)
+	}
+
+	// Pull in the accounts by the IDs
+	out := in
+	out.After = after
+	return page{
+		Items:    httpjson.Array(result),
+		LastPage: len(result) < limit,
+		Next:     out,
+	}, nil
+}
+
+// listAssets is an http handler for listing assets matching
+// an index or an ad-hoc filter.
+//
+// POST /list-assets
+func (h *Handler) listAssets(ctx context.Context, in requestQuery) (page, error) {
+	limit := in.PageSize
+	if limit == 0 {
+		limit = defGenericPageSize
+	}
+
+	// Build the filter predicate.
+	p, err := filter.Parse(in.Filter)
+	if err != nil {
+		return page{}, err
+	}
+	after := in.After
+
+	// Use the query engine for querying asset tags.
+	assets, after, err := h.Indexer.Assets(ctx, p, in.FilterParams, after, limit)
+	if err != nil {
+		return page{}, errors.Wrap(err, "running asset query")
+	}
+
+	result := make([]*assetResponse, 0, len(assets))
+	for _, a := range assets {
+		var r assetResponse
+		err := json.Unmarshal(a, &r)
+		if err != nil {
+			return page{}, errors.Wrap(err, "unmarshaling stored asset")
+		}
+		result = append(result, &r)
+	}
+
+	out := in
+	out.After = after
+	return page{
+		Items:    httpjson.Array(result),
+		LastPage: len(result) < limit,
+		Next:     out,
+	}, nil
+}
+
+// POST /list-balances
+func (h *Handler) listBalances(ctx context.Context, in requestQuery) (result page, err error) {
+	var p filter.Predicate
+	var sumBy []filter.Field
+	p, err = filter.Parse(in.Filter)
+	if err != nil {
+		return result, err
+	}
+
+	// Since an empty SumBy yields a meaningless result, we'll provide a
+	// sensible default here.
+	if len(in.SumBy) == 0 {
+		in.SumBy = []string{"asset_alias", "asset_id"}
+	}
+
+	for _, field := range in.SumBy {
+		f, err := filter.ParseField(field)
+		if err != nil {
+			return result, err
+		}
+		sumBy = append(sumBy, f)
+	}
+
+	timestampMS := in.TimestampMS
+	if timestampMS == 0 {
+		timestampMS = math.MaxInt64
+	} else if timestampMS > math.MaxInt64 {
+		return result, errors.WithDetail(httpjson.ErrBadRequest, "timestamp is too large")
+	}
+
+	// TODO(jackson): paginate this endpoint.
+	balances, err := h.Indexer.Balances(ctx, p, in.FilterParams, sumBy, timestampMS)
+	if err != nil {
+		return result, err
+	}
+
+	result.Items = httpjson.Array(balances)
+	result.LastPage = true
+	result.Next = in
+	return result, nil
+}
+
 // listTransactions is an http handler for listing transactions matching
 // an index or an ad-hoc filter.
 //
@@ -71,89 +198,29 @@ func (h *Handler) listTransactions(ctx context.Context, in requestQuery) (result
 	}, nil
 }
 
-// listAccounts is an http handler for listing accounts matching
-// an index or an ad-hoc filter.
+// listTxFeeds is an http handler for listing txfeeds. It does not take a filter.
 //
-// POST /list-accounts
-func (h *Handler) listAccounts(ctx context.Context, in requestQuery) (page, error) {
+// POST /list-transaction-feeds
+func (h *Handler) listTxFeeds(ctx context.Context, in requestQuery) (page, error) {
 	limit := in.PageSize
 	if limit == 0 {
 		limit = defGenericPageSize
 	}
 
-	// Build the filter predicate.
-	p, err := filter.Parse(in.Filter)
-	if err != nil {
-		return page{}, errors.Wrap(err, "parsing acc query")
-	}
 	after := in.After
 
-	// Use the filter engine for querying account tags.
-	accounts, after, err := h.Indexer.Accounts(ctx, p, in.FilterParams, after, limit)
+	txfeeds, after, err := h.Indexer.TxFeeds(ctx, after, limit)
 	if err != nil {
-		return page{}, errors.Wrap(err, "running acc query")
+		return page{}, errors.Wrap(err, "running txfeed query")
 	}
 
-	result := make([]*accountResponse, 0, len(accounts))
-	for _, a := range accounts {
-		var r accountResponse
-		err := json.Unmarshal(a, &r)
-		if err != nil {
-			return page{}, errors.Wrap(err, "unmarshaling stored account")
-		}
-		result = append(result, &r)
-	}
-
-	// Pull in the accounts by the IDs
 	out := in
 	out.After = after
 	return page{
-		Items:    httpjson.Array(result),
-		LastPage: len(result) < limit,
+		Items:    httpjson.Array(txfeeds),
+		LastPage: len(txfeeds) < limit,
 		Next:     out,
 	}, nil
-}
-
-// POST /list-balances
-func (h *Handler) listBalances(ctx context.Context, in requestQuery) (result page, err error) {
-	var p filter.Predicate
-	var sumBy []filter.Field
-	p, err = filter.Parse(in.Filter)
-	if err != nil {
-		return result, err
-	}
-
-	// Since an empty SumBy yields a meaningless result, we'll provide a
-	// sensible default here.
-	if len(in.SumBy) == 0 {
-		in.SumBy = []string{"asset_alias", "asset_id"}
-	}
-
-	for _, field := range in.SumBy {
-		f, err := filter.ParseField(field)
-		if err != nil {
-			return result, err
-		}
-		sumBy = append(sumBy, f)
-	}
-
-	timestampMS := in.TimestampMS
-	if timestampMS == 0 {
-		timestampMS = math.MaxInt64
-	} else if timestampMS > math.MaxInt64 {
-		return result, errors.WithDetail(httpjson.ErrBadRequest, "timestamp is too large")
-	}
-
-	// TODO(jackson): paginate this endpoint.
-	balances, err := h.Indexer.Balances(ctx, p, in.FilterParams, sumBy, timestampMS)
-	if err != nil {
-		return result, err
-	}
-
-	result.Items = httpjson.Array(balances)
-	result.LastPage = true
-	result.Next = in
-	return result, nil
 }
 
 // POST /list-unspent-outputs
@@ -195,72 +262,5 @@ func (h *Handler) listUnspentOutputs(ctx context.Context, in requestQuery) (resu
 		Items:    httpjson.Array(outputs),
 		LastPage: len(outputs) < limit,
 		Next:     outQuery,
-	}, nil
-}
-
-// listAssets is an http handler for listing assets matching
-// an index or an ad-hoc filter.
-//
-// POST /list-assets
-func (h *Handler) listAssets(ctx context.Context, in requestQuery) (page, error) {
-	limit := in.PageSize
-	if limit == 0 {
-		limit = defGenericPageSize
-	}
-
-	// Build the filter predicate.
-	p, err := filter.Parse(in.Filter)
-	if err != nil {
-		return page{}, err
-	}
-	after := in.After
-
-	// Use the query engine for querying asset tags.
-	assets, after, err := h.Indexer.Assets(ctx, p, in.FilterParams, after, limit)
-	if err != nil {
-		return page{}, errors.Wrap(err, "running asset query")
-	}
-
-	result := make([]*assetResponse, 0, len(assets))
-	for _, a := range assets {
-		var r assetResponse
-		err := json.Unmarshal(a, &r)
-		if err != nil {
-			return page{}, errors.Wrap(err, "unmarshaling stored asset")
-		}
-		result = append(result, &r)
-	}
-
-	out := in
-	out.After = after
-	return page{
-		Items:    httpjson.Array(result),
-		LastPage: len(result) < limit,
-		Next:     out,
-	}, nil
-}
-
-// listTxFeeds is an http handler for listing txfeeds. It does not take a filter.
-//
-// POST /list-transaction-feeds
-func (h *Handler) listTxFeeds(ctx context.Context, in requestQuery) (page, error) {
-	limit := in.PageSize
-	if limit == 0 {
-		limit = defGenericPageSize
-	}
-
-	after := in.After
-
-	txfeeds, after, err := h.Indexer.TxFeeds(ctx, after, limit)
-	if err != nil {
-		return page{}, errors.Wrap(err, "running txfeed query")
-	}
-
-	out := in
-	out.After = after
-	return page{
-		Items:    httpjson.Array(txfeeds),
-		LastPage: len(txfeeds) < limit,
-		Next:     out,
 	}, nil
 }
