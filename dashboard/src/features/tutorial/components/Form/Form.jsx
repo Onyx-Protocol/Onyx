a1467d515a2cd9953817357ab2e70c273825d068
Chris Garvin <chris.garvin@me.com>
2017-01-20 16:39:27 -0800

    form styling resizes upon screen collapse

--- dashboard/src/features/tutorial/components/Form/Form.jsx
+++ dashboard/src/features/tutorial/components/Form/Form.jsx
@@ -8,44 +8,46 @@ class Form extends React.Component {
 
     return (
       <div className={styles.container}>
-        <div className={styles.header}>
-          {this.props.title}
-          <div className={styles.skip}>
-            <a onClick={this.props.handleDismiss}>End tutorial</a>
+        <div className={styles.tutorialContainer}>
+          <div className={styles.header}>
+            {this.props.title}
+            <div className={styles.skip}>
+              <a onClick={this.props.handleDismiss}>End tutorial</a>
+            </div>
           </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.listHeader}>
-            {this.props.content['header']}
-          </div>
-          <table className={styles.listItemContainer}>
-            <tbody>
-            {this.props.content['steps'].map(function (x, i){
-              let str = x['title']
-              
-              if (x['type']) {
-                if (x['type'] == 'account'){
-                  str = x['title'].replace('STRING', userInput['accounts'][x['index']]['alias'])
-                } else {
-                  str = x['title'].replace('STRING', userInput[x['type']]['alias'])
+          <div className={styles.list}>
+            <div className={styles.listHeader}>
+              {this.props.content['header']}
+            </div>
+            <table className={styles.listItemContainer}>
+              <tbody>
+              {this.props.content['steps'].map(function (x, i){
+                let str = x['title']
+
+                if (x['type']) {
+                  if (x['type'] == 'account'){
+                    str = x['title'].replace('STRING', userInput['accounts'][x['index']]['alias'])
+                  } else {
+                    str = x['title'].replace('STRING', userInput[x['type']]['alias'])
+                  }
                 }
-              }
-              let rows = [
-                <tr className={styles.listItem} key={i}>
-                  <td className={styles.listBullet}>{i+1}</td>
-                  <td>{str}</td>
-                </tr>
-              ]
-              if (x['description']) {
-                rows.push (<tr className={styles.listItemDescription}>
-                  <td></td>
-                  <td key={i}>{x['description']}</td>
-                </tr>)
-              }
-              return rows
-            })}
-            </tbody>
-          </table>
+                let rows = [
+                  <tr className={styles.listItem} key={i}>
+                    <td className={styles.listBullet}>{i+1}</td>
+                    <td>{str}</td>
+                  </tr>
+                ]
+                if (x['description']) {
+                  rows.push (<tr className={styles.listItemDescription}>
+                    <td></td>
+                    <td key={i}>{x['description']}</td>
+                  </tr>)
+                }
+                return rows
+              })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     )
