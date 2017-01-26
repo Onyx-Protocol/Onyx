11b3ad6962078644bc5ca0510f78d292cdba6780
Keith Rarick <kr@xph.us>
2017-01-26 11:30:24 -0800

    data entry is a hash of the data

--- protocol/tx/data.go
+++ protocol/tx/data.go
@@ -1,15 +1,25 @@
 package tx
 
+import (
+	"chain/crypto/sha3pool"
+	"chain/protocol/bc"
+)
+
 type data struct {
-	body    []byte
-	extHash extHash
+	body bc.Hash
 }
 
 func (data) Type() string         { return "data1" }
 func (d *data) Body() interface{} { return d.body }
 
-func newData(refData []byte) entry {
+func newData(hash bc.Hash) entry {
 	d := new(data)
-	d.body = refData
+	d.body = hash
 	return d
+}
+
+func hashData(data []byte) (h bc.Hash) {
+	// TODO(kr): domain separation here? spec might need updating
+	sha3pool.Sum256(h[:], data)
+	return
 }
