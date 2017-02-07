2ef4b6197392c17961e7aa98ea7b4bdeb0813b43
Jackson Owens <jackson_owens@alumni.brown.edu>
2017-02-06 16:07:16 -0800

    core/txfeed: move txfeed query from core/query
    Move the list txfeeds query logic from the `core/query` package to
    the `core/txfeed` package. The txfeeds query doesn't use filters, so it
    probably shouldn't be in that package. Also, the `core/query` dependency
    on `core/txfeed` makes it difficult to refactor the `core/query` and
    `core/query/filter` packages without introducing a circular dependency.
    
    Closes #476

---
+++ core/txfeed/query.go
@@ -0,0 +1,45 @@
+package txfeed
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"chain/errors"
+)
+
+// Query queries the Chain Core for txfeeds matching the query.
+func (t *Tracker) Query(ctx context.Context, after string, limit int) ([]*TxFeed, string, error) {
+	const baseQ = `
+		SELECT id, alias, filter, after FROM txfeeds
+		WHERE ($1='' OR id < $1) ORDER BY id DESC LIMIT %d
+	`
+	rows, err := t.DB.Query(ctx, fmt.Sprintf(baseQ, limit), after)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "executing txfeeds query")
+	}
+	defer rows.Close()
+
+	txfeeds := make([]*TxFeed, 0, limit)
+	for rows.Next() {
+		var (
+			feed  TxFeed
+			alias sql.NullString
+		)
+		err := rows.Scan(&feed.ID, &alias, &feed.Filter, &feed.After)
+		if err != nil {
+			return nil, "", errors.Wrap(err, "scanning txfeed row")
+		}
+
+		if alias.Valid {
+			feed.Alias = &alias.String
+		}
+		after = feed.ID
+		txfeeds = append(txfeeds, &feed)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, "", errors.Wrap(err)
+	}
+	return txfeeds, after, nil
+}
