dfc3f4110f848f1eb1940338cf7fd58030cc3324
Dominic Dagradi <dominic@chain.com>
2017-02-07 18:02:32 -0800

    Navbar -> 🚮

--- dashboard/src/components/Common/index.js
+++ dashboard/src/components/Common/index.js
@@ -1,15 +1,11 @@
-// import Autocomplete from './Autocomplete'
 import JsonField from './JsonField/JsonField'
 import KeyConfiguration from './KeyConfiguration'
-import Navbar from './Navbar'
 import ObjectSelectorField from './ObjectSelectorField/ObjectSelectorField'
 import TextField from './TextField/TextField'
 
 export {
-  // Autocomplete,
   JsonField,
   KeyConfiguration,
-  Navbar,
   ObjectSelectorField,
   TextField,
 }
