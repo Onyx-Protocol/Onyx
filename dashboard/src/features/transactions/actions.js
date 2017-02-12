b0b23c6f1061007c90b395b4bf3c2d849c2d936b
Dominic Dagradi <dominic@linebreak.co>
2017-02-11 17:56:05 -0800

    convert transactions to new SDK

--- dashboard/src/features/transactions/actions.js
+++ dashboard/src/features/transactions/actions.js
@@ -1,6 +1,5 @@
 import uuid from 'uuid'
-import chain from '_chain'
-import { context } from 'utility/environment'
+import { chainClient, chainSigner } from 'utility/environment'
 import { parseNonblankJSON } from 'utility/string'
 import { push } from 'react-router-redux'
 import { baseCreateActions, baseListActions } from 'features/shared/actions'
@@ -15,12 +14,12 @@ const form = baseCreateActions(type)
 function preprocessTransaction(formParams) {
   const copy = JSON.parse(JSON.stringify(formParams))
   const builder = {
-    base_transaction: copy.base_transaction,
+    baseTransaction: copy.base_transaction,
     actions: copy.actions,
   }
 
-  if (builder.base_transaction == '') {
-    delete builder.base_transaction
+  if (builder.baseTransaction == '') {
+    delete builder.baseTransaction
   }
 
   if (formParams.submit_action == 'generate') {
@@ -46,7 +45,7 @@ function preprocessTransaction(formParams) {
     // TODO: update JS SDK to support Java SDK builder style.
     if (a.type == 'retire_asset') {
       a.type = 'control_program'
-      a.control_program = '6a' // OP_FAIL hex byte
+      a.controlProgram = '6a' // OP_FAIL hex byte
     }
 
     try {
@@ -61,8 +60,8 @@ function preprocessTransaction(formParams) {
 
 function getTemplateXpubs(tpl) {
   const xpubs = []
-  tpl.signing_instructions.forEach((instruction) => {
-    instruction.witness_components.forEach((component) => {
+  tpl.signingInstructions.forEach((instruction) => {
+    instruction.witnessComponents.forEach((component) => {
       component.keys.forEach((key) => {
         xpubs.push(key.xpub)
       })
@@ -72,19 +71,26 @@ function getTemplateXpubs(tpl) {
 }
 
 form.submitForm = (formParams) => function(dispatch) {
-  let builder
-  try {
-    builder = preprocessTransaction(formParams)
-  } catch (err) {
-    return Promise.reject(err)
-  }
+  const buildPromise = chainClient().transactions.build(builder => {
+    const processed = preprocessTransaction(formParams)
 
-  const build = new chain.Transaction(builder).build(context())
+    builder.actions = processed.actions
+    if (processed.baseTransaction) {
+      builder.baseTransaction = processed.baseTransaction
+    }
+  })
 
   if (formParams.submit_action == 'submit') {
-    return build
-      .then(tpl => chain.MockHsm.sign([tpl], getTemplateXpubs(tpl), context()))
-      .then(signed => signed[0].submit(context()))
+    return buildPromise
+      .then(tpl => {
+        const signer = chainSigner()
+
+        getTemplateXpubs(tpl).forEach(key => {
+          signer.addKey(key, chainClient().mockHsm.signerConnection)
+        })
+
+        return signer.sign(tpl)
+      }).then(signed => chainClient().transactions.submit(signed))
       .then(resp => {
         dispatch(form.created())
         dispatch(push({
@@ -97,19 +103,23 @@ form.submitForm = (formParams) => function(dispatch) {
   }
 
   // submit_action == 'generate'
-  return build
-    .then(tpl => chain.MockHsm.sign(
-      [{...tpl, allow_additional_actions: true}],
-      getTemplateXpubs(tpl),
-      context()
-    ))
+  return buildPromise
+    .then(tpl => {
+      const signer = chainSigner()
+
+      getTemplateXpubs(tpl).forEach(key => {
+        signer.addKey(key, chainClient().mockHsm.signerConnection)
+      })
+
+      return signer.sign({...tpl, allowAdditionalActions: true})
+    })
     .then(signed => {
       const id = uuid.v4()
       dispatch({
         type: 'GENERATED_TX_HEX',
         generated: {
           id: id,
-          hex: signed[0].raw_transaction,
+          hex: signed.rawTransaction,
         },
       })
       dispatch(push(`/transactions/generated/${id}`))
