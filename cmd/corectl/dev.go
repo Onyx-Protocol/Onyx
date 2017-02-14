ebebc29f40993a24dd98617dd1bff88c419a47b0
Eric Rykwalder <e.rykwalder@gmail.com>
2017-02-14 13:28:57 -0800

    cmd/corectl: disable key generation in prod
    This disables the create-block-keypair command when the prod build tag
    is used. Additionally, mockhsm is not compiled into the binary when the
    prod build tag is used.

--- cmd/corectl/dev.go
+++ cmd/corectl/dev.go
@@ -4,8 +4,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"chain/core/coreunsafe"
+	"chain/core/mockhsm"
 	"chain/database/sql"
 )
 
@@ -19,4 +21,19 @@ func reset(db *sql.DB, args []string) {
 	if err != nil {
 		fatalln("error:", err)
 	}
+}
+
+func createBlockKeyPair(db *sql.DB, args []string) {
+	if len(args) != 0 {
+		fatalln("error: create-block-keypair takes no args")
+	}
+
+	hsm := mockhsm.New(db)
+	ctx := context.Background()
+	pub, err := hsm.Create(ctx, "block_key")
+	if err != nil {
+		fatalln("error:", err)
+	}
+
+	fmt.Printf("%x\n", pub.Pub)
 }
--- cmd/corectl/prod.go
+++ cmd/corectl/prod.go
@@ -7,3 +7,7 @@ import "chain/database/sql"
 func reset(db *sql.DB, args []string) {
 	fatalln("error: reset disabled in prod build")
 }
+
+func createBlockKeyPair(db *sql.DB, args []string) {
+	fatalln("error: create-block-keypair disabled in prod build")
+}
