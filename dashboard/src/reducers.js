a07c104f3fddc9ab73a3c2d672dab702d186264e
Jeff Lee <jeffomatic@gmail.com>
2017-02-16 10:47:57 -0800

    camelize intermediate var

--- dashboard/src/reducers.js
+++ dashboard/src/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing} from 'react-router-redux'
 import { reducer as form } from 'redux-form'
-import { reducers as access_token } from 'features/accessTokens'
+import { reducers as accessToken } from 'features/accessTokens'
 import { reducers as account } from 'features/accounts'
 import { reducers as app } from 'features/app'
 import { reducers as asset } from 'features/assets'
@@ -44,7 +44,7 @@ const makeRootReducer = () => (state, action) => {
   }
 
   return combineReducers({
-    ...access_token,
+    ...accessToken,
     account,
     app,
     asset,
