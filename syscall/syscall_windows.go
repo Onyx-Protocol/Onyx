a2b8f33a3375edc5c0af42ea0d0beb180df5106d
Keith Rarick <kr@xph.us>
2017-03-15 17:31:02 -0700

    this is dead code now

--- syscall/syscall_windows.go
+++
@@ -1,5 +0,0 @@
-package program
-
-//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go syscall_windows.go
-
-//sys	GetModuleFileName(hmodule uint32, buf *uint16, buflen uint32) (n uint32, err error) = GetModuleFileNameW
