35080e166891231be15e31c6ebf4604f1506f171
Jackson Owens <jackson@chain.com>
2017-03-17 14:03:26 -0700

    avoid racy State() == Recovering check

--- core/leader/leader_test.go
+++ core/leader/leader_test.go
@@ -12,7 +12,6 @@ func TestFailover(t *testing.T) {
 	ctx := context.Background()
 	_, db := pgtest.NewDB(t, pgtest.SchemaPath)
 
-	var l1, l2 *Leader
 	var wg1, wg2 sync.WaitGroup
 	wg1.Add(1)
 	wg2.Add(1)
@@ -23,13 +22,7 @@ func TestFailover(t *testing.T) {
 
 	// Start up the first leader process. It should immediately become
 	// leader.
-	l1 = Run(ctx1, db, ":1999", func(_ context.Context) {
-		// Since the lead func hasn't completed yet, the leader
-		// state should still be 'recovering'.
-		if s := l1.State(); s != Recovering {
-			t.Errorf("for first process state, got %s want %s", s, Recovering)
-		}
-
+	l1 := Run(ctx1, db, ":1999", func(_ context.Context) {
 		t.Log("first process is now leader")
 		wg1.Done()
 	})
@@ -49,13 +42,7 @@ func TestFailover(t *testing.T) {
 	}
 
 	// Start up the second leader process. It should be following.
-	l2 = Run(ctx2, db, ":2000", func(_ context.Context) {
-		// When this process takes over leadership, it should be
-		// 'recovering'.
-		if s := l2.State(); s != Recovering {
-			t.Errorf("for second process state, got %s want %s", s, Recovering)
-		}
-
+	l2 := Run(ctx2, db, ":2000", func(_ context.Context) {
 		t.Log("second process is now leader")
 		wg2.Done()
 	})
