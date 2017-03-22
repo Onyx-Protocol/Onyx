ec0620332143341857cc24afb0606a7cb1e29028
Jackson Owens <jackson@chain.com>
2017-03-22 10:47:13 -0700

    move errors_test.go tests

--- net/http/httperror/httperror_test.go
+++ net/http/httperror/httperror_test.go
@@ -3,12 +3,52 @@ package httperror
 import (
 	"bytes"
 	"context"
-	"errors"
+	"fmt"
 	"strings"
 	"testing"
 
+	"chain/errors"
 	"chain/log"
 )
+
+var (
+	errNotFound   = errors.New("not found")
+	testFormatter = Formatter{
+		Default:     Info{500, "CH000", "Internal server error"},
+		IsTemporary: func(Info, error) bool { return false },
+		Errors: map[error]Info{
+			errNotFound: {400, "CH002", "Not found"},
+		},
+	}
+)
+
+// Dummy error type, to test that Format
+// doesn't panic when it's used as a map key.
+type sliceError []int
+
+func (err sliceError) Error() string { return "slice error" }
+
+func TestInfo(t *testing.T) {
+	cases := []struct {
+		err  error
+		want int
+	}{
+		{nil, 500},
+		{context.Canceled, 500},
+		{errNotFound, 400},
+		{errors.Wrap(errNotFound, "foo"), 400},
+		{sliceError{}, 500},
+		{fmt.Errorf("an error!"), 500},
+	}
+
+	for _, test := range cases {
+		_, info := testFormatter.Format(test.err)
+		got := info.HTTPStatus
+		if got != test.want {
+			t.Errorf("errInfo(%#v) = %d want %d", test.err, got, test.want)
+		}
+	}
+}
 
 func TestLogSkip(t *testing.T) {
 	var buf bytes.Buffer
