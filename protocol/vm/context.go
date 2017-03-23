4b58b743294401bc81321d9c9c8e36af49b09da0
bobg <bobg@chain.com>
2017-03-23 15:45:16 -0700

    Document the optional fields of a vm.Context.

--- protocol/vm/context.go
+++ protocol/vm/context.go
@@ -2,6 +2,11 @@ package vm
 
 // Context contains the execution context for the virtual machine.
 //
+// Most fields are pointers and are not required to be present in all
+// cases. A nil pointer means the value is absent in that context. If
+// an opcode executes that requires an absent field to be present, it
+// will return ErrContext.
+//
 // By convention, variables of this type have the name context, _not_
 // ctx (to avoid confusion with context.Context).
 type Context struct {
@@ -9,11 +14,18 @@ type Context struct {
 	Code      []byte
 	Arguments [][]byte
 
+	// TxVersion must be present when verifying transaction components
+	// (such as spends and issuances).
 	TxVersion *uint64
+
+	// These fields must be present when verifying block headers.
 
 	BlockHash            *[]byte
 	BlockTimeMS          *uint64
 	NextConsensusProgram *[]byte
+
+	// Fields below this point are required by particular opcodes when
+	// verifying transaction components.
 
 	TxSigHash        *[]byte
 	NumResults       *uint64
