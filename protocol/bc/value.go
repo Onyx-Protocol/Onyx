804610d96ff5e443387de616e3592da476c55452
Bob Glickstein <bobg@chain.com>
2017-03-24 00:05:55 -0700

    Document ValueSource.Entry a little more fully.

--- protocol/bc/value.go
+++ protocol/bc/value.go
@@ -5,7 +5,9 @@ type ValueSource struct {
 	Value    AssetAmount
 	Position uint64 // what int do we actually want?
 
-	// The Entry corresponding to Ref, if available
-	// The struct tag excludes the field from hashing
+	// The Entry corresponding to Ref, if available.  Under normal
+	// conditions, this field is always set except when the containing
+	// entry is an Output inside of a Spend (i.e., a prevout).
+	// The struct tag excludes the field from hashing.
 	Entry `entry:"-"`
 }
