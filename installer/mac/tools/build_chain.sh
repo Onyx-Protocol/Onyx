1d49846e77565f8b2adfee59cda4b5a732893247
Jeff Lee <jeffomatic@gmail.com>
2017-03-30 11:01:16 -0700

    installer/mac: update 1.0-stable with most recent 1.0.x Mac installer
    This commit ports the Mac app from tag installer.mac-1.0.2 to the
    1.0-stable branch. This is part of a series of commits that will
    ensure that the 1.0-stable branch contains the latest 1.0.x
    changes for all packages.
    
    Closes #822

--- installer/mac/tools/build_chain.sh
+++ installer/mac/tools/build_chain.sh
@@ -7,13 +7,9 @@ export CHAIN="${SOURCE_DIR}/../.."
 export GOPATH="${CHAIN}/../.."
 export PATH="${PATH}:/usr/local/go/bin"
 
-cd "${CHAIN}"
-go install -tags insecure_disable_https_redirect ./cmd/cored
-go install -tags insecure_disable_https_redirect ./cmd/corectl
+tempBuildPath=`mktemp -d`
+trap "rm -rf $tempBuildPath" EXIT
+"${CHAIN}/bin/build-cored-release" cmd.cored-1.0.2 $tempBuildPath
 
-cp -f "${GOPATH}/bin/cored" "${TARGET_DIR}/"
-cp -f "${GOPATH}/bin/corectl" "${TARGET_DIR}/"
-
-# `go build` is slower on repeated runs than `go install`
-# go build -o "${TARGET_DIR}/cored"   -tags insecure_disable_https_redirect "./cmd/cored"
-# go build -o "${TARGET_DIR}/corectl" -tags insecure_disable_https_redirect "./cmd/corectl"
+cp -f $tempBuildPath/cored "${TARGET_DIR}/"
+cp -f $tempBuildPath/corectl "${TARGET_DIR}/"
