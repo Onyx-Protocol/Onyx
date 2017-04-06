dfba51850f2c89b500a117a97922e330e4fac19a
Dominic Dagradi <ddagradi@gmail.com>
2017-04-05 18:05:07 -0700

    docs: improve version string parsing
    Previous regex relied on the presence of `/core` in the
    URL. This prevented the correct version from being detected
    on multiple paths.
    
    Backport of #883
    
    Closes #884

--- generated/rev/revid.go
+++ generated/rev/revid.go
@@ -1,3 +1,3 @@
 package rev
 
-const ID string = "1.1-stable/rev2759"
+const ID string = "1.1-stable/rev2760"
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "1.1-stable/rev2759".freeze
+	ID = "1.1-stable/rev2760".freeze
 end
