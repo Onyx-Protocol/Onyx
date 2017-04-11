948cb93a89dc1a121beef1f641aecd46feb53524
Jackson Owens <jackson@chain.com>
2017-04-11 15:25:12 -0700

    re-work to use explicit batching

--- analytics/src/main/java/com/chain/analytics/Importer.java
+++ analytics/src/main/java/com/chain/analytics/Importer.java
@@ -1,7 +1,10 @@
 package analytics;
 
 import com.chain.http.Client;
+import com.chain.api.PagedItems;
+import com.chain.api.Query;
 import com.chain.api.Transaction;
+import com.chain.api.Transaction.QueryBuilder;
 import com.chain.exception.APIException;
 import com.chain.exception.ChainException;
 import com.google.gson.Gson;
@@ -15,7 +18,9 @@ import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
 import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import javax.sql.DataSource;
 
 import org.apache.logging.log4j.Logger;
@@ -30,6 +35,7 @@ public class Importer {
 
     private static final String TRUE = "1";
     private static final String FALSE = "0";
+    private static final long defaultTimeoutMillis = 30 * 1000; // 30 seconds
 
     private static final Logger logger = LogManager.getLogger();
     private static final Gson gson = new Gson();
@@ -173,124 +179,167 @@ public class Importer {
      * TODO(jackson): Support propagating persistent errors?
      */
     public void process() {
-        final String insertTxQ = mTransactionsTbl.getInsertStatement();
-        final String insertInputQ = mTransactionInputsTbl.getInsertStatement();
-        final String insertOutputQ = mTransactionOutputsTbl.getInsertStatement();
+        ThreadContext.put("feed_id", mFeed.id);
+        ThreadContext.put("feed_filter", mFeed.filter);
 
-        // TODO(jackson): Batch insert transactions instead of inserting
-        // them individually. We'll need to circumvent the SDK's transaction
-        // feed interface.
         for (;;) {
-            try (Connection conn = mDataSource.getConnection()) {
-                // Manage our own SQL transactions so we can make this whole
-                // blockchain transaction atomic.
-                conn.setAutoCommit(false);
+            try {
+                ThreadContext.put("feed_after", mFeed.after);
 
-                final Transaction tx = mFeed.next(mChain);
-                ThreadContext.put("tx_id", tx.id);
-                logger.debug("Importing transaction {}", tx.id);
+                // Retrieve another page of transactions matching the feed.
+                final Transaction.Items page = new QueryBuilder()
+                    .setFilter(mFeed.filter)
+                    .setAfter(mFeed.after)
+                    .setTimeout(defaultTimeoutMillis)
+                    .setAscendingWithLongPoll()
+                    .execute(mChain);
 
-                // Insert the transaction itself.
-                try (PreparedStatement ps = conn.prepareStatement(insertTxQ)) {
-                    ps.setString(1, tx.id);
-                    ps.setLong(2, tx.blockHeight);
-                    ps.setTimestamp(3, new Timestamp(tx.timestamp.getTime()));
-                    ps.setInt(4, tx.position);
-                    ps.setString(5, "yes".equals(tx.isLocal) ? TRUE : FALSE);
-                    ps.setBlob(6, asJsonBlob(tx.referenceData));
-                    ps.setBlob(7, asJsonBlob(tx));
-                    ps.executeUpdate();
+                // Commit the batch of transactions to the database.
+                try {
+                    processBatch(page.list);
+                } catch (SQLException ex) {
+                    logger.catching(ex);
+                    // Skip the ack so we re-fetch this page and try again.
+                    // TODO(jackson): Do we need any backoff? Don't want to
+                    // hammer Chain Core or the Oracle database if there's
+                    // an issue.
+                    // TODO(jackson): Add retry logic in processBatch so that
+                    // we don't re-query Chain Core if we don't need to?
+                    continue;
                 }
 
-                // Insert each of the inputs.
-                try (PreparedStatement ps = conn.prepareStatement(insertInputQ)) {
-                    for (int i = 0; i < tx.inputs.size(); i++) {
-                        final Transaction.Input input = tx.inputs.get(i);
-
-                        ps.setString(1, tx.id);
-                        ps.setInt(2, i);
-                        ps.setString(3, input.type);
-                        ps.setString(4, input.assetId);
-                        ps.setString(5, input.assetAlias);
-                        ps.setBlob(6, asJsonBlob(input.assetDefinition));
-                        ps.setBlob(7, asJsonBlob(input.assetTags));
-                        ps.setString(8, "yes".equals(input.assetIsLocal) ? TRUE : FALSE);
-                        ps.setLong(9, input.amount);
-                        ps.setString(10, input.accountId);
-                        ps.setString(11, input.accountAlias);
-                        ps.setBlob(12, asJsonBlob(input.accountTags));
-                        ps.setString(13, input.issuanceProgram); // clob
-                        ps.setBlob(14, asJsonBlob(input.referenceData));
-                        ps.setString(15, "yes".equals(input.isLocal) ? TRUE : FALSE);
-                        ps.setString(16, input.spentOutputId);
-
-                        // TODO(jackson): We can't use addBatch in Oracle
-                        // earlier than 12.1. If customers need support for
-                        // Oracle < 12.1, we'll need to use the deprecated
-                        // OraclePreparedStatement.setExecuteBatch method:
-                        // http://docs.oracle.com/cd/B28359_01/java.111/b31224/oraperf.htm
-                        ps.addBatch();
-                    }
-                    ps.executeBatch();
-                }
-
-                // Insert each of the outputs.
-                try (PreparedStatement ps = conn.prepareStatement(insertOutputQ)) {
-                    for (int i = 0; i < tx.outputs.size(); i++) {
-                        final Transaction.Output output = tx.outputs.get(i);
-
-                        ps.setString(1, tx.id);
-                        ps.setInt(2, i);
-                        ps.setString(3, output.id);
-                        ps.setString(4, output.type);
-                        ps.setString(5, output.purpose);
-                        ps.setString(6, output.assetId);
-                        ps.setString(7, output.assetAlias);
-                        ps.setBlob(8, asJsonBlob(output.assetDefinition));
-                        ps.setBlob(9, asJsonBlob(output.assetTags));
-                        ps.setString(10, "yes".equals(output.assetIsLocal) ? TRUE : FALSE);
-                        ps.setLong(11, output.amount);
-                        ps.setString(12, output.accountId);
-                        ps.setString(13, output.accountAlias);
-                        ps.setBlob(14, asJsonBlob(output.accountTags));
-                        ps.setString(15, output.controlProgram); // clob
-                        ps.setBlob(16, asJsonBlob(output.referenceData));
-                        ps.setString(17, "yes".equals(output.isLocal) ? TRUE : FALSE);
-                        ps.setString(18, FALSE);
-
-                        ps.addBatch();
-                    }
-                    ps.executeBatch();
-                }
-
-                // Commit the entire transaction at once.
-                conn.commit();
-
-                // Mark this transaction as processed, now that we've
-                // successfully indexed it.
-                mFeed.ack(mChain);
-            } catch (SQLException ex) {
-                // We can hit a unique constraint violation (ORA-00001)
-                // iff we already processed this transaction but have not
-                // yet successfully acked the transaction. If that's the
-                // case, just ack the transaction and move on to the next
-                // blockchain transaction.
-                if (ex.getErrorCode() == 1) {
-                    logger.info("Processed transaction twice; acking and continuing");
-                    try {
-                        mFeed.ack(mChain);
-                    } catch(ChainException chainEx) {
-                        logger.catching(chainEx);
-                    }
-                } else {
-                    // If it's a different error code, log the error, don't
-                    // ack and try again.
+                // Acknowledge that we've processed the entire page.
+                Map<String, Object> requestBody = new TreeMap<>();
+                requestBody.put("id", mFeed.id);
+                requestBody.put("previous_after", mFeed.after);
+                requestBody.put("after", page.next.after);
+                mFeed = mChain.request("update-transaction-feed", requestBody, Transaction.Feed.class);
+            } catch (APIException ex) {
+                // If there was an issue retrieving the transactions,
+                // log it. If the request just timed out, no matching
+                // transactions were committed so just silently ignore it.
+                if (!"CH001".equals(ex.code)) {
                     logger.catching(ex);
                 }
             } catch (ChainException ex) {
                 logger.catching(ex);
             } finally {
-                ThreadContext.remove("tx_id");
+                ThreadContext.remove("feed_after");
+            }
+        }
+    }
+
+    // processBatch inserts a batch of transactions into the Oracle
+    // database.
+    //
+    // TODO(jackson): Ideally we'd call executeBatch once per prepared-statement
+    // per call to processBatch, but handling the unique constraint violations
+    // gets trickier. We might be able to write a PL/SQL function that ignores
+    // the ORA-00001 exception in Oracle instead of client-side to get around
+    // that.
+    void processBatch(final List<Transaction> transactions) throws SQLException {
+        final String insertTxQ = mTransactionsTbl.getInsertStatement();
+        final String insertInputQ = mTransactionInputsTbl.getInsertStatement();
+        final String insertOutputQ = mTransactionOutputsTbl.getInsertStatement();
+
+        try (   Connection conn = mDataSource.getConnection();
+                PreparedStatement psTx = conn.prepareStatement(insertTxQ);
+                PreparedStatement psIn = conn.prepareStatement(insertInputQ);
+                PreparedStatement psOut = conn.prepareStatement(insertOutputQ)) {
+
+            // Manage our own SQL transactions so we can make this whole
+            // blockchain transaction atomic.
+            conn.setAutoCommit(false);
+
+            for (Transaction tx : transactions) {
+                try {
+                    ThreadContext.put("tx_id", tx.id);
+                    logger.debug("Importing transaction {}", tx.id);
+
+                    // Insert the transaction itself.
+                    psTx.setString(1, tx.id);
+                    psTx.setLong(2, tx.blockHeight);
+                    psTx.setTimestamp(3, new Timestamp(tx.timestamp.getTime()));
+                    psTx.setInt(4, tx.position);
+                    psTx.setString(5, "yes".equals(tx.isLocal) ? TRUE : FALSE);
+                    psTx.setBlob(6, asJsonBlob(tx.referenceData));
+                    psTx.setBlob(7, asJsonBlob(tx));
+
+                    // TODO(jackson): We can't use addBatch in Oracle
+                    // earlier than 12.1. If customers need support for
+                    // Oracle < 12.1, we'll need to use the deprecated
+                    // OraclePreparedStatement.setExecuteBatch method:
+                    // http://docs.oracle.com/cd/B28359_01/java.111/b31224/oraperf.htm
+                    psTx.addBatch();
+
+                    // Insert each of the inputs.
+                    for (int i = 0; i < tx.inputs.size(); i++) {
+                        final Transaction.Input input = tx.inputs.get(i);
+
+                        psIn.setString(1, tx.id);
+                        psIn.setInt(2, i);
+                        psIn.setString(3, input.type);
+                        psIn.setString(4, input.assetId);
+                        psIn.setString(5, input.assetAlias);
+                        psIn.setBlob(6, asJsonBlob(input.assetDefinition));
+                        psIn.setBlob(7, asJsonBlob(input.assetTags));
+                        psIn.setString(8, "yes".equals(input.assetIsLocal) ? TRUE : FALSE);
+                        psIn.setLong(9, input.amount);
+                        psIn.setString(10, input.accountId);
+                        psIn.setString(11, input.accountAlias);
+                        psIn.setBlob(12, asJsonBlob(input.accountTags));
+                        psIn.setString(13, input.issuanceProgram); // clob
+                        psIn.setBlob(14, asJsonBlob(input.referenceData));
+                        psIn.setString(15, "yes".equals(input.isLocal) ? TRUE : FALSE);
+                        psIn.setString(16, input.spentOutputId);
+
+                        psIn.addBatch();
+                    }
+
+                    // Insert each of the outputs.
+                    for (int i = 0; i < tx.outputs.size(); i++) {
+                        final Transaction.Output output = tx.outputs.get(i);
+
+                        psOut.setString(1, tx.id);
+                        psOut.setInt(2, i);
+                        psOut.setString(3, output.id);
+                        psOut.setString(4, output.type);
+                        psOut.setString(5, output.purpose);
+                        psOut.setString(6, output.assetId);
+                        psOut.setString(7, output.assetAlias);
+                        psOut.setBlob(8, asJsonBlob(output.assetDefinition));
+                        psOut.setBlob(9, asJsonBlob(output.assetTags));
+                        psOut.setString(10, "yes".equals(output.assetIsLocal) ? TRUE : FALSE);
+                        psOut.setLong(11, output.amount);
+                        psOut.setString(12, output.accountId);
+                        psOut.setString(13, output.accountAlias);
+                        psOut.setBlob(14, asJsonBlob(output.accountTags));
+                        psOut.setString(15, output.controlProgram); // clob
+                        psOut.setBlob(16, asJsonBlob(output.referenceData));
+                        psOut.setString(17, "yes".equals(output.isLocal) ? TRUE : FALSE);
+                        psOut.setString(18, FALSE);
+
+                        psOut.addBatch();
+                    }
+
+                    // Commit the entire blockchain transaction at once.
+                    psTx.executeBatch();
+                    psIn.executeBatch();
+                    psOut.executeBatch();
+                    conn.commit();
+                } catch (SQLException ex) {
+                    // We can hit a unique constraint violation (ORA-00001)
+                    // iff we already processed these transactions but have not
+                    // yet successfully acked them. If that's the case, just
+                    // fallthrough to the acknowledgement below.
+                    if (ex.getErrorCode() == 1) {
+                        logger.info("Processed transaction twice; ignoring");
+                    } else {
+                        throw ex;
+                    }
+                } finally {
+                    ThreadContext.remove("tx_id");
+                }
             }
         }
     }
