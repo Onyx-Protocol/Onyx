ffdec5204db37c8a4aef4d7d99e8872036548326
bobg <bobg@chain.com>
2017-04-13 17:57:58 -0700

    WIP: protobuf tx and block types.

--- protocol/bc/tx.go
+++ protocol/bc/tx.go
@@ -1,6 +1,8 @@
 package bc
 
 import (
+	"github.com/golang/protobuf/proto"
+
 	"chain/crypto/sha3pool"
 	"chain/errors"
 )
@@ -83,4 +85,80 @@ func (tx *Tx) Issuance(id Hash) (*Issuance, error) {
 		return nil, errors.Wrapf(ErrEntryType, "entry %x has unexpected type %T", id.Bytes(), e)
 	}
 	return iss, nil
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+func (tx Tx) MarshalBinary() ([]byte, error) {
+	p := &ProtoTx{Header: tx.TxHeader}
+	for _, e := range tx.Entries {
+		switch e := e.(type) {
+		case *Mux:
+			p.Muxes = append(p.Muxes, e)
+		case *Nonce:
+			p.Nonces = append(p.Nonces, e)
+		case *Output:
+			p.Outputs = append(p.Outputs, e)
+		case *Retirement:
+			p.Retirements = append(p.Retirements, e)
+		case *TimeRange:
+			p.Timeranges = append(p.Timeranges, e)
+		case *Issuance:
+			p.Issuances = append(p.Issuances, e)
+		case *Spend:
+			p.Spends = append(p.Spends, e)
+		}
+	}
+	return proto.Marshal(p)
+}
+
+// MarshalBinary implements the encoding.BinaryUnmarshaler interface.
+func (tx *Tx) UnmarshalBinary(b []byte) error {
+	var p ProtoTx
+	err := proto.Unmarshal(b, &p)
+	if err != nil {
+		return err
+	}
+	tx.TxHeader = p.Header
+	tx.ID = EntryID(tx.TxHeader)
+	tx.Entries = make(map[Hash]Entry)
+	tx.InputIDs = []Hash{}
+	tx.NonceIDs = []Hash{}
+	tx.SpentOutputIDs = []Hash{}
+	tx.OutputIDs = []Hash{}
+	for _, m := range p.Muxes {
+		tx.Entries[EntryID(m)] = m
+	}
+	for _, n := range p.Nonces {
+		id := EntryID(n)
+		tx.Entries[id] = n
+		tx.NonceIDs = append(tx.NonceIDs, id)
+	}
+	outputIDs := make(map[Hash]bool)
+	for _, o := range p.Outputs {
+		id := EntryID(o)
+		tx.Entries[id] = o
+		outputIDs[id] = true
+	}
+	for _, r := range p.Retirements {
+		tx.Entries[EntryID(r)] = r
+	}
+	for _, tr := range p.Timeranges {
+		tx.Entries[EntryID(tr)] = tr
+	}
+	for _, iss := range p.Issuances {
+		tx.Entries[EntryID(iss)] = iss
+	}
+	for _, sp := range p.Spends {
+		id := EntryID(sp)
+		tx.Entries[id] = sp
+		tx.SpentOutputIDs = append(tx.SpentOutputIDs, *sp.Body.SpentOutputId)
+		outputIDs[id] = false
+	}
+	for id, ok := range outputIDs {
+		if ok {
+			tx.OutputIDs = append(tx.OutputIDs, id)
+		}
+	}
+	// xxx populate inputids
+	return nil
 }
