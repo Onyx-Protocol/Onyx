522ed6fbca496efae883a83b9e494a1e7ddd060d
Keith Rarick <kr@xph.us>
2017-04-18 18:05:02 -0700

    core: add function TLSConfig
    Closes #963

--- cmd/cored/compat.go
+++ cmd/cored/compat.go
@@ -11,8 +11,8 @@ import (
 // or other option that has been deprecated or removed.
 func warnCompat(ctx context.Context) {
 	for _, name := range []string{
-		"TLSCRT",
-		"TLSKEY",
+		"TLSCRT", // read directly by package chain/core
+		"TLSKEY", // read directly by package chain/core
 	} {
 		if os.Getenv(name) != "" {
 			log.Printkv(ctx, "warning", "deprecated env var", "name", name)
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "main/rev2947".freeze
+	ID = "main/rev2948".freeze
 end
