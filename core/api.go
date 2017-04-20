102657dd28663743648c4e35ddcd94de815dba1e
Tess Rinearson <tess.rinearson@gmail.com>
2017-04-20 16:34:42 -0700

    undo debugging attempt

--- core/api.go
+++ core/api.go
@@ -254,7 +254,6 @@ func (a *API) authnHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		req, err := auth.Authenticate(req)
 		if err != nil {
-			panic(err)
 			errorFormatter.Write(req.Context(), rw, errNotAuthenticated)
 			return
 		}
@@ -267,10 +266,10 @@ func (a *API) authzHandler(mux *http.ServeMux, handler http.Handler) http.Handle
 	auth.GrantInternal(a.internalSubj)
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// return failure early if this path isn't legit
-		// if _, pat := mux.Handler(req); pat != req.URL.Path {
-		// 	errorFormatter.Write(req.Context(), rw, errNotFound)
-		// 	return
-		// }
+		if _, pat := mux.Handler(req); pat != req.URL.Path {
+			errorFormatter.Write(req.Context(), rw, errNotFound)
+			return
+		}
 		err := auth.Authorize(req)
 		if errors.Root(err) == authz.ErrNotAuthorized {
 			// TODO(kr): remove this workaround once dashboard
