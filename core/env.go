44ea49167cae98f63cd77084b5c67e15a26d9224
Keith Rarick <kr@xph.us>
2017-04-21 16:13:22 -0700

    s/DATA/HOME/

--- core/env.go
+++ core/env.go
@@ -7,10 +7,10 @@ import (
 
 // DataDirFromEnvironment returns the directory to use
 // for reading config and storing variable data.
-// It returns $CHAIN_CORE_DATA,
+// It returns $CHAIN_CORE_HOME,
 // or, if that is empty, $HOME/.chaincore.
 func DataDirFromEnvironment() string {
-	if s := os.Getenv("CHAIN_CORE_DATA"); s != "" {
+	if s := os.Getenv("CHAIN_CORE_HOME"); s != "" {
 		return s
 	}
 	return filepath.Join(os.Getenv("HOME"), ".chaincore")
