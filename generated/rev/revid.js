526ec507bb4aafb08088403c69231dd8e2aff7ef
Dominic Dagradi <ddagradi@gmail.com>
2017-04-21 17:22:06 -0700

    docs: update block signer guide with latest corectl instructions
    Backport of #1016
    
    Closes #1017

--- generated/rev/revid.js
+++ generated/rev/revid.js
@@ -1,2 +1,2 @@
 
-export const rev_id = "1.1-stable/rev2762"
+export const rev_id = "1.1-stable/rev2763"
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "1.1-stable/rev2762".freeze
+	ID = "1.1-stable/rev2763".freeze
 end
