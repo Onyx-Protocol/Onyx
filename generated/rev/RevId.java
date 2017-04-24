127a3a12a411009cd5b4d6e2171aec33978db60a
chainbot <ops@chain.com>
2017-04-24 20:56:34 +0000

    auto rev id

--- generated/rev/RevId.java
+++ generated/rev/RevId.java
@@ -1,4 +1,4 @@
 
 public final class RevId {
-	public final String Id = "1.1-stable/rev2763";
+	public final String Id = "1.1-stable/rev2764";
 }
--- generated/rev/revid.go
+++ generated/rev/revid.go
@@ -1,3 +1,3 @@
 package rev
 
-const ID string = "1.1-stable/rev2763"
+const ID string = "1.1-stable/rev2764"
