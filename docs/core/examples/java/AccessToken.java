fe5f6e4eb9fc96dca3f8e18832489ac77f2e9151
Dominic Dagradi <dominic@linebreak.co>
2017-04-24 17:52:29 -0700

    docs class

--- docs/core/examples/java/AccessToken.java
+++ docs/core/examples/java/AccessToken.java
@@ -4,22 +4,26 @@ import com.chain.api.*;
 import com.chain.http.*;
 import com.chain.signing.*;
 
-// snippet connect-with-token
-Client client = new Client(
-  "https://remote-server-url:1999",
-  "token:25f658b749f154a790c8a3aeb57ea98968f51a991c4771fb072fcbb2fa63b6f7"
-);
-// endsnippet
+class AccessToken {
+  public static void main(String[] args) throws Exception {
+    // snippet connect-with-token
+    Client client = new Client(
+      "https://remote-server-url:1999",
+      "token:25f658b749f154a790c8a3aeb57ea98968f51a991c4771fb072fcbb2fa63b6f7"
+    );
+    // endsnippet
 
-// snippet create-read-only
-AccessToken *token = new AccessToken.Builder()
-  .setId("newAccessToken")
-  .create(client);
+    // snippet create-read-only
+    AccessToken *token = new AccessToken.Builder()
+      .setId("newAccessToken")
+      .create(client);
 
-new AuthorizationGrant.Builder()
-  .setGuard(
-    new AuthorizationGrant.AccessTokenGuard().setId("newAccessToken")
-  )
-  .setPolicy("client-readonly")
-  .create(client);
-// endsnippet
+    new AuthorizationGrant.Builder()
+      .setGuard(
+        new AuthorizationGrant.AccessTokenGuard().setId("newAccessToken")
+      )
+      .setPolicy("client-readonly")
+      .create(client);
+    // endsnippet
+  }
+}
