64b20dbf1e66b7306767d99f654f7b12cdcf2fec
Jeff Lee <jeffomatic@gmail.com>
2017-04-26 13:29:12 -0700

    add integration test

---
+++ sdk/java/src/test/java/com/chain/integration/CoreConfigTest.java
@@ -0,0 +1,25 @@
+package com.chain.integration;
+
+import com.chain.api.*;
+import com.chain.http.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CoreConfigTest {
+    @Test
+    public void run() throws Exception {
+        Client c = new Client();
+
+        CoreConfig.Info info = CoreConfig.getInfo(c);
+        assertTrue(info.isConfigured);
+
+        CoreConfig.resetEverything(c);
+        info = CoreConfig.getInfo(c);
+        assertFalse(info.isConfigured);
+
+        new CoreConfig.Builder().setIsGenerator(true).configure(c);
+        info = CoreConfig.getInfo(c);
+        assertTrue(info.isConfigured);
+    }
+}
