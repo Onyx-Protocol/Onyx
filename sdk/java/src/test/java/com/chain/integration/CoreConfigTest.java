0fb7f9a7afdf82cbdf3080b2fdbfb9e211ea3624
Jeff Lee <jeffomatic@gmail.com>
2017-04-26 13:33:44 -0700

    jfmt

--- sdk/java/src/test/java/com/chain/integration/CoreConfigTest.java
+++ sdk/java/src/test/java/com/chain/integration/CoreConfigTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CoreConfigTest {
-    @Test
-    public void run() throws Exception {
-        Client c = new Client();
+  @Test
+  public void run() throws Exception {
+    Client c = new Client();
 
-        CoreConfig.Info info = CoreConfig.getInfo(c);
-        assertTrue(info.isConfigured);
+    CoreConfig.Info info = CoreConfig.getInfo(c);
+    assertTrue(info.isConfigured);
 
-        CoreConfig.resetEverything(c);
-        info = CoreConfig.getInfo(c);
-        assertFalse(info.isConfigured);
+    CoreConfig.resetEverything(c);
+    info = CoreConfig.getInfo(c);
+    assertFalse(info.isConfigured);
 
-        new CoreConfig.Builder().setIsGenerator(true).configure(c);
-        info = CoreConfig.getInfo(c);
-        assertTrue(info.isConfigured);
-    }
+    new CoreConfig.Builder().setIsGenerator(true).configure(c);
+    info = CoreConfig.getInfo(c);
+    assertTrue(info.isConfigured);
+  }
 }
