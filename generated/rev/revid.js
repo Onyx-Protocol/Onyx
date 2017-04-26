4be1f16343f17e5c119cee2817d6db1d70a34f04
Jeff Lee <jeffomatic@gmail.com>
2017-04-26 12:23:16 -0700

    sdk/node: sanitize X509 guard data
    This commit enforces consistency of behavior and language for the Node
    SDK's auth grant interface. Most significantly, the Node SDK will now
    arrayify X509 Subject attributes where necessary.
    
    Closes #1066

--- generated/rev/revid.js
+++ generated/rev/revid.js
@@ -1,2 +1,2 @@
 
-export const rev_id = "main/rev3024"
+export const rev_id = "main/rev3025"
---
+++ sdk/node/src/util.js
@@ -0,0 +1,42 @@
+const x509SubjectAttributes = {
+  C: {array: true},
+  O: {array: true},
+  OU: {array: true},
+  L: {array: true},
+  ST: {array: true},
+  STREET: {array: true},
+  POSTALCODE: {array: true},
+  SERIALNUMBER: {array: false},
+  CN: {array: false},
+}
+
+const sanitizeX509GuardData = guardData => {
+  const keys = Object.keys(guardData)
+  if (keys.length !== 1 || keys[0].toLowerCase() !== 'subject') {
+    throw new Error('X509 guard data must contain exactly one key, "subject"')
+  }
+
+  const newSubject = {}
+  const oldSubject = Object.values(guardData)[0]
+  for (let k in oldSubject) {
+    const attrib = x509SubjectAttributes[k.toUpperCase()]
+    if (!attrib) {
+      throw new Error(`X509 guard data contains invalid subject attribute: ${k}`)
+    }
+
+    let v = oldSubject[k]
+    if (!attrib.array && Array.isArray(v)) {
+      throw new Error(`X509 guard data contains invalid array for attribute ${k}: ${v.toString()}`)
+    } else if (attrib.array && !Array.isArray(v)) {
+      newSubject[k] = [v]
+    } else {
+      newSubject[k] = v
+    }
+  }
+
+  return {subject: newSubject}
+}
+
+module.exports = {
+  sanitizeX509GuardData,
+}
