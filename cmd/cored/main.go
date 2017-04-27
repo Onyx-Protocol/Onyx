2521559dd7039ab9e5bb0a72040c11b14682ce6d
Keith Rarick <kr@xph.us>
2017-04-27 11:03:04 -0700

    separate important config from defaults

--- cmd/cored/main.go
+++ cmd/cored/main.go
@@ -156,6 +156,8 @@ func main() {
 	// and use this object in it.
 	httpClient := new(http.Client)
 	httpClient.Transport = &http.Transport{
+		TLSClientConfig: tlsConfig,
+
 		DialContext: (&net.Dialer{
 			Timeout:   30 * time.Second,
 			KeepAlive: 30 * time.Second,
@@ -163,7 +165,6 @@ func main() {
 		}).DialContext,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
-		TLSClientConfig:       tlsConfig,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
