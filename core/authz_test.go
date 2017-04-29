919f2541073d0d66d96df2718463a1ed1a0efc9c
Tess Rinearson <tess.rinearson@gmail.com>
2017-04-28 18:31:18 -0700

    fixes

--- core/authz_test.go
+++ core/authz_test.go
@@ -33,7 +33,7 @@ func TestAuthz(t *testing.T) {
 	}
 	defer os.RemoveAll(raftDir)
 
-	raftDB, err := raft.Start("", raftDir, "", new(http.Client))
+	raftDB, err := raft.Start("", raftDir, "", new(http.Client), false)
 	if err != nil {
 		t.Fatal(err)
 	}
