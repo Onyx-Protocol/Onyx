dc92574e681d83f2faeb056fb5e17d89cb469734
Dominic Dagradi <dominic@chain.com>
2017-05-01 16:14:49 -0700

    docs/core/examples: make access token examples runnable

--- docs/core/examples/node/accessToken.js
+++ docs/core/examples/node/accessToken.js
@@ -1,11 +1,16 @@
 const chain = require('chain-sdk')
 
+const throwAway = () => {
 // snippet connect-with-token
 const client = new chain.Client({
   url: 'https://remote-server-url:1999',
   token: 'token:25f658b749f154a790c8a3aeb57ea98968f51a991c4771fb072fcbb2fa63b6f7'
 })
 // endsnippet
+}
+
+// Create client without fake token for next example
+const client = new chain.Client()
 
 // snippet create-read-only
 client.accessTokens.create({
