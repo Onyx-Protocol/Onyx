fdb59dda31e55a68b2e2213f82d8a1556787b88d
Keith Rarick <kr@xph.us>
2017-05-01 12:04:35 -0700

    net/http/authz: add missing policy to loopback authz

--- net/http/authz/loopback_authz.go
+++ net/http/authz/loopback_authz.go
@@ -4,10 +4,13 @@ package authz
 
 func init() {
 	builtinGrants = append(builtinGrants,
+		// TODO(kr): refactor this list/logic somehow to get data
+		// from the source of truth for all policies.
 		&Grant{GuardType: "localhost", Policy: "client-readwrite"},
 		&Grant{GuardType: "localhost", Policy: "client-readonly"},
 		&Grant{GuardType: "localhost", Policy: "monitoring"},
 		&Grant{GuardType: "localhost", Policy: "crosscore"},
+		&Grant{GuardType: "localhost", Policy: "crosscore-signblock"},
 		&Grant{GuardType: "localhost", Policy: "internal"},
 	)
 }
