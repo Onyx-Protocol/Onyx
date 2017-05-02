ae960893ea7a8f548b3b62f48deee5ab6f7e34fd
Jeff Lee <jeffomatic@gmail.com>
2017-05-02 13:43:29 -0700

    docker/de: fix startup.sh token generation
    Now that corectl does not talk to the data store directly, we need the
    Docker startup script to wait for cored to become available before
    generating an access token.
    
    Example output:
    
        Chain Core is online!
        Listening on: http://localhost:1999
        Autogenerating acccess token with client-readwrite privileges...
        Copy the whole line below:
        client:965aca3b62a068bf57af2302c621359237116ee6683f17fd5ca09d86645886ed
    
    Closes #1130

--- docker/de/startup.sh
+++ docker/de/startup.sh
@@ -10,16 +10,30 @@ chown -R postgres /var/lib/postgresql/data
 su postgres -c 'initdb -D /var/lib/postgresql/data' >$initlog 2>&1
 su postgres -c 'pg_ctl start -w -D /var/lib/postgresql/data -l /var/lib/postgresql/data/postgres.log' >>$initlog 2>&1
 su postgres -c 'createdb core' >>$initlog 2>&1
-if [[ $? -eq 0 ]]; then
-	/usr/bin/chain/corectl migrate
-	/usr/bin/chain/corectl create-token client | tee -a $initlog | tail -n1 >/var/log/chain/client-token
-fi
+newDatabase=$?
+
 (
 	/usr/bin/chain/cored
 	echo 'cored' >$psmgr
 ) &
-echo "Listening on: http://localhost:1999"
-echo "Client access token: `tail /var/log/chain/client-token`"
+
+/usr/bin/chain/corectl wait
+
 echo "Chain Core is online!"
+echo "Listening on: http://localhost:1999"
+
+if [[ $newDatabase -eq 0 ]]; then
+  echo "Autogenerating acccess token with client-readwrite privileges..."
+  /usr/bin/chain/corectl create-token client client-readwrite \
+    | tee -a $initlog \
+    | tail -n1 > /var/log/chain/client-token
+
+  tail /var/log/chain/client-token | grep -q ":"
+  if [[ $? -eq 0 ]]; then
+    echo "Copy the whole line below:"
+    tail /var/log/chain/client-token
+  fi
+fi
+
 read exit_process <$psmgr
 exit 1
