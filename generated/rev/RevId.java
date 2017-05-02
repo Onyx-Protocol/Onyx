0b76cb751a1bcf132f4204d0c487eacbff4c1fc2
Tess Rinearson <tess.rinearson@gmail.com>
2017-05-02 14:22:42 -0700

    cmd/corectl: print blockchain id after configuring generator
    Closes #1135

--- generated/rev/RevId.java
+++ generated/rev/RevId.java
@@ -1,4 +1,4 @@
 
 public final class RevId {
-	public final String Id = "main/rev3088";
+	public final String Id = "main/rev3089";
 }
--- generated/rev/revid.go
+++ generated/rev/revid.go
@@ -1,3 +1,3 @@
 package rev
 
-const ID string = "main/rev3088"
+const ID string = "main/rev3089"
