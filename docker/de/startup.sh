6d6823f0b2994c8dde6f719aba12f1164d3bfba9
Jeff Lee <jeffomatic@gmail.com>
2017-05-02 10:34:30 -0700

    WIP

--- docker/de/startup.sh
+++ docker/de/startup.sh
@@ -10,16 +10,20 @@ chown -R postgres /var/lib/postgresql/data
 su postgres -c 'initdb -D /var/lib/postgresql/data' >$initlog 2>&1
 su postgres -c 'pg_ctl start -w -D /var/lib/postgresql/data -l /var/lib/postgresql/data/postgres.log' >>$initlog 2>&1
 su postgres -c 'createdb core' >>$initlog 2>&1
-if [[ $? -eq 0 ]]; then
-	/usr/bin/chain/corectl migrate
-	/usr/bin/chain/corectl create-token client | tee -a $initlog | tail -n1 >/var/log/chain/client-token
-fi
+shouldInitToken=$?
+
 (
 	/usr/bin/chain/cored
 	echo 'cored' >$psmgr
 ) &
 echo "Listening on: http://localhost:1999"
-echo "Client access token: `tail /var/log/chain/client-token`"
+
+corectl wait
+
+if [[ $shouldInitToken -eq 0 ]]; then
+  /usr/bin/chain/corectl create-token client client-readwrite | tee -a $initlog | tail -n1 >/var/log/chain/client-token
+fi
+
 echo "Chain Core is online!"
 read exit_process <$psmgr
 exit 1
