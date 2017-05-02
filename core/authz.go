46c98d1ccf93a2433307aca7f6e3f0bf71b93984
Keith Rarick <kr@xph.us>
2017-05-02 14:28:06 -0700

    core: add /info to internal policy
    Another RPC used by corectl.

--- core/authz.go
+++ core/authz.go
@@ -55,7 +55,7 @@ var policyByRoute = map[string][]string{
 	"/delete-access-token":        {"client-readwrite"},
 	"/add-allowed-member":         {"internal"},
 	"/configure":                  {"client-readwrite", "internal"},
-	"/info":                       {"client-readwrite", "client-readonly", "crosscore", "crosscore-signblock", "monitoring"},
+	"/info":                       {"client-readwrite", "client-readonly", "crosscore", "crosscore-signblock", "monitoring", "internal"},
 
 	"/debug/vars":          {"client-readwrite", "client-readonly", "monitoring"}, // should monitoring endpoints also be available to any other policy-holders?
 	"/debug/pprof":         {"client-readwrite", "client-readonly", "monitoring"},
