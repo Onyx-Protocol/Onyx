38feec102bbe9637e7ac28ecdfba4c52e3c9adc3
Bob Glickstein <bobg@chain.com>
2017-05-03 18:34:10 -0700

    Return clause-arg info from Compile.

--- core/ivy.go
+++ core/ivy.go
@@ -14,7 +14,8 @@ type (
 	}
 
 	compileResp struct {
-		Bytes   chainjson.HexBytes `json:"bytes"`
+		Program chainjson.HexBytes `json:"program"`
+		Clauses []ivy.ClauseInfo   `json:"clause_info"`
 		Opcodes string             `json:"opcodes"`
 		Error   string             `json:"error"`
 	}
@@ -22,10 +23,11 @@ type (
 
 func compileIvy(req compileReq) (compileResp, error) {
 	var resp compileResp
-	prog, err := ivy.Compile(strings.NewReader(req.Contract))
+	compiled, err := ivy.Compile(strings.NewReader(req.Contract))
 	if err == nil {
-		resp.Bytes = prog
-		resp.Opcodes, err = vm.Disassemble(prog)
+		resp.Program = compiled.Program
+		resp.Clauses = compiled.Clauses
+		resp.Opcodes, err = vm.Disassemble(resp.Program)
 		if err != nil {
 			return resp, err
 		}
