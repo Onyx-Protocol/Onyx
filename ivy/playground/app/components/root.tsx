11e6c339261a0b10611797a1d1a71ab777e0e270
Dan Robinson <danrobinson010@gmail.com>
2017-05-04 13:54:12 -0700

    add tooltip error messages to template editor

--- ivy/playground/app/components/root.tsx
+++ ivy/playground/app/components/root.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import NavBar from './navbar'
+import ReactTooltip from 'react-tooltip'
 
 type Props = { children?: any }
 
 export default function Root(props: Props) {
   return (
     <div>
+      <ReactTooltip place="bottom" type="error" effect="solid"/>
       <NavBar />
       <div className="container fixedcontainer">
         {props.children}
