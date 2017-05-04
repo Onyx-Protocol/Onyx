bd9988aacf11f1f0c66655f71b8e06cfff4d14f6
Dan Robinson <danrobinson010@gmail.com>
2017-05-04 14:24:27 -0700

    add tooltip error message to Create button

--- ivy/playground/app/components/root.tsx
+++ ivy/playground/app/components/root.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import NavBar from './navbar'
-import ReactTooltip from 'react-tooltip'
 
 type Props = { children?: any }
 
 export default function Root(props: Props) {
   return (
     <div>
-      <ReactTooltip place="bottom" type="error" effect="solid"/>
       <NavBar />
       <div className="container fixedcontainer">
         {props.children}
