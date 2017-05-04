e275bc692f243a5a9fa28f4e70d019e1f4f93386
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-03 17:08:23 -0700

    core/utxos: track all unspents
    This is needed for spending arbitrary outputs.

---
+++ core/utxos/builder.go
@@ -0,0 +1,52 @@
+package utxos
+
+import (
+	"context"
+	"encoding/json"
+
+	"chain/core/txbuilder"
+	chainjson "chain/encoding/json"
+	"chain/errors"
+	"chain/protocol/bc"
+	"chain/protocol/bc/legacy"
+)
+
+type spendUTXOAction struct {
+	store    *Store
+	OutputID *bc.Hash `json:"output_id"`
+
+	ReferenceData chainjson.Map `json:"reference_data"`
+}
+
+func (s *Store) DecodeSpendUTXOAction(data []byte) (txbuilder.Action, error) {
+	a := &spendUTXOAction{store: s}
+	err := json.Unmarshal(data, a)
+	return a, err
+}
+
+func (a *spendUTXOAction) Build(ctx context.Context, b *txbuilder.TemplateBuilder) error {
+	if a.OutputID == nil {
+		return txbuilder.MissingFieldsError("output_id")
+	}
+
+	var (
+		sourceID       bc.Hash
+		assetID        bc.AssetID
+		amount         uint64
+		sourcePos      uint64
+		controlProgram []byte
+		refDataHash    bc.Hash
+	)
+	const q = `
+		SELECT source_id, asset_id, amount, source_pos, control_program, ref_data_hash
+		FROM utxos WHERE output_id=$1
+	`
+	err := a.store.DB.QueryRow(ctx, q, a.OutputID).Scan(&sourceID, &assetID, &amount, &sourcePos, &controlProgram, &refDataHash)
+	if err != nil {
+		return errors.Wrap(err)
+	}
+
+	txInput := legacy.NewSpendInput(nil, sourceID, assetID, amount, sourcePos, controlProgram, refDataHash, a.ReferenceData)
+
+	return b.AddInput(txInput, &txbuilder.SigningInstruction{})
+}
