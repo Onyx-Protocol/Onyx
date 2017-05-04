ad57f0f81945d873a23b2e4e26c548729a7fdcaf
Dan Robinson <danrobinson010@gmail.com>
2017-05-04 13:54:12 -0700

    add tooltip error messages to template editor

--- ivy/playground/templates/components/save.tsx
+++ ivy/playground/templates/components/save.tsx
@@ -4,15 +4,31 @@ import { getTemplate, getItemMap } from '../selectors'
 import { isError } from '../util'
 import { saveTemplate } from '../actions'
 
-function SaveUnconnected({ reset, disabled }) {
-  return <button className="btn btn-primary" onClick={reset} disabled={disabled}><span className="glyphicon glyphicon-floppy-disk"></span> Save</button>
+function SaveUnconnected({ reset, error, exists }) {
+  if (error) {
+    return <td data-tip="Contract template does not compile."><button className="btn btn-primary" disabled={true}>
+        <span className="glyphicon glyphicon-floppy-disk"></span>
+        Save
+    </button></td>
+  } else if (exists) {
+    return <td data-tip="There is already a contract template with that name."><button className="btn btn-primary" disabled={true}>
+        <span className="glyphicon glyphicon-floppy-disk"></span>
+        Save
+    </button></td>
+  } else {
+    return <td><button className="btn btn-primary" onClick={reset}>
+      <span className="glyphicon glyphicon-floppy-disk"></span>
+      Save
+    </button></td>
+  }
 }
 
 function mapStateToSaveProps(state) {
   let template = getTemplate(state)
   let templates = getItemMap(state)
   return {
-    disabled: (isError(template) || templates[template.name] !== undefined)
+    error: (isError(template)),
+    exists: !isError(template) && templates[template.name] !== undefined
   }
 }
 
