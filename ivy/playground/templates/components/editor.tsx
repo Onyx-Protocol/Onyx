63dc82648b09037c997e1803f97d2f214d5c28a6
Dan Robinson <danrobinson010@gmail.com>
2017-05-04 13:54:12 -0700

    add tooltip error messages to template editor

--- ivy/playground/templates/components/editor.tsx
+++ ivy/playground/templates/components/editor.tsx
@@ -25,7 +25,7 @@ const Editor = ({ template }) => {
           <h1 className="panel-title pull-left">Template Editor</h1>
           <table className="pull-right"><tbody><tr>
             <td style={{width: 300, paddingRight: 10}}><Load /></td>
-            <td><Save /></td>
+            <Save />{/* Save returns a <td> element */}
           </tr></tbody></table>
         </div>
         <Ace source={template.source} />
