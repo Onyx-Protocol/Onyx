f6b2a9c126b32d65694bcd5bbef663a9dbff1fda
Bob Glickstein <bobg@chain.com>
2017-05-05 11:29:49 -0700

    Improve compiler error messages (pass 1 of 2).

--- protocol/ivy/types.go
+++ protocol/ivy/types.go
@@ -1,6 +1,11 @@
 package ivy
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type contract struct {
 	name    string
@@ -52,6 +57,7 @@ type returnStatement struct {
 func (returnStatement) iamaStatement() {}
 
 type expression interface {
+	String() string
 	iamaExpression()
 }
 
@@ -62,12 +68,9 @@ type binaryExpr struct {
 
 func (binaryExpr) iamaExpression() {}
 
-type partialBinaryExpr struct {
-	expr expression
-	op   string
+func (e binaryExpr) String() string {
+	return fmt.Sprintf("(%s %s %s)", e.left, e.op.op, e.right)
 }
-
-func (partialBinaryExpr) iamaExpression() {}
 
 type unaryExpr struct {
 	op   *unaryOp
@@ -76,12 +79,24 @@ type unaryExpr struct {
 
 func (unaryExpr) iamaExpression() {}
 
+func (e unaryExpr) String() string {
+	return fmt.Sprintf("%s%s", e.op.op, e.expr)
+}
+
 type call struct {
 	fn   expression
 	args []expression
 }
 
 func (call) iamaExpression() {}
+
+func (e call) String() string {
+	var argStrs []string
+	for _, a := range e.args {
+		argStrs = append(argStrs, a.String())
+	}
+	return fmt.Sprintf("%s(%s)", e.fn, strings.Join(argStrs, ", "))
+}
 
 type propRef struct {
 	expr     expression
@@ -112,13 +127,28 @@ type bytesLiteral []byte
 
 func (bytesLiteral) iamaExpression() {}
 
+func (e bytesLiteral) String() string {
+	return "0x" + hex.EncodeToString([]byte(e))
+}
+
 type integerLiteral int64
 
 func (integerLiteral) iamaExpression() {}
 
+func (e integerLiteral) String() string {
+	return strconv.FormatInt(int64(e), 10)
+}
+
 type booleanLiteral bool
 
 func (booleanLiteral) iamaExpression() {}
+
+func (e booleanLiteral) String() string {
+	if e {
+		return "true"
+	}
+	return "false"
+}
 
 func typeOf(expr expression) string {
 	switch e := expr.(type) {
