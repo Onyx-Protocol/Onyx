22533ec31588b5848dc11b1decb9d3f22c99439c
Keith Rarick <kr@xph.us>
2017-05-09 14:51:50 -0700

    fix proto includes

--- protocol/bc/gen.go
+++ protocol/bc/gen.go
@@ -1,7 +1,6 @@
 package bc
 
-// Note that we discard the generated hash.pb.go.
+// Note that we do not generate hash.pb.go.
 // See the comment in hash.proto.
 
-//go:generate protoc --go_out=. bc.proto hash.proto
-//go:generate rm hash.pb.go
+//go:generate protoc -I. -I$CHAIN/.. --go_out=. bc.proto
