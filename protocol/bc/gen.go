cb19980c285d08786c332e0ed1693f4a9116240f
Keith Rarick <kr@xph.us>
2017-05-09 14:29:27 -0700

    protocol/bc: marshal Hash with custom code
    This allows us to use our preferred internal
    representation, [32]byte, at all times.
    
    Future work: some helper functions and methods are now
    extraneous and should be removed. For example, NewHash,
    Byte32, and possibly Bytes.

--- protocol/bc/gen.go
+++ protocol/bc/gen.go
@@ -1,3 +1,7 @@
 package bc
 
-//go:generate protoc --go_out=. bc.proto
+// Note that we discard the generated hash.pb.go.
+// See the comment in hash.proto.
+
+//go:generate protoc --go_out=. bc.proto hash.proto
+//go:generate rm hash.pb.go
---
+++ protocol/bc/internal/doc.go
@@ -0,0 +1,4 @@
+// Package internal contains definitions used internally by chain/protocol/bc.
+package internal
+
+//go:generate protoc --go_out=. internal.proto
