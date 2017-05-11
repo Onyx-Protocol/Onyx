4f10af1cfc3306958a4aee7c937a3187351deadf
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-05-11 16:09:00 -0700

    Do not use JS compiler for typechecking

--- ivy/playground/templates/actions.tsx
+++ ivy/playground/templates/actions.tsx
@@ -2,6 +2,8 @@ import { client } from '../util'
 import { getItemMap, getIdList, getCompiled, getContractParameters } from './selectors'
 import { Template, ItemMap, CompilerResult } from './types'
 import { ContractParameter, TemplateClause, ClauseParameter } from 'ivy-compiler'
+import { mapServerTemplate } from './util'
+import { generateInputMap } from '../contracts/reducer'
 
 export const SET_INITIAL_TEMPLATES = 'templates/SET_TEMPLATES'
 export const SET_SOURCE = 'templates/SET_SOURCE'
@@ -10,12 +12,8 @@ export const COMPILER_ERROR = 'templates/COMPILER_ERROR'
 
 export const load = (selected: string) => {
   return (dispatch, getState) => {
-    let state = getState()
-    let source = getItemMap(state)[selected]
-    dispatch({
-      type: SET_SOURCE,
-      source: source
-    })
+    const state = getState()
+    const source = getItemMap(state)[selected]
     dispatch(fetchCompiled(source))
   }
 }
@@ -23,15 +21,27 @@ export const load = (selected: string) => {
 
 export const fetchCompiled = (source: string) => {
   return (dispatch, getState) => {
-    return client.ivy.compile({ contract: source }).then(
-      (compiled: CompilerResult) => dispatch({
-        type: SET_COMPILED, 
-        result: {
-          ...compiled,
-          source: source // because error results have source: ""
-        }
-      })
-    ).catch((e) => {throw e})
+    return client.ivy.compile({ contract: source }).then((res) => {
+      let tpl
+      if (res.error === "") {
+        tpl = mapServerTemplate(res)
+      }
+      tpl = {...tpl, error: res.error}
+      if (tpl.instructions) {
+        tpl.opcodes = tpl.instructions.join(' ')
+      }
+
+      let type = SET_COMPILED
+      // Error results return a blank source so we
+      // pass the compiled source to the reducer.
+      const compiled = { ...tpl, source}
+      dispatch({ type, compiled })
+
+      type = SET_SOURCE
+      const contractParameters = compiled.contractParameters
+      const inputMap = contractParameters ? generateInputMap(contractParameters) : undefined
+      dispatch({ type, source, contractParameters, inputMap })
+    }).catch((e) => {throw e})
   }
 }
 
@@ -39,7 +49,6 @@ export const setSource = (source: string) => {
   return (dispatch, getState) => {
     const type = SET_SOURCE
     dispatch(fetchCompiled(source))
-    return dispatch({ type, source })
   }
 }
 
