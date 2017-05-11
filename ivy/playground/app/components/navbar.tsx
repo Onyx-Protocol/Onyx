ca3219dc91eca28a24ca69d191f0d74b7fb64d7f
Devon Gundry <devon@devon-2.local>
2017-05-10 22:55:52 -0700

    External docs link

--- ivy/playground/app/components/navbar.tsx
+++ ivy/playground/app/components/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
           <li><Link to={prefixRoute('/')}>Create Contract</Link></li>
           <li><Link to={prefixRoute('/spend')}>Spend Contract</Link></li>
           <li className="divider-vertical"></li>
-          <li><Link to={prefixRoute('/docs')}>Docs</Link></li>
+          <li><a href="https://chain.com/docs" target="_blank">Docs</a></li>
           <li className="dropdown">
             <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Setup <span className="caret"></span></a>
             <ul className="dropdown-menu">
