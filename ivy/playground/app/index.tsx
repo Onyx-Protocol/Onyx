2e93708525b408a6fa4f6f803e9990156cbaee35
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-05-12 15:56:50 -0700

    Clean up app module

--- ivy/playground/app/index.tsx
+++ ivy/playground/app/index.tsx
@@ -1,6 +1,5 @@
 import * as actions from './actions'
-import * as constants from './constants'
 import components from './components'
 import reducer from './reducer'
 
-export default { actions, components, constants, reducer }
+export default { actions, components, reducer }
--- ivy/playground/app/reducer.tsx
+++ ivy/playground/app/reducer.tsx
@@ -1,19 +1,26 @@
+// external imports
 import { routerReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
 
+// ivy imports
 import accounts from '../accounts'
 import assets from '../assets'
 import contracts from '../contracts'
 import templates from '../templates'
 
-import { AppState } from './types'
-import { RESET, INITIAL_STATE } from './constants'
+// internal imports
+import * as actions from './actions'
+import * as types from './types'
 
-export default function reducer(state: AppState, action): AppState {
+export default function reducer(state: types.AppState, action): types.AppState {
   switch (action.type) {
-    case RESET:
+    case actions.RESET:
       return {
-        ...INITIAL_STATE,
+        accounts: accounts.reducer(undefined, {}),
+        assets: assets.reducer(undefined, {}),
+        contracts: contracts.reducer(undefined, {}),
+        templates: templates.reducer(undefined, {}),
+        routing: routerReducer(undefined, {})
       }
     default:
       return combineReducers({
@@ -22,6 +29,6 @@ export default function reducer(state: AppState, action): AppState {
         contracts: contracts.reducer,
         templates: templates.reducer,
         routing: routerReducer
-      })(state, action) as AppState
+      })(state, action) as types.AppState
   }
 }
