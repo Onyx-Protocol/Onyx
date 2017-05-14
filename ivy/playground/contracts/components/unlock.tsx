d2988be3116eb2d14fad99095071476ac7b5f922
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-05-14 14:13:33 -0700

    Display message when no contract found

--- ivy/playground/contracts/components/unlock.tsx
+++ ivy/playground/contracts/components/unlock.tsx
@@ -6,7 +6,6 @@ import { connect } from 'react-redux'
 // ivy imports
 import Section from '../../app/components/section'
 import { Contract } from '../types'
-import { getSpendContractId } from '../selectors'
 
 // internal imports
 import SpendInputs from './argsDisplay'
@@ -19,29 +18,41 @@ import { ContractValue } from './argsDisplay'
 
 export default function Unlock(props: { contract: Contract }) {
   const contract = props.contract
-  return (
-    <DocumentTitle title="Unlock Value">
-      <div>
-        <Section name="Contract Summary">
-          <div className="form-wrapper with-subsections">
-            <section>
-              <h4>Contract Template</h4>
-              <DisplaySpendContract />
-            </section>
-            <ContractValue />
-            <SpendInputs />
-          </div>
-        </Section>
-        <Section name="Unlocking Details">
-          <div className="form-wrapper with-subsections">
-            <ClauseSelect />
-            <ClauseValue />
-            <ClauseParameters />
-            <UnlockDestination />
-          </div>
-        </Section>
-        <UnlockButton />
-      </div>
-    </DocumentTitle>
-  )
+  if (contract) {
+    return (
+      <DocumentTitle title="Unlock Value">
+        <div>
+          <Section name="Contract Summary">
+            <div className="form-wrapper with-subsections">
+              <section>
+                <h4>Contract Template</h4>
+                <DisplaySpendContract />
+              </section>
+              <ContractValue />
+              <SpendInputs />
+            </div>
+          </Section>
+          <Section name="Unlocking Details">
+            <div className="form-wrapper with-subsections">
+              <ClauseSelect />
+              <ClauseValue />
+              <ClauseParameters />
+              <UnlockDestination />
+            </div>
+          </Section>
+          <UnlockButton />
+        </div>
+      </DocumentTitle>
+    )
+  } else {
+    return (
+      <DocumentTitle title="Unlock Value">
+       <div>
+         <Section name="Contract Summary">
+           <div className="table-placeholder">No Contract Found</div>
+         </Section>
+       </div>
+      </DocumentTitle>
+    )
+  }
 }
