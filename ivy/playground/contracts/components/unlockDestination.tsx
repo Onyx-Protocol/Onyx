90f708e3252d01f5eb2c6dab269acfebb1a79a23
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-05-15 12:30:06 -0700

    Display Unlocked Value Dest asset and amount

--- ivy/playground/contracts/components/unlockDestination.tsx
+++ ivy/playground/contracts/components/unlockDestination.tsx
@@ -5,23 +5,36 @@ import { connect } from 'react-redux'
 // ivy imports
 import { Input } from '../../inputs/types'
 import { getWidget } from '../../contracts/components/parameters'
+import { getItemMap as getAssetMap } from '../../assets/selectors'
 
 // internal imports
-import { getClauseUnlockInput } from '../selectors'
+import { getSpendContract, getClauseUnlockInput } from '../selectors'
 
-const UnlockDestination = (props: { unlockInput: Input }) => {
-  if (props.unlockInput === undefined) {
+const UnlockDestination = (props: { assetMap, contract, unlockInput: Input }) => {
+  if (props.unlockInput === undefined || props.assetMap === undefined) {
     return <div></div>
   } else {
     return (
       <section>
         <h4>Unlocked Value Destination</h4>
         {getWidget("unlockValue.accountInput")}
+        <div className="form-group">
+          <div className="input-group">
+            <div className="input-group-addon">Asset</div>
+            <input type="text" className="form-control" value={props.assetMap[props.contract.assetId].alias} disabled />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="input-group">
+            <div className="input-group-addon">Amount</div>
+            <input type="text" className="form-control" value={props.contract.amount} disabled />
+          </div>
+        </div>
       </section>
     )
   }
 }
 
 export default connect(
-  (state) => ({ unlockInput: getClauseUnlockInput(state) })
+  (state) => ({ assetMap: getAssetMap(state), unlockInput: getClauseUnlockInput(state), contract: getSpendContract(state) })
 )(UnlockDestination)
