beaa810a2e95b296a7c361443c42b11e835ae88c
bobg <bobg@chain.com>
2017-05-15 11:47:44 -0700

    Contract args are reversed, clause args are not.

--- protocol/ivy/stack.go
+++ protocol/ivy/stack.go
@@ -6,11 +6,19 @@ func (s stackEntry) matches(expr expression) bool {
 	return string(s) == expr.String()
 }
 
-func addParamsToStack(stack []stackEntry, params []*param) []stackEntry {
-	for i := len(params) - 1; i >= 0; i-- {
-		p := params[i]
-		if p.typ != "Value" {
-			stack = append(stack, stackEntry(p.name))
+func addParamsToStack(stack []stackEntry, params []*param, reversed bool) []stackEntry {
+	if reversed {
+		for i := len(params) - 1; i >= 0; i-- {
+			p := params[i]
+			if p.typ != "Value" {
+				stack = append(stack, stackEntry(p.name))
+			}
+		}
+	} else {
+		for _, p := range params {
+			if p.typ != "Value" {
+				stack = append(stack, stackEntry(p.name))
+			}
 		}
 	}
 	return stack
