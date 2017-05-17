206ce6da2035ff3532c228179e06cceaeadf0ef8
Boyma Fahnbulleh <boymanjor@icloud.com>
2017-05-16 22:08:36 -0700

    ivy/playground: include Chain Inc footer

--- ivy/playground/app/components/root.tsx
+++ ivy/playground/app/components/root.tsx
@@ -10,6 +10,11 @@ export default function Root(props: Props) {
       <div className="container fixedcontainer">
         {props.children}
       </div>
+      <footer>
+        <div className="container fixedcontainer">
+          &copy; 2017 Chain Inc
+        </div>
+      </footer>
     </div>
   )
 }
