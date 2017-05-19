e708351f885a1d76d853cb607e1418fc04784c7a
Dan Robinson <danrobinson010@gmail.com>
2017-05-19 19:37:02 -0400

    add confirmation alert to reset button

--- ivy/playground/app/components/reset.tsx
+++ ivy/playground/app/components/reset.tsx
@@ -6,7 +6,10 @@ const mapStateToProps = undefined
 const mapDispatchToProps = (dispatch) => ({
   handleClick(e) {
     e.preventDefault()
-    dispatch(reset())
+    let confirm = window.confirm("Are you sure you want to reset the playground? Your contracts and any data used to generate them will be forgotten, making it impossible to recover any locked assets.");
+    if (confirm) {
+      dispatch(reset())
+    }
   }
 })
 
