38bb2e3cef71f2b0f7eeece3873763c389a3df58
Jeff Lee <jeffomatic@gmail.com>
2017-05-22 14:32:39 -0700

    docs: clarify backward compatibility rules for SDKs
    This commit refines our versioning language such that
    backward compatibility guarantees only hold between server packages and
    client packages of the previous minor version. In particular, this
    is intended to eliminate any interpretation that holds that new SDKs
    should be backward-compatible with application code written for a
    previous SDK version.
    
    The goal of this change is to allow us to pursue a more aggressive
    release roadmap, and to eliminate versioning kludges in the SDK. It
    allows us to introduce new usage patterns (potentially breaking changes)
    in consecutive minor versions of the SDK without eliminating a smooth
    upgrade path for application code.
    
    Closes #1225

--- generated/rev/RevId.java
+++ generated/rev/RevId.java
@@ -1,4 +1,4 @@
 
 public final class RevId {
-	public final String Id = "main/rev3164";
+	public final String Id = "main/rev3165";
 }
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "main/rev3164".freeze
+	ID = "main/rev3165".freeze
 end
