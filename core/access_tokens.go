1afe01b88194a188f17120c7059f7fcdbbd797f5
chainbot <ops@chain.com>
2017-05-24 15:37:03 -0700

    net/http/authz: remove unnecessary return param
    The caller doesn't need a copy of the existing grant;
    it already has all that data at hand.

--- core/access_tokens.go
+++ core/access_tokens.go
@@ -52,7 +52,7 @@ func (a *API) createAccessToken(ctx context.Context, x struct{ ID, Type string }
 		// We've already returned if x.Type wasn't specified, so this must be a bad type.
 		return nil, accesstoken.ErrBadType
 	}
-	_, err = a.grants.Save(ctx, grant)
+	err = a.grants.Save(ctx, grant)
 	if err != nil {
 		return nil, errors.Wrap(err)
 	}
--- net/http/authz/store.go
+++ net/http/authz/store.go
@@ -44,7 +44,7 @@ func (s *Store) Load(ctx context.Context, policy []string) ([]*Grant, error) {
 // If a grant equivalent to g is already stored,
 // Save has no effect and returns a copy of the existing grant.
 // Otherwise, if successful, it returns g.
-func (s *Store) Save(ctx context.Context, g *Grant) (*Grant, error) {
+func (s *Store) Save(ctx context.Context, g *Grant) error {
 	key := s.keyPrefix + g.Policy
 	if g.CreatedAt == "" {
 		g.CreatedAt = time.Now().UTC().Format(time.RFC3339)
@@ -53,14 +53,14 @@ func (s *Store) Save(ctx context.Context, g *Grant) (*Grant, error) {
 	var grantList GrantList
 	_, err := s.sdb.Get(ctx, key, &grantList)
 	if err != nil {
-		return nil, errors.Wrap(err)
+		return errors.Wrap(err)
 	}
 
 	grants := grantList.Grants
 	for _, existing := range grants {
 		if EqualGrants(*existing, *g) {
-			// this grant already exists, return for idempotency
-			return existing, nil
+			// this grant already exists, do nothing
+			return nil
 		}
 	}
 
@@ -70,10 +70,7 @@ func (s *Store) Save(ctx context.Context, g *Grant) (*Grant, error) {
 	// TODO(tessr): Make this safe for concurrent updates. Will likely require a
 	// conditional write operation for raftDB
 	err = s.sdb.Exec(ctx, sinkdb.Set(s.keyPrefix+g.Policy, &GrantList{Grants: grants}))
-	if err != nil {
-		return nil, errors.Wrap(err)
-	}
-	return g, nil
+	return errors.Wrap(err)
 }
 
 // Delete deletes from policy all stored grants for which delete returns true.
