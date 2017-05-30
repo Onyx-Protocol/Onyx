2acb89d952b7c15b5ea2ed800db837681dd373d9
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-30 14:19:18 -0700

    core/asset: fix inserts for escaped strings
    Passing the definition as a string can cause unexpected problems with
    escaped strings, so we pass it as a byte array instead.
    
    Closes #1245

--- core/asset/block.go
+++ core/asset/block.go
@@ -107,7 +107,7 @@ func (reg *Registry) ProcessBlocks(ctx context.Context) {
 func (reg *Registry) indexAssets(ctx context.Context, b *legacy.Block) error {
 	var (
 		assetIDs         pq.ByteaArray
-		definitions      pq.StringArray
+		definitions      pq.ByteaArray
 		vmVersions       pq.Int64Array
 		issuancePrograms pq.ByteaArray
 		seen             = make(map[bc.AssetID]bool)
@@ -125,7 +125,7 @@ func (reg *Registry) indexAssets(ctx context.Context, b *legacy.Block) error {
 				definition := ii.AssetDefinition
 				seen[assetID] = true
 				assetIDs = append(assetIDs, assetID.Bytes())
-				definitions = append(definitions, string(definition))
+				definitions = append(definitions, definition)
 				vmVersions = append(vmVersions, int64(ii.VMVersion))
 				issuancePrograms = append(issuancePrograms, in.IssuanceProgram())
 			}
--- generated/rev/revid.go
+++ generated/rev/revid.go
@@ -1,3 +1,3 @@
 package rev
 
-const ID string = "main/rev3186"
+const ID string = "main/rev3187"
