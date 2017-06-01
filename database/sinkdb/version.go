b788a4a3515fe08274db2fc4c2f9e3ad5b0d8c30
Jackson Owens <jackson@chain.com>
2017-06-01 11:24:46 -0700

    database/sinkdb: use map key existence for Exists
    For now use the existence of the key in the state map for
    Version.Exists. Once we have a backfill for Chain Core 1.2 snapshots
    that omit versions, we use the version check instead.

--- database/sinkdb/version.go
+++ database/sinkdb/version.go
@@ -5,13 +5,16 @@ package sinkdb
 // Every time a key is set, its version changes.
 type Version struct {
 	key string
+	ok  bool
 	n   uint64 // raft log index
 }
 
 // Exists returns whether v's key exists
 // when it was read.
 func (v Version) Exists() bool {
-	return v.n != 0
+	// TODO(jackson): use v.n != 0 once we've backfilled versions
+	// for Chain Core 1.2.x snapshots.
+	return v.ok
 }
 
 // Key returns the key for which v is valid.
