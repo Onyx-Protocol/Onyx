1be1ea51e8e468d9cf17f2bfc5549162ccfb51cb
Chris Garvin <chris.garvin@me.com>
2017-06-01 17:45:01 -0700

    sdk/node: create authorization grant specific test

--- sdk/node/test/accounts.js
+++ sdk/node/test/accounts.js
@@ -15,119 +15,116 @@ const yAccountAlias = `y-${uuid.v4()}`
 
 let mockHsmKey
 
-describe('Accounts test', () => {
+describe('Account', () => {
 
-  describe('Promise style', () => {
+  before('set up API objects', () => {
 
-    before('set up API objects', () => {
-
-      // Key and account creation
-      return client.mockHsm.keys.create()
-        .then(key => { mockHsmKey = key })
-        .then(() => {
-          return client.accounts.create({alias: xAccountAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {x: 0}})
-        })
-        .then(() => {
-          return client.accounts.create({alias: yAccountAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {y: 0}})
-        })
-    })
-
-    describe('Single account creation', () => {
-
-      it('successful', () => {
-        return client.accounts.create({alias: `alice-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1})
-          .then(resp => expect(resp.id).not.to.be.empty)
+    // Key and account creation
+    return client.mockHsm.keys.create()
+      .then(key => { mockHsmKey = key })
+      .then(() => {
+        return client.accounts.create({alias: xAccountAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {x: 0}})
       })
+      .then(() => {
+        return client.accounts.create({alias: yAccountAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {y: 0}})
+      })
+  })
 
-      it('rejected due to missing key fields', () => {
-        return expect(client.accounts.create({alias: 'david'})).to.be.rejectedWith('CH202')
+  describe('Single account creation', () => {
+
+    it('successful', () => {
+      return client.accounts.create({alias: `alice-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1})
+        .then(resp => expect(resp.id).not.to.be.empty)
+    })
+
+    it('rejected due to missing key fields', () => {
+      return expect(client.accounts.create({alias: 'david'})).to.be.rejectedWith('CH202')
+    })
+  })
+
+  describe('Batch account creation', () => {
+    let batchResponse = {}
+
+    before(() => client.accounts.createBatch([
+        {alias: `carol-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
+        {alias: 'david'}, // failure
+        {alias: `eve-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
+      ])
+      .then(resp => {batchResponse = resp})
+    )
+
+    it('returns two successes', () => assert.equal(batchResponse.successes[1], null))
+    it('returns one error', () => assert.deepEqual([batchResponse.errors[0], batchResponse.errors[2]], [null, null]))
+  })
+
+  describe('Single account tags update', () => {
+
+    it('successful', () => {
+      return client.accounts.updateTags({
+        alias: xAccountAlias,
+        tags: {x: 1},
+      })
+      .then(() => {
+        return client.accounts.query({
+          filter: `alias='${xAccountAlias}'`
+        })
+      })
+      .then(page => {
+        assert.deepEqual(page.items[0].tags, {x: 1})
       })
     })
 
-    describe('Batch account creation', () => {
-      let batchResponse = {}
-
-      before(() => client.accounts.createBatch([
-          {alias: `carol-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
-          {alias: 'david'}, // failure
-          {alias: `eve-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
-        ])
-        .then(resp => {batchResponse = resp})
-      )
-
-      it('returns two successes', () => assert.equal(batchResponse.successes[1], null))
-      it('returns one error', () => assert.deepEqual([batchResponse.errors[0], batchResponse.errors[2]], [null, null]))
-    })
-
-    describe('Single account tags update', () => {
-
-      it('successful', () => {
-        return client.accounts.updateTags({
-          alias: xAccountAlias,
+    it('rejected due to missing ID/Alias', () => {
+      return expect(
+        client.accounts.updateTags({
+          // ID/Alias intentionally omitted
           tags: {x: 1},
         })
-        .then(() => {
-          return client.accounts.query({
-            filter: `alias='${xAccountAlias}'`
-          })
-        })
-        .then(page => {
-          assert.deepEqual(page.items[0].tags, {x: 1})
+      ).to.be.rejectedWith('CH051')
+    })
+  })
+
+  describe('Batch account tags update', () => {
+
+    it('successful', () => {
+      return client.accounts.updateTagsBatch([{
+        alias: xAccountAlias,
+        tags: {x: 2},
+      }, {
+        alias: yAccountAlias,
+        tags: {y: 2},
+      }])
+      .then(() => {
+        return client.accounts.query({
+          filter: `alias='${xAccountAlias}' OR alias='${yAccountAlias}'`
         })
       })
-
-      it('rejected due to missing ID/Alias', () => {
-        return expect(
-          client.accounts.updateTags({
-            // ID/Alias intentionally omitted
-            tags: {x: 1},
-          })
-        ).to.be.rejectedWith('CH051')
+      .then(page => {
+        assert.deepEqual(page.items.find(i => i.alias.match(/^x-/)).tags, {x: 2})
+        assert.deepEqual(page.items.find(i => i.alias.match(/^y-/)).tags, {y: 2})
       })
     })
 
-    describe('Batch account tags update', () => {
-
-      it('successful', () => {
-        return client.accounts.updateTagsBatch([{
-          alias: xAccountAlias,
-          tags: {x: 2},
-        }, {
-          alias: yAccountAlias,
-          tags: {y: 2},
-        }])
-        .then(() => {
-          return client.accounts.query({
-            filter: `alias='${xAccountAlias}' OR alias='${yAccountAlias}'`
-          })
-        })
-        .then(page => {
-          assert.deepEqual(page.items.find(i => i.alias.match(/^x-/)).tags, {x: 2})
-          assert.deepEqual(page.items.find(i => i.alias.match(/^y-/)).tags, {y: 2})
-        })
-      })
-
-      it('fails to update accounts tags with missing ID/Alias', () => {
-        return client.accounts.updateTagsBatch([{
-          alias: xAccountAlias,
-          tags: {x: 3},
-        }, {
-          // ID/Alias intentionally omitted
-          tags: {y: 3}
-        }])
-        .then(batch => {
-          assert(batch.successes[0])
-          assert(!batch.successes[1])
-          assert(!batch.errors[0])
-          assert(batch.errors[1])
-        })
+    it('fails to update accounts tags with missing ID/Alias', () => {
+      return client.accounts.updateTagsBatch([{
+        alias: xAccountAlias,
+        tags: {x: 3},
+      }, {
+        // ID/Alias intentionally omitted
+        tags: {y: 3}
+      }])
+      .then(batch => {
+        assert(batch.successes[0])
+        assert(!batch.successes[1])
+        assert(!batch.errors[0])
+        assert(batch.errors[1])
       })
     })
   })
 
   // These just test that the callback is engaged correctly. Behavior is
   // tested in the promises test.
-  describe('Callback style', () => {
+  describe('Callback support', () => {
 
     it('Single account creation', (done) => {
       client.accounts.create(
---
+++ sdk/node/test/authorizationGrants.js
@@ -0,0 +1,111 @@
+/* eslint-env mocha */
+
+const chain = require('../dist/index.js')
+const uuid = require('uuid')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+const client = new chain.Client()
+
+let tokenName, tokenGrant
+
+describe('Authorization grant', () => {
+
+  before('set up grant data', () => {
+    tokenName = uuid.v4()
+    return client.accessTokens.create({ id: tokenName }).then(resp => {
+      tokenGrant = {
+        guard_data: { id: resp.id },
+        guard_type: 'access_token',
+        policy: 'client-readwrite'
+      }
+    })
+  })
+
+  it('creation successful', () => {
+    return client.authorizationGrants.create(tokenGrant)
+      .then(resp => expect(resp.createdAt).not.to.be.empty)
+  })
+
+  it('creation rejected due to invalid ID', () => {
+    return expect(client.authorizationGrants.create({
+      guard_data: { id: 'invalidId' },
+      guard_type: 'access_token',
+      policy: 'client-readwrite'
+    })).to.be.rejectedWith('CH303')
+  })
+
+  it('returned in list after creation', () => {
+    return client.authorizationGrants.create(tokenGrant)
+      .then(() => client.authorizationGrants.list())
+      .then(resp => expect(resp.items.map(item => item.guardData.id)).to.contain(tokenName))
+  })
+
+  it('deletion successful', () => {
+    return client.authorizationGrants.delete(tokenGrant)
+      .then(resp => expect(resp.message).to.equal('ok'))
+  })
+
+  it('removed from list after deletion', () => {
+    return client.authorizationGrants.create(tokenGrant)
+      .then(() => client.authorizationGrants.delete(tokenGrant))
+      .then(() => client.authorizationGrants.list())
+      .then(resp => expect(resp.items.map(item => item.guardData.id)).to.not.contain(tokenName))
+  })
+
+  it('sanitizes X509 guard data', () => {
+    return client.authorizationGrants.create({
+        guardType: 'x509',
+        guardData: {
+          subject: {
+            cn: tokenName,
+            ou: 'test-ou',
+          },
+        },
+        policy: 'client-readwrite',
+      })
+      .then(g => {
+        delete g.createdAt // ignore timestamp
+
+        expect(g).deep.equals({
+          guardType: 'x509',
+          guardData: {
+            subject: {
+              cn: tokenName,
+              ou: ['test-ou'],
+            }
+          },
+          policy: 'client-readwrite',
+          protected: false,
+        })
+      })
+  })
+
+  // These just test that the callback is engaged correctly. Behavior is
+  // tested in the promises test.
+  describe('Callback support', () => {
+
+    it('Authorization grant creation', (done) => {
+      client.authorizationGrants.create(
+        {}, // intentionally blank
+        () => done() // intentionally ignore errors
+      )
+    })
+
+    it('Authorization grant deletion', (done) => {
+      client.authorizationGrants.delete(
+        {}, // intentionally blank
+        () => done() // intentionally ignore errors
+      )
+    })
+
+    it('Authorization grant list', (done) => {
+      client.authorizationGrants.list(
+        () => done() // intentionally ignore errors
+      )
+    })
+  })
+})
