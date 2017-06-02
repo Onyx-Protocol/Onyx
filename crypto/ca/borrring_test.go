da9a6c2b3541b5edb4eb15b833f3a8e1834b7e74
Oleg Andreev <oleganza@gmail.com>
2017-06-02 14:09:57 -0700

    one more good test for M=2

--- crypto/ca/borrring_test.go
+++ crypto/ca/borrring_test.go
@@ -80,6 +80,49 @@ func TestBorrRingSig121(t *testing.T) {
 	}
 }
 
+// n=1 ring, m=1 sig, M=2 base points
+func TestBorrRingSig112(t *testing.T) {
+	msg := []byte("message")
+
+	pbytes := [][]string{
+		[]string{
+			"b5ea5caeec9dafa11a337b0e047166c159e10a1f37f2f0e4a1f289f2d6d83736",
+		},
+	}
+
+	B := []ecmath.Point{G, J}
+
+	n := len(pbytes)
+	m := len(pbytes[0])
+	M := len(B)
+
+	p := make([]ecmath.Scalar, n)
+	P := make([][][]ecmath.Point, n)
+	for i := 0; i < n; i++ {
+		P[i] = make([][]ecmath.Point, m)
+		for j := 0; j < m; j++ {
+			var p2 ecmath.Scalar
+			hex.Decode(p2[:], []byte(pbytes[i][j]))
+
+			P[i][j] = make([]ecmath.Point, M)
+
+			for u := 0; u < M; u++ {
+				P[i][j][u].ScMul(&B[u], &p2)
+			}
+
+			if j == 0 {
+				p[i] = p2
+			}
+		}
+	}
+	j := make([]uint64, n) // n zeroes
+	payload := make([][32]byte, m*n)
+	brs := CreateBorromeanRingSignature(msg, B, P, p, j, payload)
+	if !brs.Validate(msg, B, P) {
+		t.Error("failed to validate borromean ring signature")
+	}
+}
+
 // n=1 ring, m=2 sig, M=1 base point
 func TestBorrRingSig321(t *testing.T) {
 	msg := []byte("message")
