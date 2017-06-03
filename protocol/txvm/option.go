5c929fa540002b28654fa172f2bd772186f708aa
chainbot <ops@chain.com>
2017-06-02 18:19:20 -0700

    remove stale comment

--- protocol/txvm/option.go
+++ protocol/txvm/option.go
@@ -23,10 +23,6 @@ func TraceLock(f func(Contract)) Option {
 }
 
 func TraceOp(f func(stack data.List, prog []byte)) Option {
-	// TODO(kr): currently it is impossible for a client
-	// package to provide f because they can't import
-	// our internal package ./internal/data.
-	// Figure out some way to resolve that.
 	// TODO(kr): provide other state too (if necessary?)
 	return func(vm *vm) {
 		vm.traceOp = f
