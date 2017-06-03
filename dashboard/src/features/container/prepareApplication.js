c9434ad198b25c6898bc661f0de94e8f89903171
Dominic Dagradi <dominic@chain.com>
2017-06-02 18:12:37 -0700

    route refactor

--- dashboard/src/features/container/prepareApplication.js
+++ dashboard/src/features/container/prepareApplication.js
@@ -1,5 +1,3 @@
-import Container from './components/Container'
-
 import { getAuthenticationStatus } from 'features/authn/actions'
 import { fetchCoreInfo } from 'features/core/actions'
 import { fetchTestnetInfo } from 'features/testnet/actions'
@@ -7,10 +5,8 @@ import { fetchTestnetInfo } from 'features/testnet/actions'
 const CORE_POLLING_TIME = 2 * 1000
 const TESTNET_INFO_POLLING_TIME = 30 * 1000
 
-export default (store) => ({
-  path: '/',
-  component: Container,
-  onEnter: () => {
+export default (store) => {
+  return () => {
     store.dispatch(getAuthenticationStatus())
     store.dispatch(fetchTestnetInfo())
 
@@ -21,4 +17,4 @@ export default (store) => ({
     }, CORE_POLLING_TIME)
     setInterval(() => store.dispatch(fetchTestnetInfo()), TESTNET_INFO_POLLING_TIME)
   }
-})
+}
--- dashboard/src/routes.js
+++ dashboard/src/routes.js
@@ -1,45 +1,18 @@
-import { routes as container } from 'features/container'
-import accessControl from 'features/accessControl/routes'
-import { routes as accounts } from 'features/accounts'
-import { routes as assets } from 'features/assets'
+import Container from 'features/container/components/Container'
+import prepareApplication from 'features/container/prepareApplication'
 import { routes as authn } from 'features/authn'
-import { routes as balances } from 'features/balances'
 import { routes as configuration } from 'features/configuration'
-import { routes as core } from 'features/core'
-import { routes as transactions } from 'features/transactions'
-import { routes as transactionFeeds } from 'features/transactionFeeds'
-import { routes as unspents } from 'features/unspents'
-import { routes as mockhsm } from 'features/mockhsm'
-import { NotFound } from 'features/shared/components'
-
-import Main from 'features/app/components/Main/Main'
-
+import appRoutes from 'features/app/appRoutes'
 
 const makeRoutes = (store) => {
   return({
-    ...container(store),
+    path: '/',
+    component: Container,
+    onEnter: prepareApplication(store),
     childRoutes: [
       authn(store),
       configuration,
-      {
-        useForBreadcrumbs: true, // key for app functions inspecting routes
-        component: Main,
-        childRoutes: [
-          transactions(store),
-          accessControl(store),
-          accounts(store),
-          assets(store),
-          balances(store),
-          core,
-          transactionFeeds(store),
-          unspents(store),
-          mockhsm(store),
-          {
-            path: '*',
-            component: NotFound
-          }
-        ]
-      },
+      appRoutes(store),
     ]
   })
 }
