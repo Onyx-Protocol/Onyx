f4f7f35e3b5b3a09bb63ca6cf85393b9f9947034
Chris Garvin <chris.garvin@me.com>
2017-06-05 15:29:57 -0700

    sdk/node: create authorization grant specific test
    Closes #1276

--- sdk/node/test/accessTokens.js
+++ sdk/node/test/accessTokens.js
@@ -10,143 +10,145 @@ const expect = chai.expect
 
 const client = new chain.Client()
 
-let tokenId, clientTokenId, networkTokenId
+function createToken() {
+  return client.accessTokens.create({
+    id: `token-${uuid.v4()}`
+  })
+}
 
-describe('Access tokens test', () => {
+describe('Access token', () => {
 
-  describe('Promise style', () => {
+  it('creation successful', () => {
+    return client.accessTokens.create({
+      id: `another-${uuid.v4()}`
+    }).then(resp => expect(resp.token).not.to.be.empty)
+  })
 
-    describe('Access token', () => {
+  it('creation rejected due to duplicate ID', () => {
+    return createToken()
+      .then((token) => expect(client.accessTokens.create({
+      id: token.id
+    })).to.be.rejectedWith('CH302'))
+  })
 
-      beforeEach(() => {
-        tokenId = `token-${uuid.v4()}`
-        return client.accessTokens.create({
-          type: 'client',
-          id: tokenId
-        })
+  it('returned in list after creation', () => {
+    let tokenId
+    return createToken()
+      .then((token) => {
+        tokenId = token.id
+        return client.accessTokens.query()
       })
+      .then(resp => expect(resp.items.map(item => item.id)).to.contain(tokenId))
+  })
+
+  it('deletion successful', () => {
+    return createToken()
+      .then((token) => client.accessTokens.delete(token.id))
+      .then(resp => expect(resp.message).to.equal('ok'))
+  })
+
+  it('deletion rejected due to missing ID', () => {
+    return createToken()
+      .then(() => expect(client.accessTokens.delete())
+      .to.be.rejectedWith('CH310'))
+  })
+
+  it('removed from list after deletion', () => {
+    let tokenId
+    return createToken()
+      .then((token) => {
+        tokenId = token.id
+        return client.accessTokens.delete(tokenId)
+      })
+      .then(() => client.accessTokens.query())
+      .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(tokenId))
+  })
+
+  describe('Deprecated syntax', () => {
+    let clientTokenId, networkTokenId
+
+    describe('Client token', () => {
 
       it('creation successful', () => {
         return client.accessTokens.create({
           type: 'client',
-          id: `another-${tokenId}`
-        }).then(resp => expect(resp.token).not.to.be.empty)
+          id: `client-${uuid.v4()}`
+        }).then(resp => expect(resp.type).to.equal('client'))
       })
 
-      it('creation rejected due to duplicate ID', () => {
-        return expect(client.accessTokens.create({
+      it('creation adds client-readwrite grant', () => {
+        return client.accessTokens.create({
           type: 'client',
-          id: tokenId
-        })).to.be.rejectedWith('CH302')
-      })
-
-      it('returned in list after creation', () => {
-        return client.accessTokens.query()
-          .then(resp => expect(resp.items.map(item => item.id)).to.contain(tokenId))
-      })
-
-      it('deletion successful', () => {
-        return client.accessTokens.delete(tokenId)
-          .then(resp => expect(resp.message).to.equal('ok'))
-      })
-
-      it('deletion rejected due to missing ID', () => {
-        return expect(client.accessTokens.delete())
-          .to.be.rejectedWith('CH310')
-      })
-
-      it('removed from list after deletion', () => {
-        return client.accessTokens.delete(tokenId)
-          .then(() => client.accessTokens.query())
-          .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(tokenId))
+          id: `client-${uuid.v4()}`
+        }).then((resp) => {
+          clientTokenId = resp.id
+          return client.authorizationGrants.list()
+        })
+          .then(resp => expect(resp.items.filter(guard => guard.guardData['id'] == clientTokenId)[0].policy).to.equal('client-readwrite'))
       })
     })
 
-    describe('Deprecated syntax', () => {
+    describe('Network token', () => {
 
-      describe('Client token', () => {
-
-        it('creation successful', () => {
-          return client.accessTokens.create({
-            type: 'client',
-            id: `client-${uuid.v4()}`
-          }).then(resp => expect(resp.type).to.equal('client'))
-        })
-
-        it('creation adds client-readwrite grant', () => {
-          return client.accessTokens.create({
-            type: 'client',
-            id: `client-${uuid.v4()}`
-          }).then((resp) => {
-            clientTokenId = resp.id
-            return client.authorizationGrants.list()
-          })
-            .then(resp => expect(resp.items.filter(guard => guard.guardData['id'] == clientTokenId)[0].policy).to.equal('client-readwrite'))
-        })
+      it('creation successful', () => {
+        return client.accessTokens.create({
+          type: 'network',
+          id: `network-${uuid.v4()}`
+        }).then(resp => expect(resp.type).to.equal('network'))
       })
 
-      describe('Network token', () => {
-
-        it('creation successful', () => {
-          return client.accessTokens.create({
-            type: 'network',
-            id: `network-${uuid.v4()}`
-          }).then(resp => expect(resp.type).to.equal('network'))
+      it('creation adds crosscore grant', () => {
+        return client.accessTokens.create({
+          type: 'network',
+          id: `network-${uuid.v4()}`
+        }).then((resp) => {
+          networkTokenId = resp.id
+          return client.authorizationGrants.list()
         })
+          .then(resp => expect(resp.items.filter(guard => guard.guardData['id'] == networkTokenId)[0].policy).to.equal('crosscore'))
+      })
+    })
 
-        it('creation adds crosscore grant', () => {
-          return client.accessTokens.create({
-            type: 'network',
-            id: `network-${uuid.v4()}`
-          }).then((resp) => {
-            networkTokenId = resp.id
-            return client.authorizationGrants.list()
-          })
-            .then(resp => expect(resp.items.filter(guard => guard.guardData['id'] == networkTokenId)[0].policy).to.equal('crosscore'))
-        })
+    describe('Deprecated roles', () => {
+
+      beforeEach(() => {
+        clientTokenId = `client-${uuid.v4()}`
+        networkTokenId = `network-${uuid.v4()}`
+
+        return client.accessTokens.create({
+          type: 'client',
+          id: clientTokenId
+        }).then(() => client.accessTokens.create({
+          type: 'network',
+          id: networkTokenId
+        }))
       })
 
-      describe('Deprecated roles', () => {
+      it('filtered by client type contains clientTokenId', () => {
+        return client.accessTokens.query({type: 'client'})
+          .then(resp => expect(resp.items.map(item => item.id)).to.contain(clientTokenId))
+      })
 
-        beforeEach(() => {
-          clientTokenId = `client-${uuid.v4()}`
-          networkTokenId = `network-${uuid.v4()}`
+      it('filtered by client type does not contain networkTokenId', () => {
+        return client.accessTokens.query({type: 'client'})
+          .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(networkTokenId))
+      })
 
-          return client.accessTokens.create({
-            type: 'client',
-            id: clientTokenId
-          }).then(() => client.accessTokens.create({
-            type: 'network',
-            id: networkTokenId
-          }))
-        })
+      it('filtered by network type contains networkTokenId', () => {
+        return client.accessTokens.query({type: 'network'})
+          .then(resp => expect(resp.items.map(item => item.id)).to.contain(networkTokenId))
+      })
 
-        it('filtered by client type contains clientTokenId', () => {
-          return client.accessTokens.query({type: 'client'})
-            .then(resp => expect(resp.items.map(item => item.id)).to.contain(clientTokenId))
-        })
-
-        it('filtered by client type does not contain networkTokenId', () => {
-          return client.accessTokens.query({type: 'client'})
-            .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(networkTokenId))
-        })
-
-        it('filtered by network type contains networkTokenId', () => {
-          return client.accessTokens.query({type: 'network'})
-            .then(resp => expect(resp.items.map(item => item.id)).to.contain(networkTokenId))
-        })
-
-        it('filtered by network type does not contain clientTokenId', () => {
-          return client.accessTokens.query({type: 'network'})
-            .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(clientTokenId))
-        })
+      it('filtered by network type does not contain clientTokenId', () => {
+        return client.accessTokens.query({type: 'network'})
+          .then(resp => expect(resp.items.map(item => item.id)).to.not.contain(clientTokenId))
       })
     })
   })
 
   // These just test that the callback is engaged correctly. Behavior is
   // tested in the promises test.
-  describe('Callback style', () => {
+  describe('Callback support', () => {
 
     it('Access token creation', (done) => {
       client.accessTokens.create(
--- sdk/node/test/assets.js
+++ sdk/node/test/assets.js
@@ -15,119 +15,116 @@ const yAssetAlias = `y-${uuid.v4()}`
 
 let mockHsmKey
 
-describe('Assets test', () => {
+describe('Asset', () => {
 
-  describe('Promise style', () => {
+  before('set up API objects', () => {
 
-    before('set up API objects', () => {
-
-      // Key and asset creation
-      return client.mockHsm.keys.create()
-        .then(key => { mockHsmKey = key })
-        .then(() => {
-          return client.assets.create({alias: xAssetAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {x: 0}})
-        })
-        .then(() => {
-          return client.assets.create({alias: yAssetAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {y: 0}})
-        })
-    })
-
-    describe('Single asset creation', () => {
-
-      it('successful', () => {
-        return client.assets.create({alias: `asset-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1})
-          .then(resp => expect(resp.id).not.to.be.empty)
+    // Key and asset creation
+    return client.mockHsm.keys.create()
+      .then(key => { mockHsmKey = key })
+      .then(() => {
+        return client.assets.create({alias: xAssetAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {x: 0}})
       })
+      .then(() => {
+        return client.assets.create({alias: yAssetAlias, rootXpubs: [mockHsmKey.xpub], quorum: 1, tags: {y: 0}})
+      })
+  })
 
-      it('rejected due to missing key fields', () => {
-        return expect(client.assets.create({alias: 'asset'})).to.be.rejectedWith('CH202')
+  describe('Single asset creation', () => {
+
+    it('successful', () => {
+      return client.assets.create({alias: `asset-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1})
+        .then(resp => expect(resp.id).not.to.be.empty)
+    })
+
+    it('rejected due to missing key fields', () => {
+      return expect(client.assets.create({alias: 'asset'})).to.be.rejectedWith('CH202')
+    })
+  })
+
+  describe('Batch asset creation', () => {
+    let batchResponse = {}
+
+    before(() => client.assets.createBatch([
+        {alias: `bronze-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
+        {alias: 'unobtanium'}, // failure
+        {alias: `copper-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
+      ])
+      .then(resp => {batchResponse = resp})
+    )
+
+    it('returns two successes', () => assert.equal(batchResponse.successes[1], null))
+    it('returns one error', () => assert.deepEqual([batchResponse.errors[0], batchResponse.errors[2]], [null, null]))
+  })
+
+  describe('Single asset tags update', () => {
+
+    it('successful', () => {
+      return client.assets.updateTags({
+        alias: xAssetAlias,
+        tags: {x: 1},
+      })
+      .then(() => {
+        return client.assets.query({
+          filter: `alias='${xAssetAlias}'`
+        })
+      })
+      .then(page => {
+        assert.deepEqual(page.items[0].tags, {x: 1})
       })
     })
 
-    describe('Batch asset creation', () => {
-      let batchResponse = {}
-
-      before(() => client.assets.createBatch([
-          {alias: `bronze-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
-          {alias: 'unobtanium'}, // failure
-          {alias: `copper-${uuid.v4()}`, rootXpubs: [mockHsmKey.xpub], quorum: 1}, // success
-        ])
-        .then(resp => {batchResponse = resp})
-      )
-
-      it('returns two successes', () => assert.equal(batchResponse.successes[1], null))
-      it('returns one error', () => assert.deepEqual([batchResponse.errors[0], batchResponse.errors[2]], [null, null]))
-    })
-
-    describe('Single asset tags update', () => {
-
-      it('successful', () => {
-        return client.assets.updateTags({
-          alias: xAssetAlias,
+    it('rejected due to missing ID/Alias', () => {
+      return expect(
+        client.assets.updateTags({
+          // ID/Alias intentionally omitted
           tags: {x: 1},
         })
-        .then(() => {
-          return client.assets.query({
-            filter: `alias='${xAssetAlias}'`
-          })
-        })
-        .then(page => {
-          assert.deepEqual(page.items[0].tags, {x: 1})
+      ).to.be.rejectedWith('CH051')
+    })
+  })
+
+  describe('Batch asset tags update', () => {
+
+    it('successful', () => {
+      return client.assets.updateTagsBatch([{
+        alias: xAssetAlias,
+        tags: {x: 2},
+      }, {
+        alias: yAssetAlias,
+        tags: {y: 2},
+      }])
+      .then(() => {
+        return client.assets.query({
+          filter: `alias='${xAssetAlias}' OR alias='${yAssetAlias}'`
         })
       })
-
-      it('rejected due to missing ID/Alias', () => {
-        return expect(
-          client.assets.updateTags({
-            // ID/Alias intentionally omitted
-            tags: {x: 1},
-          })
-        ).to.be.rejectedWith('CH051')
+      .then(page => {
+        assert.deepEqual(page.items.find(i => i.alias.match(/^x-/)).tags, {x: 2})
+        assert.deepEqual(page.items.find(i => i.alias.match(/^y-/)).tags, {y: 2})
       })
     })
 
-    describe('Batch asset tags update', () => {
-
-      it('successful', () => {
-        return client.assets.updateTagsBatch([{
-          alias: xAssetAlias,
-          tags: {x: 2},
-        }, {
-          alias: yAssetAlias,
-          tags: {y: 2},
-        }])
-        .then(() => {
-          return client.assets.query({
-            filter: `alias='${xAssetAlias}' OR alias='${yAssetAlias}'`
-          })
-        })
-        .then(page => {
-          assert.deepEqual(page.items.find(i => i.alias.match(/^x-/)).tags, {x: 2})
-          assert.deepEqual(page.items.find(i => i.alias.match(/^y-/)).tags, {y: 2})
-        })
-      })
-
-      it('fails to update assets tags with missing ID/Alias', () => {
-        return client.assets.updateTagsBatch([{
-          alias: xAssetAlias,
-          tags: {x: 3},
-        }, {
-          // ID/Alias intentionally omitted
-          tags: {y: 3}
-        }])
-        .then(batch => {
-          assert(batch.successes[0])
-          assert(!batch.successes[1])
-          assert(!batch.errors[0])
-          assert(batch.errors[1])
-        })
+    it('fails to update assets tags with missing ID/Alias', () => {
+      return client.assets.updateTagsBatch([{
+        alias: xAssetAlias,
+        tags: {x: 3},
+      }, {
+        // ID/Alias intentionally omitted
+        tags: {y: 3}
+      }])
+      .then(batch => {
+        assert(batch.successes[0])
+        assert(!batch.successes[1])
+        assert(!batch.errors[0])
+        assert(batch.errors[1])
       })
     })
   })
 
   // These just test that the callback is engaged correctly. Behavior is
   // tested in the promises test.
-  describe('Callback style', () => {
+  describe('Callback support', () => {
 
     it('Single asset creation', (done) => {
       client.assets.create(
