cdd0e4eab587440240f5721793a0bca6bb757434
Jeff Lee <jeffomatic@gmail.com>
2017-06-09 16:33:47 -0700

    sdk/node/test: add UTXO tests

---
+++ sdk/node/test/unspentOutputs.js
@@ -0,0 +1,74 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+const {
+  client,
+  createAccount,
+  createAsset,
+  buildSignSubmit,
+} = require('../testHelpers/util')
+
+describe('Unspent output', () => {
+  let asset, account1, account2, tx1, tx2
+
+  before(() =>
+    Promise.all([
+      createAsset(),
+      createAccount(),
+      createAccount(),
+    ]).then(res =>
+      [asset, account1, account2] = res
+    ).then(() =>
+      buildSignSubmit(b => {
+        b.issue({assetId: asset.id, amount: 1})
+        b.controlWithAccount({
+          accountId: account1.id,
+          assetId: asset.id,
+          amount: 1,
+        })
+      }).then(tx => tx1 = tx.id)
+    ).then(() =>
+      buildSignSubmit(b => {
+        b.issue({assetId: asset.id, amount: 1})
+        b.controlWithAccount({
+          accountId: account2.id,
+          assetId: asset.id,
+          amount: 1,
+        })
+      }).then(tx => tx2 = tx.id)
+    )
+  )
+
+  describe('Query with filter', () => {
+    it('simple example', () => {
+      client.unspentOutputs.query({
+        filter: "account_id=$1",
+        filterParams: [account1.id],
+      }).then(page =>
+        expect(page.items.map(output => output.transactionId)).to.include(tx1)
+      )
+    })
+  })
+
+  describe('queryAll', () => {
+    it('simple example', () => {
+      let queried = []
+
+      client.unspentOutputs.queryAll({
+        filter: "asset_id=$1",
+        filterParams: [asset.id],
+      }, (utxo, next, done) => {
+        queried.push(utxo.transactionId)
+        next()
+      }).then(() => {
+        expect(queried).to.include(tx1)
+        expect(queried).to.include(tx2)
+      })
+    })
+  })
+
+})
