14d1aeb2858b0ea53f3d55563a62e10d88bb9b4c
Dominic Dagradi <dominic@chain.com>
2017-06-12 14:16:02 -0700

    extract default tls config

---
+++ net/tls.go
@@ -0,0 +1,18 @@
+package net
+
+import "crypto/tls"
+
+// This is gtank's cryptopasta defaults
+func DefaultTLSConfig() *tls.Config {
+	return &tls.Config{
+		// Avoids most of the memorably-named TLS attacks
+		MinVersion: tls.VersionTLS12,
+		// Causes servers to use Go's default ciphersuite preferences,
+		// which are tuned to avoid attacks. Does nothing on clients.
+		PreferServerCipherSuites: true,
+		// Only use curves which have constant-time implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+		},
+	}
+}
