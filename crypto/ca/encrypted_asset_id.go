dff6c7deb50acb7978aac79d502cbe47f2bb5614
Oleg Andreev <oleganza@gmail.com>
2017-06-15 12:59:49 -0700

    fixed API and added tests for EncryptedAssetID

--- crypto/ca/encrypted_asset_id.go
+++ crypto/ca/encrypted_asset_id.go
@@ -7,9 +7,9 @@ import (
 // EncryptedAssetIDSize is size in bytes of an encrypted asset ID and its blinding factor
 const EncryptedAssetIDSize = 64
 
-// EncryptAsset encrypts value and blinding factor f
+// EncryptAssetID encrypts value and blinding factor f
 // using encryption key vek to the output buffer evef.
-func EncryptAsset(ac *AssetCommitment, assetID AssetID, c *ecmath.Scalar, aek AssetKey, eaec []byte) {
+func EncryptAssetID(ac *AssetCommitment, assetID AssetID, c *ecmath.Scalar, aek AssetKey, eaec []byte) {
 	if len(eaec) != EncryptedAssetIDSize {
 		panic("Invalid buffer size for the encrypted asset ID: should have EncryptedAssetIDSize bytes.")
 	}
@@ -24,8 +24,8 @@ func EncryptAsset(ac *AssetCommitment, assetID AssetID, c *ecmath.Scalar, aek As
 	xorSlices(c[:], eaec[32:64], eaec[32:64])
 }
 
-// DecryptAsset decrypts eaec using key aek and verifies it using ID commitment ac.
-func DecryptAsset(eaec []byte, vc *ValueCommitment, ac *AssetCommitment, aek AssetKey) (assetID AssetID, c *ecmath.Scalar, ok bool) {
+// DecryptAssetID decrypts eaec using key aek and verifies it using ID commitment ac.
+func DecryptAssetID(eaec []byte, ac *AssetCommitment, aek AssetKey) (assetID AssetID, c *ecmath.Scalar, ok bool) {
 	if len(eaec) != EncryptedAssetIDSize {
 		return AssetID{}, &ecmath.Zero, false
 	}
@@ -54,33 +54,3 @@ func DecryptAsset(eaec []byte, vc *ValueCommitment, ac *AssetCommitment, aek Ass
 	// 6. Return `(assetID, c)`.
 	return assetID, c, true
 }
-
-// func (enc EncryptedAssetID) Decrypt(
-// 	H AssetCommitment,
-// 	aek AssetKey,
-// ) (assetID AssetID, c Scalar, err error) {
-
-// 	// 1. Expand the encryption key: `ek = SHA3-512(aek || H’)`, split the resulting hash in two halves.
-// 	ek := hash512(aek[:], H.Bytes())
-
-// 	// 2. Decrypt the asset ID using the first half: `assetID = ea XOR ek[0,32]`.
-// 	assetID = AssetID(xor256(enc.AssetID[:], ek[0:32]))
-
-// 	// 3. Decrypt the cumulative blinding factor using the second half: `c = ec XOR ek[32,32]`.
-// 	c = xor256(enc.BlindingFactor[:], ek[32:64])
-
-// 	// 4. Calculate `A` as a nonblinded asset ID commitment:
-// 	//    an elliptic curve point `8*decode(SHA3-256(assetID))`.
-// 	A := CreateNonblindedAssetCommitment(assetID)
-
-// 	// 5. Calculate point `P = A + c*G` where `c` is interpreted as a little-endian 256-bit integer.
-// 	P := multiplyAndAddPoint(one, Point(A), c)
-
-// 	// 6. Verify that `P` equals target commitment `H’`. If not, halt and return `nil`.
-// 	if encodePoint(&P) != encodePoint((*Point)(&H)) {
-// 		return assetID, c, errors.New("Asset ID decryption failed")
-// 	}
-
-// 	// 7. Return `(assetID, c)`.
-// 	return assetID, c, nil
-// }
--- crypto/ca/encrypted_asset_id_test.go
+++ crypto/ca/encrypted_asset_id_test.go
@@ -1,1 +1,52 @@
 package ca
+
+import (
+	"bytes"
+	"chain/crypto/ed25519/ecmath"
+	"testing"
+)
+
+func TestEncryptAssetID(t *testing.T) {
+	assetID := AssetID{0xff}
+	aek := []byte("asset id encryption key")
+
+	ac, c := CreateAssetCommitment(assetID, aek)
+
+	eaec := make([]byte, EncryptedAssetIDSize)
+	EncryptAssetID(ac, assetID, c, aek, eaec)
+
+	assetID2, c2, ok := DecryptAssetID(eaec, ac, aek)
+
+	if !ok {
+		t.Error("decryption failed")
+	} else {
+		if assetID2 != assetID {
+			t.Errorf("got asset ID %x, want %x", assetID2, assetID)
+		}
+		if !c.Equal(c2) {
+			t.Errorf("got blinding factor %x, want %x", c2[:], c[:])
+		}
+	}
+
+	for i := 0; i < len(eaec); i++ {
+		for j := uint(0); j < 8; j++ {
+			eaec[i] ^= 1 << j
+
+			assetID2, c2, ok := DecryptAssetID(eaec, ac, aek)
+
+			if ok {
+				t.Error("unexpected decryption success with bad encrypted value amount")
+			}
+
+			if bytes.Equal(assetID[:], assetID2[:]) {
+				t.Error("unexpected value from failed decryption")
+			}
+
+			if !c2.Equal(&ecmath.Zero) {
+				t.Error("unexpected value from failed decryption")
+			}
+
+			eaec[i] ^= 1 << j
+		}
+	}
+}
