6eef1947cd424ab137bd8106116193ac309dd7b1
Vicki Niu <vicki.niu@gmail.com>
2017-06-21 14:25:22 -0700

    Can join cluster, configuration not working

--- net/raft/dev/run0.sh
+++ net/raft/dev/run0.sh
@@ -3,12 +3,17 @@
 # Enables job control to allow backgrounding & foregrounding cored process
 set -m
 
+# Move TLS certs to chaincore0
+mkdir $HOME/.chaincore0
+cp $CHAIN_CORE_HOME/tls.* $HOME/.chaincore0/
+
 # Start initial node
-LISTEN=127.0.0.1:1999 \
+LISTEN=localhost:1999 \
 CHAIN_CORE_HOME=$HOME/.chaincore0 \
 DATABASE_URL=postgres:///core0?sslmode=disable \
-cored & 
+cored &
+
 corectl init
-corectl allow-address 127.0.0.1:1998
-corectl allow-address 127.0.0.1:1997
+corectl allow-address localhost:1998
+corectl allow-address localhost:1997
 fg
