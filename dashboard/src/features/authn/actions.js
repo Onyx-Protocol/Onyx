11c3855755e62d392c3b3776f744c1e8b70a3692
Dominic Dagradi <ddagradi@gmail.com>
2017-06-26 14:55:22 -0700

    dashboard: poll for core connection before determining authn status
    Closes #1353

--- dashboard/src/features/authn/actions.js
+++ dashboard/src/features/authn/actions.js
@@ -9,22 +9,28 @@ export const logIn = (token) => (dispatch) => {
 }
 
 export const getAuthenticationStatus = () => {
-  return (dispatch) => {
+  let connectedToCore = false
+  const pingAuth = (dispatch) => {
     return unauthedClient().config.info()
       .then(() => {
+        connectedToCore = true
         dispatch({type: 'CONNECTED_TO_CORE'})
       }, (err) => {
         if (err.status < 500) {
+          connectedToCore = true
           dispatch({type: 'CONNECTED_TO_CORE'})
         } else {
           dispatch({type: 'CORE_DISCONNECT'})
+          setTimeout(() => pingAuth(dispatch), 1000)
         }
 
         if (err.status == 401) {
           dispatch({type: 'AUTHENTICATION_REQUIRED'})
         }
       })
-      .then(() => dispatch(fetchCoreInfo()))
-      .then(() => dispatch({type: 'AUTHENTICATION_READY'}))
+      .then(() => connectedToCore && dispatch(fetchCoreInfo()))
+      .then(() => connectedToCore && dispatch({type: 'AUTHENTICATION_READY'}))
   }
+
+  return pingAuth
 }
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "main/rev3272".freeze
+	ID = "main/rev3273".freeze
 end
