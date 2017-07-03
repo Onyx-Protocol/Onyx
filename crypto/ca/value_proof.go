ec98908c1b5532fc0c855eec6bbf6e8be5eaddd6
Oleg Andreev <oleganza@gmail.com>
2017-07-03 16:06:22 +0300

    implemented value proof in terms of AssetIDProof and AmountProof

--- crypto/ca/value_proof.go
+++ crypto/ca/value_proof.go
@@ -10,19 +10,11 @@ func CreateAssetIDProof(
 	c ecmath.Scalar,
 	message []byte,
 ) []byte {
-
 	if c.Equal(&ecmath.Zero) {
 		return []byte{}
 	}
-
-	// 1. Compute a message hash to be signed:
-	//         h = Hash256("AssetIDProof", {assetid, AC, message})
 	h := hash256("ChainCA.AssetIDProof", assetID[:], (*PointPair)(ac).Bytes(), message)
-
-	// 2. [Create excess commitment](#create-excess-commitment) `E` using scalar `c` and message `h`.
 	e := CreateExcessCommitment(c, h[:])
-
-	// 4. Return Schnorr signature extracted from the excess commitment (last 64 bytes):
 	return e.signatureBytes()
 }
 
@@ -60,21 +52,68 @@ func ValidateAssetIDProof(
 		message)
 	//     2. Subtract `A` from the first point of `AC` and leave second point unmodified:
 	//             Q = AC - (A,O)
-	Q1 := *ac
-	Q1[0].Sub(&Q1[0], (*ecmath.Point)(&a))
+	Q := *ac
+	Q[0].Sub(&Q[0], (*ecmath.Point)(&a))
 
 	//     3. [Validate excess commitment](#validate-excess-commitment) `Q || p || h`.
 	var e ExcessCommitment
-	e.QC = PointPair(Q1)
+	e.QC = PointPair(Q)
 	e.setSignatureBytes(proof)
 	e.msg = h[:]
 	return e.Validate()
 }
 
-// ValueProofSize is a size of the value proof in bytes
-const ValueProofSize = 128
+// CreateAmountProof creates a proof that a specific numeric value is
+// commited by a given value commitment.
+func CreateAmountProof(
+	value uint64,
+	ac *AssetCommitment,
+	vc *ValueCommitment,
+	f ecmath.Scalar,
+	message []byte,
+) []byte {
+	if f.Equal(&ecmath.Zero) {
+		return []byte{}
+	}
+	h := hash256("ChainCA.AmountProof", uint64le(value), (*PointPair)(ac).Bytes(), (*PointPair)(vc).Bytes(), message)
+	e := CreateExcessCommitment(f, h[:])
+	return e.signatureBytes()
+}
 
-type ValueProof [ValueProofSize]byte
+// ValidateAmountProof checks a proof that a specific numeric value is
+// commited by a given value commitment.
+func ValidateAmountProof(
+	value uint64,
+	ac *AssetCommitment,
+	vc *ValueCommitment,
+	message []byte,
+	proof []byte,
+) bool {
+
+	if !(len(proof) == 0 || len(proof) == 64) {
+		return false
+	}
+
+	v := (&ecmath.Scalar{}).SetUint64(value)
+
+	vcPrime := *(*PointPair)(ac)
+	vcPrime.ScMul(&vcPrime, v)
+
+	if len(proof) == 0 {
+		return (*PointPair)(vc).ConstTimeEqual(&vcPrime)
+	}
+
+	h := hash256("ChainCA.AmountProof", uint64le(value), (*PointPair)(ac).Bytes(), (*PointPair)(vc).Bytes(), message)
+	Q := *(*PointPair)(vc)
+	Q.Sub(&Q, &vcPrime)
+
+	// 3. [Validate excess commitment](#validate-excess-commitment) `Q || p || h`.
+	var e ExcessCommitment
+	e.QC = PointPair(Q)
+	e.setSignatureBytes(proof)
+	e.msg = h[:]
+	return e.Validate()
+}
 
 // CreateValueProof creates a proof of a specifc asset ID and value bound to
 // the given asset ID commitment and value commitment.
@@ -86,77 +125,27 @@ func CreateValueProof(
 	c ecmath.Scalar,
 	f ecmath.Scalar,
 	message []byte,
-) *ValueProof {
-	// 1. Compute a message hash to be signed:
-	//         h = Hash256("ValueProof", {assetid, uint64le(value), AC, VC, message})
-	h := hash256("ChainCA.ValueProof",
-		assetID[:],
-		uint64le(value),
-		(*PointPair)(ac).Bytes(),
-		(*PointPair)(vc).Bytes(),
-		message)
+) [][]byte {
 
-	// 2. [Create excess commitment](#create-excess-commitment) `E1` using scalar `c` and message `h`.
-	e1 := CreateExcessCommitment(c, h[:])
-
-	// 3. [Create excess commitment](#create-excess-commitment) `E2` using scalar `f` and message `h`.
-	e2 := CreateExcessCommitment(f, h[:])
-
-	// 4. Return concatenation of Schnorr signatures extracted from excess commitments (last 64 bytes from the each excess commitment):
-	//         vp = E1[64:128] || E2[64:128]
-	var result [ValueProofSize]byte
-	copy(result[:64], e1.signatureBytes())
-	copy(result[64:], e2.signatureBytes())
-
-	return (*ValueProof)(&result)
+	return [][]byte{
+		CreateAssetIDProof(assetID, ac, c, message),
+		CreateAmountProof(value, ac, vc, f, message),
+	}
 }
 
 // ValidateValueProof checks if a given proof vp actually proves that commitments
 // ac and vc commit to a given assetID and value.
-func (vp *ValueProof) Validate(
+func ValidateValueProof(
 	assetID AssetID,
 	value uint64,
 	ac *AssetCommitment,
 	vc *ValueCommitment,
 	message []byte,
+	proof [][]byte,
 ) bool {
-	// 2. Compute a message hash to be signed:
-	//         h = Hash256("ValueProof", {assetid, uint64le(value), AC, VC, message})
-	h := hash256("ChainCA.ValueProof",
-		assetID[:],
-		uint64le(value),
-		(*PointPair)(ac).Bytes(),
-		(*PointPair)(vc).Bytes(),
-		message)
-
-	// 3. Compute [asset ID point](#asset-id-point): `A = PointHash("AssetID", assetID)`.
-	a := CreateAssetPoint(&assetID)
-
-	// 4. Subtract `A` from the first point of `AC` and leave second point unmodified:
-	//         Q1 = AC - (A,O)
-	Q1 := *ac
-	Q1[0].Sub(&Q1[0], (*ecmath.Point)(&a))
-
-	// 5. Scalar-multiply `AC` by `value` and subtract the resulting pair from `VC`:
-	//         Q2 = VC - value·AC
-	v := (&ecmath.Scalar{}).SetUint64(value)
-
-	Q2 := *(*PointPair)(vc)
-	tmp := *(*PointPair)(ac)
-	tmp.ScMul(&tmp, v)
-	Q2.Sub(&Q2, &tmp)
-
-	// 6. [Validate excess commitment](#validate-excess-commitment) `Q1 || vp[0:64] || h`.
-	// 7. [Validate excess commitment](#validate-excess-commitment) `Q2 || vp[64:128] || h`.
-	var e1, e2 ExcessCommitment
-
-	e1.QC = PointPair(Q1)
-	e1.setSignatureBytes(vp[0:64])
-	e1.msg = h[:]
-
-	e2.QC = PointPair(Q2)
-	e2.setSignatureBytes(vp[64:128])
-	e2.msg = h[:]
-
-	return e1.Validate() && e2.Validate()
+	if len(proof) != 2 {
+		return false
+	}
+	return ValidateAssetIDProof(assetID, ac, message, proof[0]) &&
+		ValidateAmountProof(value, ac, vc, message, proof[1])
 }
--- crypto/ca/value_proof_test.go
+++ crypto/ca/value_proof_test.go
@@ -43,6 +43,49 @@ func TestAssetIDProof(t *testing.T) {
 	}
 }
 
+func TestNonblindedAmountProof(t *testing.T) {
+	assetID := AssetID{1}
+	value := uint64(2)
+	aek := AssetKey{3}
+	ac, _ := CreateAssetCommitment(assetID, aek)
+	vc, _ := CreateValueCommitment(value, ac, nil)
+	msg := []byte("message")
+
+	proof := CreateAmountProof(value, ac, vc, ecmath.Zero, msg)
+
+	if !ValidateAmountProof(value, ac, vc, msg, proof) {
+		t.Error("failed to validate amount proof")
+	}
+	if len(proof) != 0 {
+		t.Error("amount proof must be empty for non-blinded value commitment")
+	}
+}
+func TestAmountProof(t *testing.T) {
+	assetID := AssetID{1}
+	value := uint64(2)
+	aek := AssetKey{3}
+	ac, _ := CreateAssetCommitment(assetID, aek)
+	vek := ValueKey{4}
+	vc, f := CreateValueCommitment(value, ac, vek)
+	msg := []byte("message")
+
+	proof := CreateAmountProof(value, ac, vc, *f, msg)
+
+	if !ValidateAmountProof(value, ac, vc, msg, proof) {
+		t.Error("failed to validate amount proof")
+	}
+	if ValidateAmountProof(value+1, ac, vc, msg, proof) {
+		t.Error("validated invalid amount proof")
+	}
+	if ValidateAmountProof(value, ac, vc, msg[1:], proof) {
+		t.Error("validated invalid amount proof")
+	}
+	proof[0] ^= 1
+	if ValidateAmountProof(value, ac, vc, msg, proof) {
+		t.Error("validated invalid amount proof")
+	}
+}
+
 func TestValueProof(t *testing.T) {
 	assetID := AssetID{1}
 	value := uint64(2)
@@ -52,22 +95,22 @@ func TestValueProof(t *testing.T) {
 	vc, f := CreateValueCommitment(value, ac, vek)
 	msg := []byte("message")
 
-	vp := CreateValueProof(assetID, value, ac, vc, *c, *f, msg)
+	proof := CreateValueProof(assetID, value, ac, vc, *c, *f, msg)
 
-	if !vp.Validate(assetID, value, ac, vc, msg) {
+	if !ValidateValueProof(assetID, value, ac, vc, msg, proof) {
 		t.Error("failed to validate value proof")
 	}
-	if vp.Validate(AssetID{1, 1}, value, ac, vc, msg) {
+	if ValidateValueProof(AssetID{1, 1}, value, ac, vc, msg, proof) {
 		t.Error("validated invalid value proof")
 	}
-	if vp.Validate(assetID, value+1, ac, vc, msg) {
+	if ValidateValueProof(assetID, value+1, ac, vc, msg, proof) {
 		t.Error("validated invalid value proof")
 	}
-	if vp.Validate(assetID, value, ac, vc, msg[1:]) {
+	if ValidateValueProof(assetID, value, ac, vc, msg[1:], proof) {
 		t.Error("validated invalid value proof")
 	}
-	vp[0] ^= 1
-	if vp.Validate(assetID, value, ac, vc, msg) {
+	proof[0][0] ^= 1
+	if ValidateValueProof(assetID, value, ac, vc, msg, proof) {
 		t.Error("validated invalid value proof")
 	}
 }
