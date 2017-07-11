fa5004a06fe3be844f6184684db5d7d9f9055e29
Vicki Niu <vicki.niu@gmail.com>
2017-07-10 18:28:10 -0700

    docker/ci: fix rocksDB and snappy segfault
    The wercker tests for `database/localdb` failed with a shell segfault. This
    change installs correct dependencies, including GCC 4.8, to avoid
    segfaulting on starting up localDB
    
    Closes #1429

--- generated/rev/revid.js
+++ generated/rev/revid.js
@@ -1,2 +1,2 @@
 
-export const rev_id = "main/rev3317"
+export const rev_id = "main/rev3318"
--- vendor/github.com/tecbot/gorocksdb/dynflag.go
+++ vendor/github.com/tecbot/gorocksdb/dynflag.go
@@ -4,12 +4,13 @@ package gorocksdb
 // #cgo CPPFLAGS: -I${SRCDIR}/../../facebook/rocksdb/include
 // #cgo CFLAGS: -I${SRCDIR}/../../facebook/rocksdb/include
 // #cgo LDFLAGS: -I${SRCDIR}/../../facebook/rocksdb
+// #cgo LDFLAGS: -I${SRCDIR}/../../google/snappy
 // #cgo LDFLAGS: ${SRCDIR}/../../facebook/rocksdb/librocksdb.a
+// #cgo LDFLAGS: ${SRCDIR}/../../google/snappy/build/libsnappy.a
 // #cgo LDFLAGS: -lstdc++
 // #cgo LDFLAGS: -lm
 // #cgo LDFLAGS: -lz
 // #cgo LDFLAGS: -lbz2
-// #cgo LDFLAGS: ${SRCDIR}/../../google/snappy/build/libsnappy.a
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all -lrt
 import "C"
