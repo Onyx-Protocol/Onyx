4af5b7005472e45ce77eb83983913739ef9ecfaf
chainbot <ops@chain.com>
2017-07-11 22:19:55 +0000

    auto rev id

--- generated/rev/RevId.java
+++ generated/rev/RevId.java
@@ -1,4 +1,4 @@
 
 public final class RevId {
-	public final String Id = "main/rev3323";
+	public final String Id = "main/rev3324";
 }
--- generated/rev/revid.rb
+++ generated/rev/revid.rb
@@ -1,4 +1,4 @@
 
 module Chain::Rev
-	ID = "main/rev3323".freeze
+	ID = "main/rev3324".freeze
 end
