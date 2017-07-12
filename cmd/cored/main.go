c4dbc0f95e26ff9d188e54df3f02ee148f18bb67
Jackson Owens <jackson_owens@alumni.brown.edu>
2017-07-11 17:49:34 -0700

    handle in core.Config

--- cmd/cored/main.go
+++ cmd/cored/main.go
@@ -263,7 +263,7 @@ func main() {
 		chainlog.Fatalkv(ctx, chainlog.KeyError, err)
 	}
 	confOpts, err := core.Config(ctx, db, sdb)
-	if err != nil && errors.Root(err) != raft.ErrUninitialized {
+	if err != nil {
 		chainlog.Fatalkv(ctx, chainlog.KeyError, err)
 	}
 
