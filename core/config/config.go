3bfd9605675e7ca7c86d9198b06a12c5e2cdb4ac
Vicki Niu <vicki.niu@gmail.com>
2017-07-17 12:22:12 -0700

    removing todo

--- core/config/config.go
+++ core/config/config.go
@@ -329,8 +329,6 @@ func tryGenerator(ctx context.Context, url, accessToken, blockchainID string, ht
 	return nil
 }
 
-// TODO(tessr): make all of this atomic in raft, so we don't get halfway through
-// a postgres->raft migration and fail, losing the second half of the migration
 func migrateAccessTokens(ctx context.Context, db pg.DB, sdb *sinkdb.DB) error {
 	store := authz.NewStore(sdb, GrantPrefix)
 	const q = `SELECT id, type, created FROM access_tokens`
