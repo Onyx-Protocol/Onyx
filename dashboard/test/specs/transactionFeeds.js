8c40cb873fafa03987e6242c09293ef334752419
Chris Garvin <chris.garvin@me.com>
2017-07-18 11:26:32 -0700

    dashboard: add transaction feed integration test

--- dashboard/test/specs/transactionFeeds.js
+++ dashboard/test/specs/transactionFeeds.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid')
+
 describe('transaction feeds', () => {
   describe('list view', () => {
     before(() => {
@@ -7,6 +9,26 @@ describe('transaction feeds', () => {
 
     it('displays the correct page title', () => {
       browser.getText('.PageTitle').should.contain('Transaction feeds')
+    })
+  })
+
+  describe('creating transaction feeds', () => {
+    before(() => {
+      return expect(testHelpers.ensureConfigured()).to.be.fulfilled
+        .then(() => browser.url('/transaction-feeds'))
+    })
+
+    it('can create a new transaction feed', () => {
+      const alias = 'test-tx-feed-' + uuid.v4()
+      browser.waitForVisible('.ItemList button')
+      browser.scroll('.ItemList button')
+      browser.click('.ItemList button')
+      browser.setValue('input[name=alias]', alias)
+      browser.scroll('.FormContainer button')
+      browser.click('.FormContainer button')
+      browser.waitForVisible('.ItemList')
+      browser.getText('.ItemList').should.contain('Created transaction feed. Create another?')
+      browser.getText('.ItemList').should.contain(alias)
     })
   })
 })
